5f80ed9c290b1486ec75e95624aa728039ff917d
davidf <davidf@alleninstitute.org>
2015-01-08 09:53:31 -0800

    adding setters to the ephys/orca data set modules

--- allen_wrench/core/ephys_data_set.py
+++ allen_wrench/core/ephys_data_set.py
@@ -10,6 +10,15 @@ class EphysDataSet( object ):
     
     def get_full_sweep(sweep_number):
         raise Exception("get_full_sweep not implemented")
+
+    def set_test_pulse(sweep_number, stimulus, response):
+        raise Exception("get_test_pulse not implemented")
+
+    def set_experiment(sweep_number, stimulus, response):
+        raise Exception("get_experiment not implemented")
+    
+    def set_full_sweep(sweep_number, stimulus, response):
+        raise Exception("get_full_sweep not implemented")
     
 
         
--- allen_wrench/core/orca_data_set.py
+++ allen_wrench/core/orca_data_set.py
@@ -30,15 +30,45 @@ class OrcaDataSet( EphysDataSet ):
 
     def get_full_sweep(self, sweep_number):
         with h5py.File(self.file_name,'r') as f:
-            exp = f['epochs']['Experiment_%d' % sweep_number]
+
             swp = f['epochs']['Sweep_%d' % sweep_number]
             
             stimulus = swp['stimulus']['timeseries']['data'].value
             response = swp['response']['timeseries']['data'].value
             
-            return {
+            out = {
                 'stimulus': stimulus,
                 'response': response,
-                'index_range': ( exp['stimulus']['idx_start'].value, exp['stimulus']['idx_stop'].value ),
                 'sampling_rate': swp['stimulus']['timeseries']['sampling_rate'].value
             }
+            
+            try:
+                # if the sweep has an experiment, the index range will point to the range of values containing the experimental data.
+                exp = f['epochs']['Experiment_%d' % sweep_number]
+                out['index_range'] = ( exp['stimulus']['idx_start'].value, exp['stimulus']['idx_stop'].value ),
+            except KeyError, e:
+                # this sweep has no experiment.  return the index range of the entire sweep.
+                out['index_range'] = ( swp['stimulus']['idx_start'].value, swp['stimulus']['idx_stop'].value ),
+                
+            return out
+
+
+    def set_full_sweep(self, sweep_number, stimulus, response):
+        self.set_data('Sweep_%d' % sweep_number, stimulus, response)
+
+    def set_experiment(self, sweep_number, stimulus, response):
+        self.set_data('Experiment_%d' % sweep_number, stimulus, response)
+
+    def set_test_pulse(self, sweep_number, stimulus, response):
+        self.set_data('TestPulse_%d' % sweep_number, stimulus, response)
+
+    def set_data(self, epoch, stimulus, response):
+        with h5py.File(self.file_name,'r+') as f:
+            ep = f['epochs'][epoch]
+
+            if stimulus is not None:
+                ep['stimulus']['timeseries']['data'][...] = stimulus
+
+            if response is not None:
+                ep['response']['timeseries']['data'][...] = response
+
