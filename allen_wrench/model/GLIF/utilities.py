91d71f8c6d61c962f138574a510954a2dae39a73
davidf <davidf@alleninstitute.org>
2015-03-04 10:44:55 -0800

    IT-10681: documenting utilities.py

--- allen_wrench/model/GLIF/utilities.py
+++ allen_wrench/model/GLIF/utilities.py
@@ -1,17 +1,23 @@
 import numpy as np
 import json
 
-class MissingSweepException( Exception ): pass
+
+class MissingSweepException( Exception ): 
+    """ An exception that can be used to indicate that a sweep required for processing is not available """
+    pass
 
 def read_json(file_name):
+    """ Shortcut reading JSON from a file. """
     with open(file_name, 'rb') as f:
         return json.loads(f.read())
 
 def write_json(file_name, obj):
+    """ Shortcut for writing JSON to a file.  This also takes care of serializing numpy and data types. """
     with open(file_name, 'wb') as f:
         f.write(json.dumps(obj, indent=2, default=json_handler))
 
 def json_handler(obj):
+    """ Used by write_json convert a few non-standard types to things that the json package can handle. """
     if hasattr(obj, 'to_dict'):
         return obj.to_dict()
     elif isinstance(obj, np.ndarray):
