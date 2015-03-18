a6521d3549f2c58d6a7025742d1182e9dcc1d73d
Tim Fliss <timf@alleninstitute.org>
2015-03-18 16:58:13 -0700

    IT-10246; zero out orca result traces before writing new ones.

--- allen_wrench/model/single_cell_biophysical/runner.py
+++ allen_wrench/model/single_cell_biophysical/runner.py
@@ -3,7 +3,7 @@ import allen_wrench.model.single_cell_biophysical.model_load as ml
 from allen_wrench.model.single_cell_biophysical.iclamp_stimulus import IclampStimulus
 from load_cell_parameters import load_cell_parameters
 
-
+import h5py
 import numpy as np
 from allen_wrench.core.orca_data_set import OrcaDataSet
 
@@ -22,13 +22,15 @@ def run(description):
                          description.data['conditions'][0])
     ml.setup_conditions(h, description.data['conditions'][0])
     
+    # zero out all sweeps in the output file
     orcas_out_path = manifest.get_path("output_orca")
-    output = OrcaDataSet(orcas_out_path)
+    zero_sweeps(orcas_out_path)
     
     run_params = description.data['runs'][0]
     sweeps = run_params['sweeps']
     
     stimulus_path = description.manifest.get_path('stimulus_path')
+    output = OrcaDataSet(orcas_out_path)
     
     for sweep in sweeps:
         iclamp = IclampStimulus(h)
@@ -45,6 +47,12 @@ def run(description):
         junction_potential = description.data['fitting'][0]['junction_potential']
         output_data = (np.array(vec['v']) - junction_potential) * mV
         output.set_sweep(sweep, None, output_data)
+
+def zero_sweeps(file_path):
+    with h5py.File(file_path, 'a') as f:
+        for sweep in f['epochs'].keys():
+            if sweep.startswith('Sweep'):
+                f['epochs'][sweep]['response']['timeseries']['data'][...] = 0
 
 
 if '__main__' == __name__:
