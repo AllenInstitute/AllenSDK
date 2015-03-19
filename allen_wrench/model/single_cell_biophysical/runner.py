3e82fde5b80b742b14dbb37a9f57d7f2fbe6769a
Tim Fliss <timf@alleninstitute.org>
2015-03-19 16:45:17 -0700

    IT-10246; zero out spikes and spike times before simulation, update spike times after

--- allen_wrench/model/single_cell_biophysical/runner.py
+++ allen_wrench/model/single_cell_biophysical/runner.py
@@ -1,15 +1,15 @@
 from allen_wrench.model.biophys_sim.config import Config
 import allen_wrench.model.single_cell_biophysical.model_load as ml
 from allen_wrench.model.single_cell_biophysical.iclamp_stimulus import IclampStimulus
-from load_cell_parameters import load_cell_parameters
-
-import h5py
-import numpy as np
 from allen_wrench.core.orca_data_set import OrcaDataSet
+from load_cell_parameters import load_cell_parameters
+import h5py, numpy
+
 
 def run(description):
     manifest = description.manifest
-
+    orcas_out_path = manifest.get_path("output_orca")
+    
     from neuron import h
     h.load_file("stdgui.hoc")
     h.load_file("import3d.hoc")
@@ -22,10 +22,6 @@ def run(description):
                          description.data['conditions'][0])
     ml.setup_conditions(h, description.data['conditions'][0])
     
-    # zero out all sweeps in the output file
-    orcas_out_path = manifest.get_path("output_orca")
-    zero_sweeps(orcas_out_path)
-    
     run_params = description.data['runs'][0]
     sweeps = run_params['sweeps']
     
@@ -35,24 +31,17 @@ def run(description):
     for sweep in sweeps:
         iclamp = IclampStimulus(h)
         iclamp.setup_instance(stimulus_path, sweep=sweep)
-    
+        
         vec = ml.record_values(h)
-    
+        
         h.finitialize()
         h.run()
         
-        # And to an Orca File
-        
+        # write to an Orca File
         mV = 1.0e-3
         junction_potential = description.data['fitting'][0]['junction_potential']
-        output_data = (np.array(vec['v']) - junction_potential) * mV
+        output_data = (numpy.array(vec['v']) - junction_potential) * mV
         output.set_sweep(sweep, None, output_data)
-
-def zero_sweeps(file_path):
-    with h5py.File(file_path, 'a') as f:
-        for sweep in f['epochs'].keys():
-            if sweep.startswith('Sweep'):
-                f['epochs'][sweep]['response']['timeseries']['data'][...] = 0
 
 
 if '__main__' == __name__:
