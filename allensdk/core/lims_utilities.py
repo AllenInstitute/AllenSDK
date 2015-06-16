c93af9ca6253cd943c4e8f361ed61fe23632b6ab
davidf <davidf@alleninstitute.org>
2015-06-16 11:51:45 -0700

    adding methods to lims utilities

--- allensdk/core/lims_utilities.py
+++ allensdk/core/lims_utilities.py
@@ -2,31 +2,63 @@ import os
 
 HDF5_FILE_TYPE_ID = 306905526
 NWB_FILE_TYPE_ID = 475137571
+METHOD_CONFIG_FILE_TYPE_ID = 324440685
+MODEL_PARAMETERS_FILE_TYPE_ID = 329230374
 
 def get_well_known_files_by_type(wkfs, wkf_type_id):
-    return [ os.path.join( wkf['storage_directory'], wkf['filename'] )
-             for wkf in wkfs
-             if wkf['well_known_file_type_id'] == wkf_type_id ]
+    out = [ os.path.join( wkf['storage_directory'], wkf['filename'] )
+            for wkf in wkfs
+            if wkf['well_known_file_type_id'] == wkf_type_id ]
+
+    if len(out) == 0:
+        raise IOError("Could not find well known files with type %d." % wkf_type_id)
+    
+    return out
+
+
+def get_well_known_file_by_type(wkfs, wkf_type_id):
+    out = get_well_known_files_by_type(wkfs, wkf_type_id)
+
+    nout = len(out)
+    if nout != 1:
+        raise IOError("Expected single well known file with type %d. Got %d." % (wkf_type_id, nout))
+    
+    return out[0]
+
 
 def get_well_known_files_by_name(wkfs, filename):
-    return [ os.path.join( wkf['storage_directory'], wkf['filename'] )
-             for wkf in wkfs
-             if wkf['filename'] == filename ]
+    out = [ os.path.join( wkf['storage_directory'], wkf['filename'] )
+            for wkf in wkfs
+            if wkf['filename'] == filename ]
 
+    if len(out) == 0:
+        raise IOError("Could not find well known files with name %s." % filename)
 
-def append_well_known_file(path, wkfs, wkf_type_id):
-    basename = os.path.basename(path)
-    dirname = os.path.dirname(path)
+def get_well_known_file_by_name(wkfs, filename):
+    out = get_well_known_files_by_name(wkfs, filename)
+
+    nout = len(out)
+    if nout != 1:
+        raise IOError("Expected single well known file with name %s. Got %d." % (filename, nout))
+    
+    return out[0]
+
+def append_well_known_file(wkfs, path, wkf_type_id=None, content_type=None):
+    record = {
+        'filename': os.path.basename(path),
+        'storage_directory': os.path.dirname(path)
+        }
+
+    if wkf_type_id is not None:
+        record['well_known_file_type_id'] = wkf_type_id
+
+    if content_type is not None:
+        record['content_type'] = content_type
 
     for wkf in wkfs:
-        if wkf['filename'] == basename:
-            wkf['storage_directory'] = dirname
-            wkf['well_known_file_type_id'] = wkf_type_id
+        if wkf['filename'] == record['filename']:
+            wkf.update(record)
             return
 
-    wkfs.append({
-            'filename': basename,
-            'storage_directory': dirname,
-            'well_known_file_type_id': wkf_type_id
-            })
+    wkfs.append(record)
 
