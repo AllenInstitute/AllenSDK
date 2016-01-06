1a842c2eb391b3f1149af4595028316a24b73f7d
Keith Godfrey <keithg@alleninstitute.org>
2016-01-06 12:07:07 -0800

    IT-12201 morphology feature pipeline module, free from v3d

--- allensdk/morphology/morphology.py
+++ allensdk/morphology/morphology.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import morphology_analysis as morphology
+import traceback
 import json
 import sys
 
 def usage():
-    print("This script generates feature data from neuron morphology data")
+    print("This script generates feature data from a neuron SWC morphology")
     if sys.argv[0].startswith("./"):
         name = sys.argv[0][2:]
     else:
@@ -12,29 +13,19 @@ def usage():
     print("Usage: %s <input.json> <output.json>" % name)
     sys.exit(1)
 
-# minimum input to script is script name, output file name and one swc file
-# if we don't have at least this, abort
 if len(sys.argv) != 3:
     usage()
 
 infile = sys.argv[1]
+outfile = sys.argv[2]
 try:
     jin = json.load(open(infile, "r"))
 except:
     print("Unable to open/read file '%s' as json" % infile)
     sys.exit(1)
 
-# find swc file in jin[well_known_files]
-wkf = jin["well_known_files"]
-swc_file = None
-for i in range(len(wkf)):
-    if "filename" in wkf[i] and wkf[i]["filename"].endswith(".swc"):
-        if swc_file is not None:
-            print("Error -- multiple .swc files specified in %s" % infile)
-            print("Don't know which one to process")
-            sys.exit(1)
-        swc_file = wkf[i]["storage_directory"] + wkf[i]["filename"]
-
+# get the input file
+swc_file = jin["swc_file"]
 
 # dictionary of feature data
 morph_data = {}
@@ -45,54 +36,90 @@ feature_desc = None
 gmi_desc = None
 
 # load segment data into memory
-print("Processing '%s'" % swc_file)
-nrn = morphology.SWC(swc_file)
+try:
+    print("Processing '%s'" % swc_file)
+    nrn = morphology.SWC(swc_file)
+except:
+    print("*************************")
+    print("Error loading SWC file into memory")
+    raise
 
-# apply pia transform
-# algorithm inferred from v3d:apply_transform_func.cpp:apply_transform()
-#   and Xiaoxiao's script to run v3d
-# foreach segment:
-#   x = x*tvr_00 + y*tvr_01 + z*tvr_02 + tvr_09
-#   y = x*tvr_03 + y*tvr_04 + z*tvr_05 + tvr_10
-#   z = x*tvr_06 + y*tvr_07 + z*tvr_08 + tvr_11
-xf = jin["pia_transform"]
-for i in range(len(nrn.obj_list)):
-    seg = nrn.obj_list[i]
-    x = seg.x
-    y = seg.y
-    z = seg.z
-    seg.x = x*xf["tvr_00"] + y*xf["tvr_01"] + z*xf["tvr_02"] + xf["tvr_09"]
-    seg.y = x*xf["tvr_03"] + y*xf["tvr_04"] + z*xf["tvr_05"] + xf["tvr_10"]
-    seg.z = x*xf["tvr_06"] + y*xf["tvr_07"] + z*xf["tvr_08"] + xf["tvr_11"]
+# apply transform to 'pia-space'
+try:
+    xf = jin["pia_transform"]
+    affine = []
+    affine.append(1000.0 * xf["tvr_00"])
+    affine.append(1000.0 * xf["tvr_01"])
+    affine.append(1000.0 * xf["tvr_02"])
+    affine.append(1000.0 * xf["tvr_03"])
+    affine.append(1000.0 * xf["tvr_04"])
+    affine.append(1000.0 * xf["tvr_05"])
+    affine.append(1000.0 * xf["tvr_06"])
+    affine.append(1000.0 * xf["tvr_07"])
+    affine.append(1000.0 * xf["tvr_08"])
+    affine.append(1000.0 * xf["tvr_09"])
+    affine.append(1000.0 * xf["tvr_10"])
+    affine.append(1000.0 * xf["tvr_11"])
+except:
+    print("*************************")
+    print("Error reading affine transform from input json file")
+    traceback.print_exc()
+    sys.exit(1)
+try:
+    nrn.apply_affine(affine)
+except:
+    print("*************************")
+    print("Error reading affine transform from input json file")
+    traceback.print_exc()
+    sys.exit(1)
+#nrn.save_to("bar.swc")
 
-# calculate features
-gmi, gmi_desc = morphology.computeGMI(nrn)
-gmi_out = {}
-for i in range(len(gmi)):
-    gmi_out[gmi_desc[i]] = gmi[i]
-morph_data["gmi"] = gmi_out
+########################################################################
 #
-features, feature_desc = morphology.computeFeature(nrn)
-feat_out = {}
-for i in range(len(features)):
-    feat_out[feature_desc[i]] =  features[i]
-morph_data["features"] = feat_out
+# calculate features
+# GMI (moments)
+try:
+    gmi, gmi_desc = morphology.computeGMI(nrn)
+    gmi_out = {}
+    for i in range(len(gmi)):
+        gmi_out[gmi_desc[i]] = gmi[i]
+    morph_data["gmi"] = gmi_out
+except:
+    print("*************************")
+    print("Error calculating GMI features")
+    traceback.print_exc()
+    sys.exit(1)
+# morphology features
+try:
+    features, feature_desc = morphology.computeFeature(nrn)
+    feat_out = {}
+    for i in range(len(features)):
+        feat_out[feature_desc[i]] =  features[i]
+    morph_data["features"] = feat_out
+except:
+    print("*************************")
+    print("Error calculating GMI features")
+    traceback.print_exc()
+    sys.exit(1)
 
 # make sure nothing bad happened in analysis
 if feature_desc is None or gmi_desc is None:
+    print("*************************")
     print("Internal error -- bailing out")
     sys.exit(1)
 
+########################################################################
+# output the data
 jout = jin
 if "morphology_data" in jout:
     del jout["morphology_data"]
 jout["morphology_data"] = morph_data
 try:
-    with open(sys.argv[2], "w") as f:
+    with open(outfile, "w") as f:
         json.dump(jout, f, indent=2)
         f.close()
 except:
-    print("Error writing output json file %s" % sys.argv[2])
+    print("Error writing output json file %s" % outfile)
     sys.exit(1)
 
 
