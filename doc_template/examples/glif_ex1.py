bb451bbef6f1cfdfccb7ce626f2421ab8ce80095
davidf <davidf@alleninstitute.org>
2016-02-22 15:48:40 -0800

    removing IDs from glif examples

--- doc_template/examples/glif_ex1.py
+++ doc_template/examples/glif_ex1.py
@@ -5,10 +5,10 @@ neuronal_model_id = 472423251
 
 glif_api = GlifApi()
 glif_api.get_neuronal_model(neuronal_model_id)
-glif_api.cache_stimulus_file('49154717.nwb')
+glif_api.cache_stimulus_file('stimulus.nwb')
 
 neuron_config = glif_api.get_neuron_config()
-json_utilities.write('49154717_neuron_config.json', neuron_config)
+json_utilities.write('neuron_config.json', neuron_config)
 
 ephys_sweeps = glif_api.get_ephys_sweeps()
 json_utilities.write('ephys_sweeps.json', ephys_sweeps)
