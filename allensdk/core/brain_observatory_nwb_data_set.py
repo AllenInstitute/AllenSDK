a8ac6ad3a54b9fe15478dd1145f5822eb325f3f5
David Feng <davidf@alleninstitute.org>
2016-06-08 16:05:32 -0700

    IT-13554: adding license

--- allensdk/core/brain_observatory_nwb_data_set.py
+++ allensdk/core/brain_observatory_nwb_data_set.py
@@ -1,10 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 14 14:04:24 2015
+# Copyright 2016 Allen Institute for Brain Science
+# This file is part of Allen SDK.
+#
+# Allen SDK is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# Allen SDK is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Allen SDK.  If not, see <http://www.gnu.org/licenses/>.
 
-@author: saskiad@alleninstitute.org
-Functions to extract relevant data from the CAM NWB files
-"""
 import h5py
 import pandas as pd
 import numpy as np
