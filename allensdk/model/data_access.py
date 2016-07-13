eee221ab395893ca54e25040109b67936206950f
Corinne Teeter <corinnet@alleninstitute.org>
2016-07-13 11:39:05 -0700

    commenting and renaming variables in load functions

--- allensdk/model/data_access.py
+++ allensdk/model/data_access.py
@@ -2,7 +2,26 @@ from allensdk.core.nwb_data_set import NwbDataSet
 from scipy import signal
 import numpy as np
 
-def load_sweep(file_name, sweep_number, dt=None, cut=0, bessel=False):
+def load_sweep(file_name, sweep_number, desired_dt=None, cut=0, bessel=False):
+    '''load a data sweep and do specified data processing.
+    Inputs:
+        file_name: string
+            name of .nwb data file
+        sweep_number: 
+            number specifying the sweep to be loaded
+        desired_dt: 
+            the size of the time step the data should be subsampled to
+        cut:
+            indicie of which to start reporting data (i.e. cut off data before this indicie)
+        bessel: dictionary
+            contains parameters 'N' and 'Wn' to implement standard python bessel filtering
+    Returns:
+        dictionary containing
+            voltage: array
+            current: array
+            dt: time step of the returned data
+            start_idx: the index at which the first stimulus starts (excluding the test pulse)
+    '''
     ds = NwbDataSet(file_name)
     data = ds.get_sweep(sweep_number)
 
@@ -16,12 +35,12 @@ def load_sweep(file_name, sweep_number, dt=None, cut=0, bessel=False):
         b, a = signal.bessel(bessel["N"], bessel["Wn"], "low")
         data['response'] = signal.filtfilt(b, a, data['response'], axis=0)
 
-    if dt is not None:
-        if data["dt"] != dt:
-            data["response"] = subsample_data(data["response"], "mean", data["dt"], dt)
-            data["stimulus"] = subsample_data(data["stimulus"], "mean", data["dt"], dt)
-            data["start_idx"] = int(data["index_range"][0] / (dt / data["dt"]))
-            data["dt"] = dt
+    if desired_dt is not None:
+        if data["dt"] != desired_dt:
+            data["response"] = subsample_data(data["response"], "mean", data["dt"], desired_dt)
+            data["stimulus"] = subsample_data(data["stimulus"], "mean", data["dt"], desired_dt)
+            data["start_idx"] = int(data["index_range"][0] / (desired_dt / data["dt"]))
+            data["dt"] = desired_dt
 
     if "start_idx" not in data:
         data["start_idx"] = data["index_range"][0]
@@ -35,6 +54,26 @@ def load_sweep(file_name, sweep_number, dt=None, cut=0, bessel=False):
 
 
 def load_sweeps(file_name, sweep_numbers, dt=None, cut=0, bessel=False):
+    '''load sweeps and do specified data processing.
+    Inputs:
+        file_name: string
+            name of .nwb data file
+        sweep_numbers: 
+            sweep numbers to be loaded
+        desired_dt: 
+            the size of the time step the data should be subsampled to
+        cut:
+            indicie of which to start reporting data (i.e. cut off data before this indicie)
+        bessel: dictionary
+            contains parameters 'N' and 'Wn' to implement standard python bessel filtering
+    Returns:
+        dictionary containing
+            voltage: list of voltage trace arrays
+            current: list of current trace arrays
+            dt: list of time step corresponding to each array of the returned data
+            start_idx: list of the indicies at which the first stimulus starts (excluding 
+                the test pulse) in each returned sweep
+    '''
     data = [ load_sweep(file_name, sweep_number, dt, cut, bessel) for sweep_number in sweep_numbers ]
 
     return {
@@ -47,7 +86,7 @@ def load_sweeps(file_name, sweep_numbers, dt=None, cut=0, bessel=False):
 
 def subsample_data(data, method, present_time_step, desired_time_step):
     if present_time_step > desired_time_step:
-        raise Exception("you desired times step is smaller than your current time step")
+        raise Exception("you desired time step is smaller than your present time step")
 
     # number of elements to average over
     n = int(desired_time_step / present_time_step)
