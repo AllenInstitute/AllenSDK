5d419cca0371eb1c138a42bbbbf4bf9d5fb50c5f
Tim Fliss <timf@alleninstitute.org>
2016-09-29 16:04:51 -0700

    IT-13636; use requests.py for large file download.

--- allensdk/test/api/test_api.py
+++ allensdk/test/api/test_api.py
@@ -14,10 +14,11 @@
 # along with Allen SDK.  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
-from mock import Mock, MagicMock
+from mock import MagicMock
 from allensdk.api.api import Api
 import allensdk.core.json_utilities as ju
-import socket
+import requests
+
 try:
     import urllib.request as urllib_request
 except:
@@ -39,57 +40,12 @@ def api():
     return api
 
 
-@pytest.fixture
-def failed_download_api():
-    error404 = urllib_error.HTTPError(code=404,
-                                      msg='not found',
-                                      hdrs=Mock(),
-                                      fp=Mock(),
-                                      url='')
+def test_failed_download(api):
+    with pytest.raises(requests.exceptions.RequestException) as e_info:
+        api.retrieve_file_over_http('http://example.com/yo.jpg',
+                                    '/tmp/testfile')
 
-    urllib_request.urlopen = Mock(side_effect=error404)
-
-    api = Api()
-    api._log.error = Mock()
-    # api.retrieve_file_over_http = MagicMock()
-
-    return api
-
-
-@pytest.fixture
-def timeout_download_api():
-    error_timeout = socket.timeout
-
-    urllib_request.urlopen = Mock(side_effect=error_timeout)
-
-    api = Api()
-    api._log.error = Mock()
-    # api.retrieve_file_over_http = MagicMock()
-
-    return api
-
-
-def test_timeout_download(timeout_download_api):
-    with pytest.raises(socket.timeout) as e_info:
-        timeout_download_api.retrieve_file_over_http('http://example.com/yo.zip',
-                                                     '/tmp/testfile')
-
-    # assert cp.assert_called_once_with('foobar')
-    timeout_download_api._log.error.assert_called_once_with(
-        "Timed out retrieving file from http://example.com/yo.zip")
-    assert e_info.typename == 'timeout'
-
-
-def test_failed_download(failed_download_api):
-    with pytest.raises(urllib_error.HTTPError) as e_info:
-        failed_download_api.retrieve_file_over_http('http://example.com/yo.jpg',
-                                                    '/tmp/testfile')
-
-    # assert cp.assert_called_once_with('foobar')
-    failed_download_api._log.error.assert_called_once_with(
-        "Couldn't retrieve file from http://example.com/yo.jpg")
-    assert e_info.typename == 'HTTPError'
-    assert e_info.value.code == 404
+        assert e_info.typename == 'HTTPError'
 
 
 def test_do_query_post(api):
