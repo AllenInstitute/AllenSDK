1237dc142da5ef1e8ee7a895a7fa99a31ef77b98
Nicholas Cain <nicain.seattle@gmail.com>
2017-05-15 16:08:27 -0700

    gratings working!

--- allensdk/brain_observatory/stimulus_info.py
+++ allensdk/brain_observatory/stimulus_info.py
@@ -12,7 +12,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Allen SDK.  If not, see <http://www.gnu.org/licenses/>.
+
 import six
+import numpy as np
+import scipy.ndimage.interpolation as spndi
+from scipy.misc import imresize
+import matplotlib.pyplot as plt
+import itertools
 
 # some handles for stimulus types
 DRIFTING_GRATINGS = 'drifting_gratings'
@@ -41,9 +47,21 @@ SESSION_STIMULUS_MAP = {
 
 LOCALLY_SPARSE_NOISE_DIMENSIONS = {
     LOCALLY_SPARSE_NOISE: [ 16, 28 ],
-    LOCALLY_SPARSE_NOISE_4DEG: [ 16, 28 ],    
+    LOCALLY_SPARSE_NOISE_4DEG: [ 16, 28 ],
     LOCALLY_SPARSE_NOISE_8DEG: [ 8, 14 ],
     }
+
+LOCALLY_SPARSE_NOISE_PIXELS = 45
+LOCALLY_SPARSE_NOISE_PIXELS_4DEG = 45
+LOCALLY_SPARSE_NOISE_PIXELS_8DEG = 90
+NATURAL_SCENES_PIXELS = (918, 1174)
+NATURAL_MOVIE_PIXELS = (1080, 1920)
+
+MONITOR_DIMENSIONS = (1200, 1920)
+MONITOR_DISTANCE = 15
+
+STIMULUS_GRAY = 127
+STIMULUS_BITDEPTH = 8
 
 def sessions_with_stimulus(stimulus):
     """ Return the names of the sessions that contain a given stimulus. """
@@ -177,3 +195,301 @@ class StimulusSearch(object):
 
                 # Frame is unregistered at the coarse level; return None
                 return None
+
+def rotate(X, Y, theta):
+    Xp = X * np.cos(theta) - Y * np.sin(theta)
+    Yp = X * np.sin(theta) + Y * np.cos(theta)
+    return Xp, Yp
+
+def get_spatial_grating(height=None, aspect_ratio=None, ori=None, pix_per_cycle=None, phase=None, p2p_amp=2, baseline=0):
+
+    aspect_ratio = float(aspect_ratio)
+    _height_prime = 100
+
+    sf = 1./(float(pix_per_cycle)/(height/float(_height_prime)))
+
+    # Final height set by zoom below:
+    y, x = (_height_prime,_height_prime*aspect_ratio)
+
+    theta = ori * np.pi / 180.0  # convert to radians
+
+    ph = phase * np.pi * 2.0
+
+    X, Y = np.meshgrid(np.arange(x), np.arange(y))
+    X = X - x / 2
+    Y = Y - y / 2
+    Xp, Yp = rotate(X, Y, theta)
+
+    img = np.cos(2.0 * np.pi * Xp * sf + ph)
+    return (p2p_amp/2.)*spndi.zoom(img, height/float(_height_prime)) + baseline
+
+def get_spatio_temporal_grating(t, temporal_frequency=None, **kwargs):
+
+    kwargs['phase'] = kwargs.pop('phase', 0) + (float(t)*temporal_frequency)%1
+
+    return get_spatial_grating(**kwargs)
+
+def map_image_coordinate_to_monitor_coordinate(img_coord, monitor_shape, template_shape):
+
+    rx, cx = img_coord
+    n_pixels_r, n_pixels_c = monitor_shape
+    tr, tc = template_shape
+
+    rx_new = float((n_pixels_r - tr) / 2) + rx
+    cx_new = float((n_pixels_c - tc) / 2) + cx
+
+    return rx_new, cx_new
+
+def lsn_coordinate_to_monitor_coordinate(lsn_coordinate, monitor_shape, template_shape, pixels_per_patch):
+
+    rx, cx = lsn_coordinate
+    tr, tc = template_shape
+
+    return map_image_coordinate_to_monitor_coordinate((rx*pixels_per_patch, cx*pixels_per_patch),
+                                                      monitor_shape,
+                                                      (tr*pixels_per_patch, tc*pixels_per_patch))
+
+def natural_scene_coordinate_to_monitor_coordinate(natural_scene_coordinate, monitor_shape):
+
+    return map_image_coordinate_to_monitor_coordinate(natural_scene_coordinate, monitor_shape, NATURAL_SCENES_PIXELS)
+
+def natural_movie_coordinate_to_monitor_coordinate(natural_movie_coordinate, monitor_shape):
+
+    return map_image_coordinate_to_monitor_coordinate(natural_movie_coordinate, monitor_shape, NATURAL_MOVIE_PIXELS)
+
+class Monitor(object):
+
+    def __init__(self, n_pixels_r, n_pixels_c, panel_size, spatial_unit):
+
+        self.spatial_unit = spatial_unit
+        if spatial_unit == 'cm':
+            self.spatial_conversion_factor = 1.
+        else:
+            raise NotImplementedError
+
+        self._panel_size = panel_size
+        self.n_pixels_r = n_pixels_r
+        self.n_pixels_c = n_pixels_c
+
+    @property
+    def panel_size(self):
+        return self._panel_size*self.spatial_conversion_factor
+
+    @property
+    def aspect_ratio(self):
+        return float(self.n_pixels_c)/self.n_pixels_r
+
+    @property
+    def height(self):
+        return self.spatial_conversion_factor*np.sqrt(self.panel_size**2/(1+self.aspect_ratio**2))
+
+    @property
+    def width(self):
+        return self.height*self.aspect_ratio
+
+    def set_spatial_unit(self, new_unit):
+        if new_unit == self.spatial_unit:
+            pass
+        elif new_unit == 'inch' and self.spatial_unit == 'cm':
+            self.spatial_conversion_factor *= .393701
+        elif new_unit == 'cm' and self.spatial_unit == 'inch':
+            self.spatial_conversion_factor *= 1./.393701
+        else:
+            raise NotImplementedError
+        self.spatial_unit = new_unit
+
+    @property
+    def pixel_size(self):
+        return float(self.width)/self.n_pixels_c
+
+    def pixels_to_visual_degrees(self, n, distance_from_monitor, small_angle_approximation=True):
+
+        if small_angle_approximation == True:
+            return n*self.pixel_size/distance_from_monitor*57.2958 # radians to degrees
+        else:
+            return 2*np.arctan(n*1./2*self.pixel_size / distance_from_monitor) * 57.2958  # radians to degrees
+
+    def lsn_image_to_screen(self, img, pixels_per_patch):
+
+        assert img.dtype == np.uint8
+
+        full_image = np.full((self.n_pixels_r, self.n_pixels_c), STIMULUS_GRAY, dtype=np.uint8)
+        img_full_res = imresize(img, float(pixels_per_patch), interp='nearest')
+        mr, mc = lsn_coordinate_to_monitor_coordinate((0, 0), (self.n_pixels_r, self.n_pixels_c), img.shape, pixels_per_patch)
+        Mr, Mc = lsn_coordinate_to_monitor_coordinate(img.shape, (self.n_pixels_r, self.n_pixels_c), img.shape, pixels_per_patch)
+        full_image[int(mr):int(Mr), int(mc):int(Mc)] = img_full_res
+
+        return full_image
+
+    def natural_scene_image_to_screen(self, img):
+
+        assert img.dtype == np.float32
+        img = img.astype(np.uint8)
+
+        full_image = np.full((self.n_pixels_r, self.n_pixels_c), 127, dtype=np.uint8)
+        mr, mc = natural_scene_coordinate_to_monitor_coordinate((0, 0), (self.n_pixels_r, self.n_pixels_c))
+        Mr, Mc = natural_scene_coordinate_to_monitor_coordinate((img.shape[0], img.shape[1]), (self.n_pixels_r, self.n_pixels_c))
+        full_image[int(mr):int(Mr), int(mc):int(Mc)] = img
+
+        return np.flipud(full_image)
+
+    def natural_movie_image_to_screen(self, img):
+
+        img = imresize(img, NATURAL_MOVIE_PIXELS)
+
+        assert img.dtype == np.uint8
+
+        full_image = np.full((self.n_pixels_r, self.n_pixels_c), 127, dtype=np.uint8)
+        mr, mc = natural_movie_coordinate_to_monitor_coordinate((0, 0), (self.n_pixels_r, self.n_pixels_c))
+        Mr, Mc = natural_movie_coordinate_to_monitor_coordinate((img.shape[0], img.shape[1]), (self.n_pixels_r, self.n_pixels_c))
+        full_image[int(mr):int(Mr), int(mc):int(Mc)] = img
+
+        return np.flipud(full_image)
+
+    def spatial_frequency_to_pix_per_cycle(self, spatial_frequency, distance_from_monitor):
+
+        # How many cycles do I want to see post warp:
+        number_of_cycles = spatial_frequency*2*np.degrees(np.arctan(self.width/2./distance_from_monitor))
+
+        # How many pixels to I have pre-warp to place my cycles on:
+        mask = self.get_mask()
+        _, m_col = np.where(mask != 0)
+        number_of_pixels = (m_col.max() - m_col.min())
+
+        return float(number_of_pixels)/number_of_cycles
+
+
+    def grating_to_screen(self, phase, spatial_frequency, orientation, distance_from_monitor, p2p_amp=256, baseline=127):
+
+        pix_per_cycle = self.spatial_frequency_to_pix_per_cycle(spatial_frequency, distance_from_monitor)
+
+        grating = get_spatial_grating(height=self.n_pixels_r,
+                                      aspect_ratio=self.aspect_ratio,
+                                      ori=orientation,
+                                      pix_per_cycle=pix_per_cycle,
+                                      phase=phase,
+                                      p2p_amp=p2p_amp,
+                                      baseline=baseline)
+
+        return grating
+
+    def get_mask(self):
+
+        from allensdk.core.brain_observatory_nwb_data_set import make_display_mask
+
+        mask = make_display_mask(display_shape=(self.n_pixels_c, self.n_pixels_r)).T
+        assert mask.shape[0] == self.n_pixels_r
+        assert mask.shape[1] == self.n_pixels_c
+
+        return mask
+
+    def show_image(self, img, ax=None, show=True, mask=False, warp=False):
+
+        from allensdk.core.brain_observatory_nwb_data_set import make_display_mask
+
+        assert img.shape == (self.n_pixels_r, self.n_pixels_c)
+
+        if ax is None:
+            fig, ax = plt.subplots(1, 1)
+
+        if warp == True:
+            img = self.warp_image(img)
+
+        if warp == True:
+            assert mask == False
+
+        ax.imshow(img, origin='lower', cmap=plt.cm.gray, interpolation='none')
+
+        if mask == True:
+            mask = make_display_mask(display_shape=(self.n_pixels_c, self.n_pixels_r)).T
+            alpha_mask = np.zeros((mask.shape[0], mask.shape[1], 4))
+            alpha_mask[:, :, 2] = 1 - mask
+            alpha_mask[:, :, 3] = .4
+            ax.imshow(alpha_mask, origin='lower', interpolation='none')
+
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+
+        if show == True:
+            plt.show()
+
+class ExperimentGeometry(object):
+
+    def __init__(self, distance, mon_height_cm, mon_width_cm, mon_res, eyepoint):
+
+        self.distance = distance
+        self.mon_height_cm = mon_height_cm
+        self.mon_width_cm = mon_width_cm
+        self.mon_res = mon_res
+        self.eyepoint = eyepoint
+
+        self._warp_coordinates = None
+
+    @property
+    def warp_coordinates(self):
+        if self._warp_coordinates is None:
+            self._warp_coordinates = self.generate_warp_coordinates()
+
+        return self._warp_coordinates
+
+    def generate_warp_coordinates(self):
+
+        from allensdk.core.brain_observatory_nwb_data_set import warp_stimulus_coords
+
+        display_shape=self.mon_res
+        x = np.array(range(display_shape[0])) - display_shape[0] / 2
+        y = np.array(range(display_shape[1])) - display_shape[1] / 2
+        display_coords = np.array(list(itertools.product(y, x)))
+
+        warp_coorinates = warp_stimulus_coords(display_coords,
+                                               distance=self.distance,
+                                               mon_height_cm=self.mon_height_cm,
+                                               mon_width_cm=self.mon_width_cm,
+                                               mon_res=self.mon_res,
+                                               eyepoint=self.eyepoint)
+
+        warp_coorinates[:, 0] += display_shape[1] / 2
+        warp_coorinates[:, 1] += display_shape[0] / 2
+
+        return warp_coorinates
+
+class BrainObservatoryMonitor(Monitor):
+    '''
+    http://help.brain-map.org/display/observatory/Documentation?preview=/10616846/10813485/VisualCoding_VisualStimuli.pdf
+    https://www.cnet.com/products/asus-pa248q/specs/
+    '''
+
+    def __init__(self, experiment_geometry=None):
+
+        height, width = MONITOR_DIMENSIONS
+
+        super(BrainObservatoryMonitor, self).__init__(height, width, 61.214, 'cm')
+
+        if experiment_geometry is None:
+            self.experiment_geometry = ExperimentGeometry(distance=float(MONITOR_DISTANCE), mon_height_cm=self.height, mon_width_cm=self.width, mon_res=(self.n_pixels_c, self.n_pixels_r), eyepoint=(0.5, 0.5))
+        else:
+            self.experiment_geometry = experiment_geometry
+
+    def lsn_image_to_screen(self, img):
+
+        if img.shape == tuple(LOCALLY_SPARSE_NOISE_DIMENSIONS[LOCALLY_SPARSE_NOISE]):
+            return super(BrainObservatoryMonitor, self).lsn_image_to_screen(img, LOCALLY_SPARSE_NOISE_PIXELS)
+        elif img.shape == tuple(LOCALLY_SPARSE_NOISE_DIMENSIONS[LOCALLY_SPARSE_NOISE_4DEG]):
+            return super(BrainObservatoryMonitor, self).lsn_image_to_screen(img, LOCALLY_SPARSE_NOISE_PIXELS_4DEG)
+        elif img.shape == tuple(LOCALLY_SPARSE_NOISE_DIMENSIONS[LOCALLY_SPARSE_NOISE_8DEG]):
+            return super(BrainObservatoryMonitor, self).lsn_image_to_screen(img, LOCALLY_SPARSE_NOISE_PIXELS_8DEG)
+        else:
+            raise RuntimeError
+
+    def warp_image(self, img, **kwargs):
+
+        assert img.shape == (self.n_pixels_r, self.n_pixels_c)
+        assert self.spatial_unit == 'cm'
+
+        return spndi.map_coordinates(img, self.experiment_geometry.warp_coordinates.T).reshape((self.n_pixels_r, self.n_pixels_c))
+
+    def grating_to_screen(self, phase, spatial_frequency, orientation):
+
+        return super(BrainObservatoryMonitor, self).grating_to_screen(phase, spatial_frequency, orientation,
+                                                                      self.experiment_geometry.distance,
+                                                                      p2p_amp = 256, baseline = 127)
