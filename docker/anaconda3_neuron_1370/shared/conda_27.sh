e09a5da8b1fb8120d3ec0497d851699ccc4d4648
Tim Fliss <timf@alleninstitute.org>
2017-06-05 16:06:12 -0700

    ABS-64; upgrade pip before installing requirements

--- docker/anaconda3_neuron_1370/shared/conda_27.sh
+++ docker/anaconda3_neuron_1370/shared/conda_27.sh
@@ -2,5 +2,6 @@ conda update anaconada
 conda env create -n py27 python=2.7 -f shared/environment.yml
 conda install -n py27 -c https://conda.anaconda.org/simpleitk SimpleITK
 source activate py27
+pip install --upgrade pip
 pip install -r requirements.txt
 pip install -r test_requirements.txt
