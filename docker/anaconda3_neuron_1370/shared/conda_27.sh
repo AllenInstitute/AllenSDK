361ade37f22a04bb27e36ea1e115e64c22566238
Tim Fliss <timf@alleninstitute.org>
2017-06-05 17:18:31 -0700

    ABS-64; minor dependency install fix.

--- docker/anaconda3_neuron_1370/shared/conda_27.sh
+++ docker/anaconda3_neuron_1370/shared/conda_27.sh
@@ -3,5 +3,5 @@ conda env create -n py27 python=2.7 -f shared/environment.yml
 conda install -n py27 -c https://conda.anaconda.org/simpleitk SimpleITK
 source activate py27
 pip install --upgrade pip
-pip install -r requirements.txt
-pip install -r test_requirements.txt
+pip install --ignore-installed -r requirements.txt
+pip install --ignore-installed -r test_requirements.txt
