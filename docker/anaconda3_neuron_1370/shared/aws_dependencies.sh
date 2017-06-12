e46912b08255dd4a768f91c359a222a68b08b633
Tim Fliss <timf@alleninstitute.org>
2017-06-12 16:53:45 -0700

    ABS-64; more docker dependencies

--- docker/anaconda3_neuron_1370/shared/aws_dependencies.sh
+++ docker/anaconda3_neuron_1370/shared/aws_dependencies.sh
@@ -5,9 +5,13 @@ apt-get -yq install \
  libcurl4-openssl-dev \
  libxml2-dev
 
+source activate py27
+
 git clone https://github.com/s3fs-fuse/s3fs-fuse.git
 cd s3fs-fuse
 ./autogen.sh
 ./configure
 make
 make install
+
+pip install boto3 bokeh awscli
--- docker/anaconda3_neuron_1370/shared/conda_27.sh
+++ docker/anaconda3_neuron_1370/shared/conda_27.sh
@@ -1,10 +1,12 @@
-conda update anaconada
+conda update -y conada
 source activate root
 pip install -r requirements.txt
 pip install -r test_requirements.txt
 conda env create -n py27 python=2.7 -f shared/environment.yml
-conda install -n py27 -c https://conda.anaconda.org/simpleitk SimpleITK
+conda install -n py27 -y -c https://conda.anaconda.org/simpleitk SimpleITK
 source activate py27
 pip install --upgrade pip
 pip install --ignore-installed -r requirements.txt
 pip install --ignore-installed -r test_requirements.txt
+source activate root
+/opt/conda/envs/py27/bin/python -m ipykernel install
