1a133595b1b16f84366d5ec7d168c9204cbee38d
Tim Fliss <timf@alleninstitute.org>
2017-06-13 15:59:00 -0700

    ABS-64; fix conda py27 environment creation

--- docker/anaconda3_neuron_1370/shared/conda_27.sh
+++ docker/anaconda3_neuron_1370/shared/conda_27.sh
@@ -2,7 +2,7 @@ conda update -y conada
 source activate root
 pip install -r requirements.txt
 pip install -r test_requirements.txt
-conda env create -n py27 python=2.7 -f shared/environment.yml
+conda create -n py27 python=2.7 anaconda
 conda install -n py27 -y -c https://conda.anaconda.org/simpleitk SimpleITK
 source activate py27
 pip install --upgrade pip
