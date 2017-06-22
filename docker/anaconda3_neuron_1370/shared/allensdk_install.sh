cfd714fac658654ffe1d7b324087121e3bbcf84c
Tim Fliss <timf@alleninstitute.org>
2017-06-22 14:16:36 -0700

    ABS-64; bump allensdk version in dockerfile to 0.13.2

--- docker/anaconda3_neuron_1370/shared/allensdk_install.sh
+++ docker/anaconda3_neuron_1370/shared/allensdk_install.sh
@@ -1,7 +1,7 @@
 cd /root
-wget https://github.com/AllenInstitute/AllenSDK/archive/v0.13.1.tar.gz
-tar xvzf v0.13.1.tar.gz
-mv AllenSDK-0.13.1 allensdk
+wget https://github.com/AllenInstitute/AllenSDK/archive/v0.13.2.tar.gz
+tar xvzf v0.13.2.tar.gz
+mv AllenSDK-0.13.2 allensdk
 cd allensdk
 source activate root
 pip install --ignore-installed -r requirements.txt
