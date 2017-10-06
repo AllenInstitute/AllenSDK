fce2ba4763bb8b1b59cd099dc0946528d2d649df
nicholasc <nicain.seattle@gmail.com>
2017-10-06 10:10:47 -0700

    adding documentation to deal with exception thrown by get_epoch_mask_list

--- allensdk/core/brain_observatory_nwb_data_set.py
+++ allensdk/core/brain_observatory_nwb_data_set.py
@@ -74,12 +74,15 @@ def get_epoch_mask_list(st, threshold, max_cuts=2):
     epoch_mask_list = []
 
     if len(cut_inds) > max_cuts:
-        
+        raise Exception('more than 2 epochs cut')
+
+        # Use this little snippet to help set a new high-water mark (so increase threshold) if exception occurs
+        # See https://github.com/AllenInstitute/AllenSDK/issues/66 for more info
+        '''
         import matplotlib.pyplot as plt
         plt.plot(delta)
         plt.show()
-
-        raise Exception('more than 2 epochs cut')
+        '''
 
     for ii in range(len(cut_inds)+1):
 
