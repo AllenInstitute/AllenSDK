184ecd9c21a0ce8abbc69179718d9987b53999bc
Tim Fliss <timf@alleninstitute.org>
2017-10-17 14:04:55 -0700

    ABS-147; add LICENSE.txt to sdist artifacts

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author = 'David Feng',
     author_email = 'davidf@alleninstitute.org',
     packages = prepend_find_packages('allensdk'),
-    package_data={'': ['*.conf', '*.cfg', '*.md', '*.json', '*.dat', '*.env', '*.sh', '*.txt', 'bps', 'Makefile', 'COPYING'] },
+    package_data={'': ['*.conf', '*.cfg', '*.md', '*.json', '*.dat', '*.env', '*.sh', '*.txt', 'bps', 'Makefile', 'LICENSE'] },
     description = 'core libraries for the allensdk.',
     install_requires = required,
     tests_require=test_required,
