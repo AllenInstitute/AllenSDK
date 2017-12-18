7a9e7fcf157880c6a847cb1c85c32310256cbb4d
nileg <nileg@alleninstitute.org>
2017-12-18 10:50:00 -0800

    #102 remove ontology reference in docs

--- doc_template/examples_root/examples/connectivity_ex2.py
+++ doc_template/examples_root/examples/connectivity_ex2.py
@@ -3,9 +3,9 @@ from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
 # tell the cache class what resolution (in microns) of data you want to download
 mcc = MouseConnectivityCache(resolution=25)
 
-# use the ontology class to get the id of the isocortex structure
-ontology = mcc.get_ontology('ontology.csv')
-isocortex = ontology['Isocortex']
+# use the structure tree class to get information about the isocortex structure
+structure_tree = mcc.get_structure_tree()
+isocortex_id = structure_tree.get_structures_by_name('Isocortex')[0]['id']
 
 # a list of dictionaries containing metadata for non-Cre experiments
 experiments = mcc.get_experiments(file_name='non_cre.json',
