4fa80a4528bac338c1abf68e0566694ea62e42af
David Feng <davidf@alleninstitute.org>
2018-07-18 15:17:43 -0700

    adding pipenv to test environment

--- docker/anaconda3/shared/conda_27.sh
+++ docker/anaconda3/shared/conda_27.sh
@@ -1,5 +1,7 @@
 conda update -y conda
 source activate root
+pip install -y pipenv
 conda create -n py27 python=2.7 anaconda
 source activate root
+pip install -y pipenv
 /opt/conda/envs/py27/bin/python -m ipykernel install
