fd2df62d70bd62d774cd627176c32b19fc3daada
davidf <davidf@alleninstitute.org>
2018-07-18 17:12:30 -0700

    typo

--- docker/anaconda3/shared/conda_36.sh
+++ docker/anaconda3/shared/conda_36.sh
@@ -2,4 +2,4 @@ conda update -y conda
 source activate base
 conda create -n py36 python=3.6 anaconda
 source activate base
-/opt/conda/envs/pyp36/bin/python -m ipykernel install
+/opt/conda/envs/py36/bin/python -m ipykernel install
