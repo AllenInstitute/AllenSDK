07d9bfe2c3004526853caeb9d24d53e4d183069a
davidf <davidf@alleninstitute.org>
2018-07-18 17:10:47 -0700

    miniconda needs apt update

--- docker/anaconda3/shared/apt_get_dependencies.sh
+++ docker/anaconda3/shared/apt_get_dependencies.sh
@@ -1,2 +1,2 @@
-#apt-get update
+apt-get update
 apt-get -yq install pkg-config make automake
--- docker/anaconda3/shared/conda_27.sh
+++ docker/anaconda3/shared/conda_27.sh
@@ -1,6 +1,5 @@
 conda update -y conda
 source activate base
 conda create -n py27 python=2.7 anaconda
-source activate py27
-pip install -y pipenv
+source activate base
 /opt/conda/envs/py27/bin/python -m ipykernel install
