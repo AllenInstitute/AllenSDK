3f1037dc8e0936e572b63c7e6ef45939b1dcceef
davidf <davidf@alleninstitute.org>
2018-07-18 17:10:47 -0700

    miniconda needs apt update

--- docker/anaconda3/shared/conda_36.sh
+++ docker/anaconda3/shared/conda_36.sh
@@ -1,6 +1,5 @@
 conda update -y conda
 source activate base
 conda create -n py36 python=3.6 anaconda
-source activate py36
-pip install -y pipenv
+source activate base
 /opt/conda/envs/pyp36/bin/python -m ipykernel install
