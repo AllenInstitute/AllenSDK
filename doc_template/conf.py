100b60f4ccc6cc74eac91d2de34bb256524e82f2
dyf <davidf@alleninstitute.org>
2018-07-26 09:46:16 -0700

    fixing docs

--- doc_template/conf.py
+++ doc_template/conf.py
@@ -11,6 +11,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+# this lets the docs build in a headless environment
+import matplotlib
+matplotlib.use('agg')
+
 import sys, os
 
 
