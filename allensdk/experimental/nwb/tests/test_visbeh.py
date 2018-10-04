66beb1fe570d536dddc87e0c8341aab10ea38d41
nicholasc <nicain.seattle@gmail.com>
2018-10-04 15:15:44 -0700

    reproduce table read from file error to aid in development of a fix

--- allensdk/experimental/nwb/tests/test_visbeh.py
+++ allensdk/experimental/nwb/tests/test_visbeh.py
@@ -9,7 +9,7 @@ from allensdk.experimental.nwb.stimulus import VisualBehaviorStimulusAdapter
 @pytest.mark.skipif(not os.environ.get('ALLENSDK_EXPERIMENTAL',''), reason='Experimental')
 def test_visbeh_running_speed(nwbfile, tmpfilename, visbeh_pkl):
 
-    test_speed = [0.2697893313797373, 2.294358589025947, 2.294358589025947, 0.26978933137973743, 0.2697893313797375]
+    test_speed = [0.26978933, 2.29435859, 4.31892785, 4.31892785, 4.31892785, 4.31892785]
 
     visbeh_data = VisualBehaviorStimulusAdapter(visbeh_pkl)
     assert np.allclose(visbeh_data.running_speed.data, test_speed)
@@ -23,10 +23,27 @@ def test_visbeh_running_speed(nwbfile, tmpfilename, visbeh_pkl):
 
 @pytest.mark.skipif(not os.environ.get('ALLENSDK_EXPERIMENTAL',''), reason='Experimental')
 def test_visbeh_epoch(nwbfile, tmpfilename, visbeh_pkl):
-
+    tmpfilename = '/home/nicholasc/foo.nwb'
     visbeh_data = VisualBehaviorStimulusAdapter(visbeh_pkl)
 
     epoch_table = visbeh_data.get_epoch_table()
+    nwbfile.epochs = epoch_table
+
+    # print nwbfile.epochs.to_dataframe()
+
+
+
+    nwbfile.add_acquisition(visbeh_data.running_speed)
+
+    with NWBHDF5IO(tmpfilename, mode='w') as io:
+        io.write(nwbfile)
+
+    nwbfile_in = NWBHDF5IO(tmpfilename, mode='r').read()
+
+    print nwbfile_in.epochs.to_dataframe()
+
+    raise
+
 
 
     
