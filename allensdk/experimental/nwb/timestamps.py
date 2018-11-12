2fda17a837578844f8c82c6ad931f574e18ce28e
nile graddis <nilegraddis@gmail.com>
2018-11-12 12:15:16 -0800

    #299 move sync extractor to experimental

--- allensdk/experimental/nwb/timestamps.py
+++ allensdk/experimental/nwb/timestamps.py
@@ -1,6 +1,6 @@
 # until we get a public reader...
 # pip install --trusted-host aibs-artifactory --index-url http://aibs-artifactory/artifactory/api/pypi/pypi-local/simple aibs-sync-extractor
-from aibs_sync_extractor.dataset import Dataset
+from allensdk.experimental.aibs_sync.dataset import Dataset
 
 
 def get_timestamps_from_sync(sync_file, key, use_falling_edges=True):
