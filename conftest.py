ecbd23ef666f89e8d1e33a85eb5e58212db70fb2
nile graddis <nilegraddis@gmail.com>
2018-12-06 16:49:06 -0800

    #309 centralize TEST_COMPLETE skips and improve message

--- conftest.py
+++ conftest.py
@@ -26,6 +26,15 @@ def pytest_collection_modifyitems(config, items):
         reason='this test requires that an API endpoint be specified (set the TEST_API_ENDPOINT environment variable).'
     )
 
+    skip_nightly_test = pytest.mark.skipif(
+        os.getenv('TEST_COMPLETE') != 'true',
+        reason='this test is either time/memory/compute expensive or it depends on resources internal to the Allen Institute. '\
+            'Either way, it does\'nt run by default and must be opted into (it does run in our nightly builds).'
+    )
+
     for item in items:
         if 'requires_api_endpoint' in item.keywords:
             item.add_marker(skip_api_endpoint_test)
+
+        if 'nightly' in item.keywords:
+            item.add_marker(skip_nightly_test)
