08398a623c612d44134e0a7fa09b54bbc8f5779b
nicain <nicain.seattle@gmail.com>
2019-02-07 10:56:21 -0800

    addressed @NileGraddis review request, and added a test to exercise class correct and incorrect behavior

--- allensdk/experimental/lazy_property.py
+++ allensdk/experimental/lazy_property.py
@@ -9,7 +9,7 @@ class LazyProperty(object):
             return self
 
         if not hasattr(obj.api, self.getter_name):
-            return None
+            raise NotImplementedError("API class {} does not support {}".format(obj.api.__class__.__name__, self.getter_name))
 
         if not hasattr(obj, self.hidden_attr):
             setattr(obj, self.hidden_attr, self.calculate(obj))
@@ -23,3 +23,36 @@ class LazyProperty(object):
 
     def calculate(self, obj):
         return getattr(obj.api, self.getter_name)(obj)
+
+def test_lazy_property():
+
+    invalid_api_method_name = 'get_y'
+
+    class TmpAPI(object):
+
+        def __init__(self, x):
+            self.x = x
+
+        def get_x(self, *args, **kwargs):
+            return self.x
+
+    class Foo(object):
+
+        x = LazyProperty('get_x')
+        y = LazyProperty(invalid_api_method_name)
+
+        def __init__(self, api):
+            self.api = api
+
+    x = 5
+    api = TmpAPI(x)
+    foo = Foo(api)
+    assert foo.x == x
+
+    try:
+        _ = foo.y
+    except NotImplementedError as e:
+        assert str(e) == 'API class TmpAPI does not support {}'.format(invalid_api_method_name)
+
+if __name__ == "__main__":
+    test_lazy_property()
