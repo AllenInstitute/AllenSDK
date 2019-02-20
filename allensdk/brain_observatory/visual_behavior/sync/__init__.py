134fd36a9f20045424ccfe8293ca09a5452335a3
nicain <nicain.seattle@gmail.com>
2019-02-20 15:23:49 -0800

    WIP completed max_projection, stimulus_timestamps, ophys_timestamps, metadata

---
+++ allensdk/brain_observatory/visual_behavior/sync/__init__.py
@@ -0,0 +1,81 @@
+"""
+Created on Sunday July 15 2018
+
+@author: marinag
+"""
+
+from .process_sync import filter_digital, calculate_delay  # NOQA: E402
+from .sync_dataset import Dataset as SyncDataset  # NOQA: E402
+
+def get_sync_data(sync_path, use_acq_trigger=False):
+    
+    # use_acq_trigger = lims_data.filter_solenoid_2P6
+
+    # sync_path = lims_data.sync_file
+
+
+    sync_dataset = SyncDataset(sync_path)
+    meta_data = sync_dataset.meta_data
+    sample_freq = meta_data['ni_daq']['counter_output_freq']
+    # 2P vsyncs
+    vs2p_r = sync_dataset.get_rising_edges('2p_vsync')
+    vs2p_f = sync_dataset.get_falling_edges(
+        '2p_vsync', )  # new sync may be able to do units = 'sec', so conversion can be skipped
+    vs2p_rsec = vs2p_r / sample_freq
+    vs2p_fsec = vs2p_f / sample_freq
+    if use_acq_trigger:  # if 2P6, filter out solenoid artifacts
+        vs2p_r_filtered, vs2p_f_filtered = filter_digital(vs2p_rsec, vs2p_fsec, threshold=0.01)
+        frames_2p = vs2p_r_filtered
+    else:  # dont need to filter out artifacts in pipeline data
+        frames_2p = vs2p_rsec
+    # use rising edge for Scientifica, falling edge for Nikon http://confluence.corp.alleninstitute.org/display/IT/Ophys+Time+Sync
+    # Convert to seconds - skip if using units in get_falling_edges, otherwise convert before doing filter digital
+    # vs2p_rsec = vs2p_r / sample_freq
+    # frames_2p = vs2p_rsec
+    # stimulus vsyncs
+    # vs_r = d.get_rising_edges('stim_vsync')
+    vs_f = sync_dataset.get_falling_edges('stim_vsync')
+    # convert to seconds
+    # vs_r_sec = vs_r / sample_freq
+    vs_f_sec = vs_f / sample_freq
+    # vsyncs = vs_f_sec
+    # add display lag
+    monitor_delay = calculate_delay(sync_dataset, vs_f_sec, sample_freq)
+    vsyncs = vs_f_sec + monitor_delay  # this should be added, right!?
+    # line labels are different on 2P6 and production rigs - need options for both
+    if 'lick_times' in meta_data['line_labels']:
+        lick_times = sync_dataset.get_rising_edges('lick_1') / sample_freq
+    elif 'lick_sensor' in meta_data['line_labels']:
+        lick_times = sync_dataset.get_rising_edges('lick_sensor') / sample_freq
+    else:
+        lick_times = None
+    if '2p_trigger' in meta_data['line_labels']:
+        trigger = sync_dataset.get_rising_edges('2p_trigger') / sample_freq
+    elif 'acq_trigger' in meta_data['line_labels']:
+        trigger = sync_dataset.get_rising_edges('acq_trigger') / sample_freq
+    if 'stim_photodiode' in meta_data['line_labels']:
+        stim_photodiode = sync_dataset.get_rising_edges('stim_photodiode') / sample_freq
+    elif 'photodiode' in meta_data['line_labels']:
+        stim_photodiode = sync_dataset.get_rising_edges('photodiode') / sample_freq
+    if 'cam1_exposure' in meta_data['line_labels']:
+        eye_tracking = sync_dataset.get_rising_edges('cam1_exposure') / sample_freq
+    elif 'eye_tracking' in meta_data['line_labels']:
+        eye_tracking = sync_dataset.get_rising_edges('eye_tracking') / sample_freq
+    if 'cam2_exposure' in meta_data['line_labels']:
+        behavior_monitoring = sync_dataset.get_rising_edges('cam2_exposure') / sample_freq
+    elif 'behavior_monitoring' in meta_data['line_labels']:
+        behavior_monitoring = sync_dataset.get_rising_edges('behavior_monitoring') / sample_freq
+    # some experiments have 2P frames prior to stimulus start - restrict to timestamps after trigger for 2P6 only
+    if use_acq_trigger:
+        frames_2p = frames_2p[frames_2p > trigger[0]]
+
+    sync_data = {'ophys_frames': frames_2p,
+                 'stimulus_frames': vsyncs,
+                 'lick_times': lick_times,
+                 'ophys_trigger': trigger,
+                 'eye_tracking': eye_tracking,
+                 'behavior_monitoring': behavior_monitoring,
+                 'stim_photodiode': stim_photodiode,
+                 }
+
+    return sync_data
--- allensdk/internal/api/ophys_lims_api.py
+++ allensdk/internal/api/ophys_lims_api.py
@@ -1,12 +1,17 @@
-import matplotlib.image as mpimg  # NOQA: E402
+import matplotlib.image as mpimg  # NOQA: D102
 
 from . import PostgresQueryMixin
 from allensdk.api.cache import memoize
 
 class OphysLimsApi(PostgresQueryMixin):
+    ''' hello
+    '''
 
     @memoize
     def get_ophys_experiment_dir(self, ophys_experiment_id=None):
+        '''
+        '''
+
         query = '''
                 SELECT oe.storage_directory
                 FROM ophys_experiments oe
@@ -50,7 +55,124 @@ class OphysLimsApi(PostgresQueryMixin):
         return self.fetchone(query, strict=True)
 
     @memoize
-    def get_max_projection(self, ophys_experiment_id):
+    def get_max_projection(self, ophys_experiment_id=None):
         maxInt_a13_file = self.get_maxint_file(ophys_experiment_id=ophys_experiment_id)
         max_projection = mpimg.imread(maxInt_a13_file)
         return max_projection
+
+
+    @memoize
+    def get_sync_file(self, ophys_experiment_id=None):
+        query = '''
+                SELECT sync.storage_directory || sync.filename AS sync_file
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                LEFT JOIN well_known_files sync ON sync.attachable_id=os.id AND sync.attachable_type = 'OphysSession' AND sync.well_known_file_type_id IN (SELECT id FROM well_known_file_types WHERE name = 'OphysRigSync')
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
+
+
+    @memoize
+    def get_targeted_structure(self, ophys_experiment_id=None):
+        query = '''
+                SELECT st.acronym
+                FROM ophys_experiments oe
+                LEFT JOIN structures st ON st.id=oe.targeted_structure_id
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
+
+
+    @memoize
+    def get_imaging_depth(self, ophys_experiment_id=None):
+        query = '''
+                SELECT id.depth
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                LEFT JOIN imaging_depths id ON id.id=os.imaging_depth_id
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
+
+
+    @memoize
+    def get_stimulus_name(self, ophys_experiment_id=None):
+        query = '''
+                SELECT os.stimulus_name
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
+
+
+    @memoize
+    def get_experiment_date(self, ophys_experiment_id=None):
+        query = '''
+                SELECT os.date_of_acquisition
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
+
+
+    @memoize
+    def get_reporter_line(self, ophys_experiment_id=None):
+        query = '''
+                SELECT g.name as reporter_line
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                JOIN specimens sp ON sp.id=os.specimen_id
+                JOIN donors d ON d.id=sp.donor_id
+                JOIN donors_genotypes dg ON dg.donor_id=d.id
+                JOIN genotypes g ON g.id=dg.genotype_id
+                JOIN genotype_types gt ON gt.id=g.genotype_type_id AND gt.name = 'reporter'
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
+
+
+    @memoize
+    def get_driver_line(self, ophys_experiment_id=None):
+        query = '''
+                SELECT g.name as driver_line
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                JOIN specimens sp ON sp.id=os.specimen_id
+                JOIN donors d ON d.id=sp.donor_id
+                JOIN donors_genotypes dg ON dg.donor_id=d.id
+                JOIN genotypes g ON g.id=dg.genotype_id
+                JOIN genotype_types gt ON gt.id=g.genotype_type_id AND gt.name = 'driver'
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)
+        result = self.fetchall(query)
+        if result is None or len(result) < 1:
+            raise OneOrMoreResultExpectedError('Expected one or more, but received: {} results form query'.format(x))
+        return result
+
+
+    @memoize
+    def get_LabTracks_ID(self, ophys_experiment_id=None):
+        query = '''
+                SELECT sp.external_specimen_name
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                JOIN specimens sp ON sp.id=os.specimen_id
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
+
+
+    @memoize
+    def get_full_genotype(self, ophys_experiment_id=None):
+        query = '''
+                SELECT d.full_genotype
+                FROM ophys_experiments oe
+                JOIN ophys_sessions os ON oe.ophys_session_id = os.id
+                JOIN specimens sp ON sp.id=os.specimen_id
+                JOIN donors d ON d.id=sp.donor_id
+                WHERE oe.id= {};
+                '''.format(ophys_experiment_id)        
+        return self.fetchone(query, strict=True)
