df8ddf23624339daaa14fcb1beb339357de840ec
nicain <nicain.seattle@gmail.com>
2019-03-18 16:11:53 -0700

    Nwbapi base (#457)
    * base class for nwb apis
    
    * flake8
    
    * review feedback

--- allensdk/brain_observatory/ecephys/ecephys_api/ecephys_nwb_api.py
+++ allensdk/brain_observatory/ecephys/ecephys_api/ecephys_nwb_api.py
@@ -7,33 +7,17 @@ import pynwb
 
 from .ecephys_api import EcephysApi
 from ...running_speed import RunningSpeed
+from allensdk.brain_observatory.nwb.nwb_api import NwbApi
 
 
-
-class EcephysNwbApi(EcephysApi):
-
-    __slots__ = ('path', '_nwbfile')
-
-    @property
-    def nwbfile(self):
-        if hasattr(self, '_nwbfile'):
-            return self._nwbfile
-
-        io = pynwb.NWBHDF5IO(self.path, 'r')
-        return io.read()
-
-    def __init__(self, path, **kwargs):
-        ''' Reads data for a single Extracellular Electrophysiology session from an NWB 2.0 file
-        '''
-
-        self.path = path
+class EcephysNwbApi(NwbApi, EcephysApi):
 
     def get_running_speed(self) -> RunningSpeed:
         return RunningSpeed(
             timestamps=self.nwbfile.get_acquisition('running_speed').timestamps[:],
             values=self.nwbfile.get_acquisition('running_speed').data[:]
         )
-    
+
     def get_stimulus_table(self) -> pd.DataFrame:
         table = pd.DataFrame({
             col.name: col.data for col in self.nwbfile.epochs.columns 
@@ -41,8 +25,7 @@ class EcephysNwbApi(EcephysApi):
         }, index=pd.Index(name=self.nwbfile.epochs.id.name, data=self.nwbfile.epochs.id.data))
         table.index = table.index.astype(int)
         return table
-    
-    
+
     def get_probes(self) -> pd.DataFrame:
         probes: Union[List, pd.DataFrame] = []
         for k, v in self.nwbfile.electrode_groups.items():
@@ -50,12 +33,12 @@ class EcephysNwbApi(EcephysApi):
         probes = pd.DataFrame(probes)
         probes = probes.set_index(keys='id', drop=True)
         return probes
-    
+
     def get_channels(self) -> pd.DataFrame:
         channels = self.nwbfile.electrodes.to_dataframe()
         channels.drop(columns='group', inplace=True)
         return channels
-    
+
     def get_mean_waveforms(self) -> Dict[int, np.ndarray]:
         units_table = self._get_full_units_table()
         return units_table['waveform_mean'].to_dict()
@@ -63,7 +46,7 @@ class EcephysNwbApi(EcephysApi):
     def get_spike_times(self) -> Dict[int, np.ndarray]:
         units_table = self._get_full_units_table()
         return units_table['spike_times'].to_dict()
-    
+
     def get_units(self) -> pd.DataFrame:
         units_table = self._get_full_units_table()
         units_table.drop(columns=['spike_times', 'waveform_mean'], inplace=True)
@@ -74,9 +57,3 @@ class EcephysNwbApi(EcephysApi):
         table = self.nwbfile.units.to_dataframe()
         table.index = table.index.astype(int)
         return table
-
-    @classmethod
-    def from_nwbfile(cls, nwbfile, **kwargs):
-        obj = cls(path=None, **kwargs)
-        obj._nwbfile = nwbfile
-        return obj
