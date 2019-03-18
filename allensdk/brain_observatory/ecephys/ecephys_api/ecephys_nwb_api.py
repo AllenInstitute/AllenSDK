ece045d3a0c7debaa06d36602852bf1589f45fc2
nicholasc <nicain.seattle@gmail.com>
2019-03-18 14:58:45 -0700

    flake8

--- allensdk/brain_observatory/ecephys/ecephys_api/ecephys_nwb_api.py
+++ allensdk/brain_observatory/ecephys/ecephys_api/ecephys_nwb_api.py
@@ -17,7 +17,7 @@ class EcephysNwbApi(NwbApi, EcephysApi):
             timestamps=self.nwbfile.get_acquisition('running_speed').timestamps[:],
             values=self.nwbfile.get_acquisition('running_speed').data[:]
         )
-    
+
     def get_stimulus_table(self) -> pd.DataFrame:
         table = pd.DataFrame({
             col.name: col.data for col in self.nwbfile.epochs.columns 
@@ -25,8 +25,7 @@ class EcephysNwbApi(NwbApi, EcephysApi):
         }, index=pd.Index(name=self.nwbfile.epochs.id.name, data=self.nwbfile.epochs.id.data))
         table.index = table.index.astype(int)
         return table
-    
-    
+
     def get_probes(self) -> pd.DataFrame:
         probes: Union[List, pd.DataFrame] = []
         for k, v in self.nwbfile.electrode_groups.items():
@@ -34,12 +33,12 @@ class EcephysNwbApi(NwbApi, EcephysApi):
         probes = pd.DataFrame(probes)
         probes = probes.set_index(keys='id', drop=True)
         return probes
-    
+
     def get_channels(self) -> pd.DataFrame:
         channels = self.nwbfile.electrodes.to_dataframe()
         channels.drop(columns='group', inplace=True)
         return channels
-    
+
     def get_mean_waveforms(self) -> Dict[int, np.ndarray]:
         units_table = self._get_full_units_table()
         return units_table['waveform_mean'].to_dict()
@@ -47,7 +46,7 @@ class EcephysNwbApi(NwbApi, EcephysApi):
     def get_spike_times(self) -> Dict[int, np.ndarray]:
         units_table = self._get_full_units_table()
         return units_table['spike_times'].to_dict()
-    
+
     def get_units(self) -> pd.DataFrame:
         units_table = self._get_full_units_table()
         units_table.drop(columns=['spike_times', 'waveform_mean'], inplace=True)
