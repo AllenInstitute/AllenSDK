a037e05406083d94e4eb65e169c445f2c1db5aaa
Doug Ollerenshaw <d.ollerenshaw@gmail.com>
2019-03-19 14:23:23 -0700

    tests on behavior_api functions

---
+++ allensdk/test/internal/test_behavior_api.py
@@ -0,0 +1,26 @@
+import pytest
+
+from allensdk.internal.api import OneResultExpectedError
+from allensdk.internal.api.behavior_api import BehaviorApi
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize('behavior_session_uuid', [
+    pytest.param('394a910e-94c7-4472-9838-5345aff59ed8'),
+])
+def test_foraging_id_to_behavior_session_id(behavior_session_uuid):
+    api = BehaviorApi()
+    behavior_session_id = api.foraging_id_to_behavior_session_id(
+        behavior_session_uuid)
+    assert behavior_session_id == 823847007
+
+
+@pytest.mark.nightly
+@pytest.mark.parametrize('behavior_session_uuid', [
+    pytest.param(823847007),
+])
+def test_behavior_session_id_to_foraging_id(behavior_session_uuid):
+    api = BehaviorApi()
+    foraging_id = api.behavior_session_id_to_foraging_id(
+        behavior_session_uuid)
+    assert foraging_id == '394a910e-94c7-4472-9838-5345aff59ed8'
