5885b8ed21c2db7f5a4d43eb0fccfc85e4b2938f
nilegraddis <nilegraddis@gmail.com>
2019-03-22 16:12:52 -0700

    #503 allensdk deploy script for lims

---
+++ .bamboo/deploy_allensdk.sh
@@ -0,0 +1,33 @@
+set -eu
+
+export HOME=${bamboo_PRODUCTION_HOME}
+export TMPDIR=${bamboo_PRODUCTION_HOME}/tmp
+export PATH=/allen/aibs/technology/conda/shared/miniconda/bin:$PATH
+
+export CONDA_PATH_BACKUP=${CONDA_PATH_BACKUP:-$PATH}
+export CONDA_PREFIX=${CONDA_PREFIX:-}
+export CONDA_PS1_BACKUP=${CONDA_PS1_BACKUP:-}
+
+export PYTHON_VERSION=${bamboo_PYTHON_VERSION:-"3.6"}
+
+NUM_EXISTING_ENVS=`conda env list | grep ${bamboo_NEXT_PRODUCTION_ENVIRONMENT} | wc -l`
+if [ ${NUM_EXISTING_ENVS} != 0 ] ; then
+    conda remove -y -${bamboo_VERBOSITY} --prefix ${bamboo_NEXT_PRODUCTION_ENVIRONMENT} --all
+fi
+conda create -y -${bamboo_VERBOSITY} --prefix ${bamboo_NEXT_PRODUCTION_ENVIRONMENT} python=$PYTHON_VERSION
+
+source activate ${bamboo_NEXT_PRODUCTION_ENVIRONMENT}
+
+# needs mpicc to compile with pip
+conda install -y mpi4py 
+conda install -y -c conda-forge opencv
+
+pip install -e .
+pip install -r internal_requirements.txt
+
+files=( artifacts/*.whl )
+pip install ${files[0]}
+
+source deactivate
+
+chmod -R 777 ${bamboo_NEXT_PRODUCTION_ENVIRONMENT}
