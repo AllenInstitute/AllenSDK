a7536b94fdab76a055b161a038838c7ae4437104
nilegraddis <nilegraddis@gmail.com>
2019-04-02 10:50:17 -0700

    #537 set a sensible minimum spontaneous activity duration

--- allensdk/brain_observatory/ecephys/stimulus_table/_schemas.py
+++ allensdk/brain_observatory/ecephys/stimulus_table/_schemas.py
@@ -7,7 +7,7 @@ class InputParameters(ArgSchema):
     stimulus_pkl_path = String(required=True, help="path to pkl file containing raw stimulus information")
     sync_h5_path = String(required=True, help="path to h5 file containing syncronization information")
     output_stimulus_table_path = String(required=True, help="the output stimulus table csv will be written here")
-    minimum_spontaneous_activity_duration = Float(default=0.0, help="detected spontaneous activity sweeps will be refected if they last fewer that this many seconds")
+    minimum_spontaneous_activity_duration = Float(default=-1.0, help="detected spontaneous activity sweeps will be rejected if they last fewer that this many seconds")
     maximum_expected_spontanous_activity_duration = Float(default=1225.02541, help='validation will fail if a spontanous activity epoch longer than this one is computed.')
     frame_time_strategy = String(default='use_photodiode', 
         help='technique used to align frame times. Options are \'use_photodiode\', which interpolates frame times between photodiode edge times (preferred when vsync times are unreliable) and \'use_vsyncs\', which is preferred when reliable vsync times are available.'
