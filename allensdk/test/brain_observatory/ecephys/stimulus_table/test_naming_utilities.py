57e5bb8cfb8e13942585952192cb9ab2d1b924b4
nile graddis <nilegraddis@gmail.com>
2019-04-04 17:52:33 -0700

    #537 implement function to add movie number to natural_movie_shuffled

--- allensdk/test/brain_observatory/ecephys/stimulus_table/test_naming_utilities.py
+++ allensdk/test/brain_observatory/ecephys/stimulus_table/test_naming_utilities.py
@@ -1,7 +1,20 @@
 import pytest
 import pandas as pd
 
-from allensdk.brain_observatory.ecephys.stimulus_table.naming_utilities as nu
+from allensdk.brain_observatory.ecephys.stimulus_table import naming_utilities as nu
 
 
-def test_
+@pytest.mark.parametrize('table,expected', [
+    [
+        pd.DataFrame({'stimulus_name': ['natural_movie_four_more_repeats', 'natural_movie_four', 'natural_movie_shuffled']}),
+        pd.DataFrame({'stimulus_name': ['natural_movie_four_more_repeats', 'natural_movie_four', 'natural_movie_four_shuffled']})
+    ],
+    [
+        pd.DataFrame({'stimulus_name': ['natural_movie_four_more_repeats', 'natural_movie_four']}),
+        pd.DataFrame({'stimulus_name': ['natural_movie_four_more_repeats', 'natural_movie_four']})
+    ]
+])
+def test_add_number_to_shuffled_movie(table, expected):
+
+    obtained = nu.add_number_to_shuffled_movie(table)
+    pd.testing.assert_frame_equal(expected, obtained, check_like=True)
