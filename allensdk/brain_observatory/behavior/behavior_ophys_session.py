5247213fc7508d7a6c54ffecddbde300008a4b70
nicholasc <nicain.seattle@gmail.com>
2019-04-10 18:14:28 -0700

    WIP more sessions

--- allensdk/brain_observatory/behavior/behavior_ophys_session.py
+++ allensdk/brain_observatory/behavior/behavior_ophys_session.py
@@ -42,16 +42,26 @@ if __name__ == "__main__":
 
     from allensdk.brain_observatory.behavior.behavior_ophys_api.behavior_ophys_nwb_api import BehaviorOphysNwbApi
 
-    # api_list = []
-    # df = BehaviorOphysLimsApi.get_ophys_experiment_df()
-    # for cid in [791352433, 814796698, 814796612, 814796558, 814797528]:
-    #     df2 = df[(df['container_id'] == cid) & (df['workflow_state'] == 'passed')]
-    #     api_list += [BehaviorOphysLimsApi(oeid) for oeid in df2['ophys_experiment_id'].values]
+    blacklist = []
+    df = BehaviorOphysLimsApi.get_ophys_experiment_df()
+    for cid in [791352433, 814796698, 814796612, 814796558, 814797528]:
+        df2 = df[(df['container_id'] == cid) & (df['workflow_state'] == 'passed')]
+        api_list = [BehaviorOphysLimsApi(oeid) for oeid in df2['ophys_experiment_id'].values]
 
-    # for api in api_list:
-    #     session = BehaviorOphysSession(api=api)
-    #     print(session.task_parameters)
-    #     print(session.running_speed)
+        # print()
+        # print('cid', cid)
+        for api in api_list:
+            session = BehaviorOphysSession(api=api)
+            try:
+                session.task_parameters
+                # print('    ', api.get_ophys_experiment_id(), )
+            except KeyError:
+                # print('    ', api.get_ophys_experiment_id(), 'fail')
+                blacklist.append(api.get_ophys_experiment_id())
+    
+    print(blacklist)
+
+        # print(session.running_speed)
 
 
     # nwb_filepath = '/home/nicholasc/projects/allensdk/tmp.nwb'
