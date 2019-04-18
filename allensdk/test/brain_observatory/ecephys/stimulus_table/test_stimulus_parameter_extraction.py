22335a93893e3c1507969a5d2b5d2be0e617a1a3
nile graddis <nilegraddis@gmail.com>
2019-04-18 13:11:45 -0700

    #537 implement test for extract_const_params_from_stim_repr

--- allensdk/test/brain_observatory/ecephys/stimulus_table/test_stimulus_parameter_extraction.py
+++ allensdk/test/brain_observatory/ecephys/stimulus_table/test_stimulus_parameter_extraction.py
@@ -3,3 +3,22 @@ import pytest
 from allensdk.brain_observatory.ecephys.stimulus_table import (
     stimulus_parameter_extraction as spe,
 )
+
+
+def test_extract_const_params_from_stim_repr():
+
+    stim_repr = "GratingStim(autoDraw=False, autoLog=True, color=array([1., 1., 1.]), colorSpace='rgb', contrast=0.8, depth=0)"
+    expected = {
+        "autoDraw": False,
+        "autoLog": True,
+        "color": [1.0, 1.0, 1.0],
+        "colorSpace": "rgb",
+        "contrast": 0.8,
+        "depth": 0,
+    }
+
+    obtained = spe.extract_const_params_from_stim_repr(stim_repr)
+
+    assert len(expected) == len(obtained)
+    for key in obtained:
+        assert expected[key] == obtained[key]
