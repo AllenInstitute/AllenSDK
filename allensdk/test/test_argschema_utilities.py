7117357601c4d158dc2cde8b4f78d132e013bef9
nicholasc <nicain.seattle@gmail.com>
2019-05-30 12:15:56 -0700

    catches all four cases of permissions, exisitng, and not exisitin files and directories

--- allensdk/test/test_argschema_utilities.py
+++ allensdk/test/test_argschema_utilities.py
@@ -1,34 +1,52 @@
 import pytest
 from marshmallow import ValidationError
 import os
+import stat
 
 from allensdk.brain_observatory.argschema_utilities import check_write_access #, check_write_access_overwrite
+
 
 def write_some_text(path):
     with open(path, 'w') as fil:
         fil.write('some_text')
 
-def existing_file(tmpdir):
-    path = os.path.join(tmpdir, 'parent', 'foo.txt')
+
+def existing_file(tmpdir_factory, filename='existing_file.txt'):
+    base_dir = str(tmpdir_factory.mktemp('HW'))
+    path = os.path.join(base_dir, 'parent', filename)
     os.makedirs(os.path.dirname(path))
     write_some_text(path)
     return path
 
-def nonexistent_file(tmpdir):
-    return os.path.join(tmpdir, 'parent', 'foo.txt')
+
+def file_in_bad_permissions_dir(tmpdir_factory):
+    base_dir = str(tmpdir_factory.mktemp('HW'))
+    os.chmod(base_dir, stat.S_IREAD)
+    return os.path.join(base_dir, 'parent_doesnt_have_permission_file.txt')
+
+
+def file_in_bad_permissions_middle_dir(tmpdir_factory):
+    base_dir = str(tmpdir_factory.mktemp('HW'))
+    os.chmod(base_dir, stat.S_IREAD)
+    return os.path.join(base_dir, 'doesnt_exist', 'parent_doesnt_have_permission_file.txt')
+
+
+def nonexistent_file(tmpdir_factory):
+    base_dir = str(tmpdir_factory.mktemp('HW'))
+    return os.path.join(base_dir, 'parent', 'nonexistent_file.txt')
 
 
 @pytest.mark.parametrize('setup,raises', [
     [existing_file, True],
-    [nonexistent_file, False]
+    [nonexistent_file, False],
+    [file_in_bad_permissions_dir, True],
+    [file_in_bad_permissions_middle_dir, True]
 ])
 def test_check_write_access(tmpdir_factory, setup, raises):
 
-    # Definitely exists: base_dir
-    base_dir = str(tmpdir_factory.mktemp('HW'))
-
+    testpath = setup(tmpdir_factory)
     if raises:
         with pytest.raises(ValidationError):
-            check_write_access(setup(base_dir))
+            check_write_access(testpath)
     else:
-        setup(base_dir)
+        assert check_write_access(testpath)
