291fb4d748189a1c91772983a788acdcc632293e
nile graddis <nilegraddis@gmail.com>
2019-06-06 13:06:30 -0700

    #522 apply black linter to ecephys_project api work

--- allensdk/test/brain_observatory/ecephys/test_ecephys_project_lims_api.py
+++ allensdk/test/brain_observatory/ecephys/test_ecephys_project_lims_api.py
@@ -9,83 +9,85 @@ from allensdk.brain_observatory.ecephys.ecephys_project_api import (
 )
 
 
-@pytest.mark.parametrize('method,conditions,expected_query', [
-    [   
-        "get_sessions",
-        {"published": True}, 
-        (
-        "select es.* from ecephys_sessions es "
-        "join projects pr on pr.id = es.project_id "
-        "where true "
-        "and es.workflow_state in ('uploaded') "
-        "and es.habituation = false "
-        "and es.published_at is not null "
-        "and pr.name in ('BrainTV Neuropixels Visual Behavior','BrainTV Neuropixels Visual Coding')"
-        )
+@pytest.mark.parametrize(
+    "method,conditions,expected_query",
+    [
+        [
+            "get_sessions",
+            {"published": True},
+            (
+                "select es.* from ecephys_sessions es "
+                "join projects pr on pr.id = es.project_id "
+                "where true "
+                "and es.workflow_state in ('uploaded') "
+                "and es.habituation = false "
+                "and es.published_at is not null "
+                "and pr.name in ('BrainTV Neuropixels Visual Behavior','BrainTV Neuropixels Visual Coding')"
+            ),
+        ],
+        [
+            "get_sessions",
+            {"session_ids": [1, 2, 3]},
+            (
+                "select es.* from ecephys_sessions es "
+                "join projects pr on pr.id = es.project_id "
+                "where true "
+                "and es.id in (1,2,3) "
+                "and es.workflow_state in ('uploaded') "
+                "and es.habituation = false "
+                "and pr.name in ('BrainTV Neuropixels Visual Behavior','BrainTV Neuropixels Visual Coding')"
+            ),
+        ],
+        [
+            "get_units",
+            {"session_ids": [1, 2, 3]},
+            (
+                "select eu.* from ecephys_units eu "
+                "join ecephys_channels ec on ec.id = eu.ecephys_channel_id "
+                "join ecephys_probes ep on ep.id = ec.ecephys_probe_id "
+                "join ecephys_sessions es on es.id = ep.ecephys_session_id "
+                "where true "
+                "and es.id in (1,2,3)"
+            ),
+        ],
+        [
+            "get_units",
+            {"session_ids": [1, 2, 3], "unit_ids": (4, 5, 6)},
+            (
+                "select eu.* from ecephys_units eu "
+                "join ecephys_channels ec on ec.id = eu.ecephys_channel_id "
+                "join ecephys_probes ep on ep.id = ec.ecephys_probe_id "
+                "join ecephys_sessions es on es.id = ep.ecephys_session_id "
+                "where true "
+                "and eu.id in (4,5,6) "
+                "and es.id in (1,2,3)"
+            ),
+        ],
+        [
+            "get_channels",
+            {},
+            (
+                "select ec.* from ecephys_channels ec "
+                "join ecephys_probes ep on ep.id = ec.ecephys_probe_id "
+                "join ecephys_sessions es on es.id = ep.ecephys_session_id "
+                "where true "
+            ),
+        ],
+        [
+            "get_probes",
+            {},
+            (
+                "select ep.* from ecephys_probes ep "
+                "join ecephys_sessions es on es.id = ep.ecephys_session_id "
+                "where true "
+            ),
+        ],
     ],
-    [   
-        "get_sessions",
-        {"session_ids": [1, 2, 3]}, 
-        (
-        "select es.* from ecephys_sessions es "
-        "join projects pr on pr.id = es.project_id "
-        "where true "
-        "and es.id in (1,2,3) "
-        "and es.workflow_state in ('uploaded') "
-        "and es.habituation = false "
-        "and pr.name in ('BrainTV Neuropixels Visual Behavior','BrainTV Neuropixels Visual Coding')"
-        )
-    ],
-    [   
-        "get_units",
-        {"session_ids": [1, 2, 3]}, 
-        (
-        "select eu.* from ecephys_units eu "
-        "join ecephys_channels ec on ec.id = eu.ecephys_channel_id "
-        "join ecephys_probes ep on ep.id = ec.ecephys_probe_id "
-        "join ecephys_sessions es on es.id = ep.ecephys_session_id "
-        "where true "
-        "and es.id in (1,2,3)"
-        )
-    ],
-    [   
-        "get_units",
-        {"session_ids": [1, 2, 3], "unit_ids": (4, 5, 6)}, 
-        (
-        "select eu.* from ecephys_units eu "
-        "join ecephys_channels ec on ec.id = eu.ecephys_channel_id "
-        "join ecephys_probes ep on ep.id = ec.ecephys_probe_id "
-        "join ecephys_sessions es on es.id = ep.ecephys_session_id "
-        "where true "
-        "and eu.id in (4,5,6) "
-        "and es.id in (1,2,3)"
-        )
-    ],
-    [   
-        "get_channels",
-        {}, 
-        (
-        "select ec.* from ecephys_channels ec "
-        "join ecephys_probes ep on ep.id = ec.ecephys_probe_id "
-        "join ecephys_sessions es on es.id = ep.ecephys_session_id "
-        "where true "
-        )
-    ],
-    [   
-        "get_probes",
-        {}, 
-        (
-        "select ep.* from ecephys_probes ep "
-        "join ecephys_sessions es on es.id = ep.ecephys_session_id "
-        "where true "
-        )
-    ],
-])
+)
 def test_query(method, conditions, expected_query):
-
     class MockPgEngine:
         def select(self, rendered):
-            self.query =  " ".join([item.strip() for item in str(rendered).split()])
+            self.query = " ".join([item.strip() for item in str(rendered).split()])
             return pd.DataFrame({})
 
     pg_engine = MockPgEngine()
@@ -93,7 +95,7 @@ def test_query(method, conditions, expected_query):
 
     results = getattr(api, method)(**conditions)
     assert expected_query.strip() == pg_engine.query.strip()
-    
+
 
 def test_get_session_data():
 
@@ -102,15 +104,19 @@ def test_get_session_data():
 
     class MockPgEngine:
         def select(self, rendered):
-            pattern = re.compile(r".*and ear.ecephys_session_id = (?P<session_id>\d+).*", re.DOTALL)
+            pattern = re.compile(
+                r".*and ear.ecephys_session_id = (?P<session_id>\d+).*", re.DOTALL
+            )
             match = pattern.match(rendered)
             sid_obt = int(match["session_id"])
             assert session_id == sid_obt
             return pd.DataFrame({"id": [wkf_id]})
-        
+
     class MockHttpEngine:
         def stream(self, path):
             assert path == f"well_known_files/download/{wkf_id}?wkf_id={wkf_id}"
 
-    api = epla.EcephysProjectLimsApi(postgres_engine=MockPgEngine(), app_engine=MockHttpEngine())
+    api = epla.EcephysProjectLimsApi(
+        postgres_engine=MockPgEngine(), app_engine=MockHttpEngine()
+    )
     api.get_session_data(session_id)
