5c74afce21f543cd33da99dcaff8217e36f26877
nicholasc <nicain.seattle@gmail.com>
2019-06-27 15:02:26 -0700

    GH-731 roll in integration test from visual_behavior_analysis

--- allensdk/brain_observatory/behavior/dprime.py
+++ allensdk/brain_observatory/behavior/dprime.py
@@ -3,39 +3,59 @@ import numpy as np
 from scipy.stats import norm
 
 
-def get_hit_rate(hit=None, miss=None, aborted=None, sliding_window=100):
-
-    assert len(hit) == len(miss)
+def get_go_responses(hit=None, miss=None, aborted=None):
+    assert len(hit) == len(miss) == len(aborted)
     aborted = np.logical_not(np.array(aborted, dtype=np.bool))
     hit = np.array(hit, dtype=np.bool)[aborted]
     miss = np.array(miss, dtype=np.bool)[aborted]
-    hit_responses = np.empty_like(hit, dtype=np.float)
-    hit_responses.fill(float('nan'))
+    go_responses = np.empty_like(hit, dtype=np.float)
+    go_responses.fill(float('nan'))
+    go_responses[hit] = 1
+    go_responses[miss] = 0
+    return go_responses
 
-    hit_responses[hit] = 1
-    hit_responses[miss] = 0
-    hit_rate = pd.Series(hit_responses).rolling(window=sliding_window, min_periods=0).mean().values
-    
+
+def get_hit_rate(hit=None, miss=None, aborted=None, sliding_window=100):
+    go_responses = get_go_responses(hit=hit, miss=miss, aborted=aborted)
+    hit_rate = pd.Series(go_responses).rolling(window=sliding_window, min_periods=0).mean().values
     return hit_rate
 
 
-def get_false_alarm_rate(correct_reject=None, false_alarm=None, aborted=None, sliding_window=100):
+def get_trial_count_corrected_hit_rate(hit=None, miss=None, aborted=None, sliding_window=100):
+    go_responses = get_go_responses(hit=hit, miss=miss, aborted=aborted)
+    go_responses_count = pd.Series(go_responses).rolling(window=sliding_window, min_periods=0).count()
+    hit_rate = pd.Series(go_responses).rolling(window=sliding_window, min_periods=0).mean().values
+    trial_count_corrected_hit_rate = np.vectorize(trial_number_limit)(hit_rate, go_responses_count)
+    return trial_count_corrected_hit_rate
 
-    assert len(correct_reject) == len(false_alarm)
+
+def get_catch_responses(correct_reject=None, false_alarm=None, aborted=None):
+    assert len(correct_reject) == len(false_alarm) == len(aborted)
     aborted = np.logical_not(np.array(aborted, dtype=np.bool))
     correct_reject = np.array(correct_reject, dtype=np.bool)[aborted]
     false_alarm = np.array(false_alarm, dtype=np.bool)[aborted]
-    false_alarm_responses = np.empty_like(correct_reject, dtype=np.float)
-    false_alarm_responses.fill(float('nan'))
+    catch_responses = np.empty_like(correct_reject, dtype=np.float)
+    catch_responses.fill(float('nan'))
+    catch_responses[false_alarm] = 1
+    catch_responses[correct_reject] = 0
+    return catch_responses
 
-    false_alarm_responses[false_alarm] = 1
-    false_alarm_responses[correct_reject] = 0
-    false_alarm_rate = pd.Series(false_alarm_responses).rolling(window=sliding_window, min_periods=0).mean().values
 
+def get_false_alarm_rate(correct_reject=None, false_alarm=None, aborted=None, sliding_window=100):
+    catch_responses = get_catch_responses(correct_reject=correct_reject, false_alarm=false_alarm, aborted=aborted)
+    false_alarm_rate = pd.Series(catch_responses).rolling(window=sliding_window, min_periods=0).mean().values
     return false_alarm_rate
 
 
-def get_dprime_pipeline(hit_rate, fa_rate, limits=(0.01, 0.99)):
+def get_trial_count_corrected_false_alarm_rate(correct_reject=None, false_alarm=None, aborted=None, sliding_window=100):
+    catch_responses = get_catch_responses(correct_reject=correct_reject, false_alarm=false_alarm, aborted=aborted)
+    catch_responses_count = pd.Series(catch_responses).rolling(window=sliding_window, min_periods=0).count()
+    false_alarm_rate = pd.Series(catch_responses).rolling(window=sliding_window, min_periods=0).mean().values
+    trial_count_corrected_false_alarm_rate = np.vectorize(trial_number_limit)(false_alarm_rate, catch_responses_count)
+    return trial_count_corrected_false_alarm_rate
+
+
+def get_dprime(hit_rate, fa_rate, limits=(0.01, 0.99)):
     """ calculates the d-prime for a given hit rate and false alarm rate
     https://en.wikipedia.org/wiki/Sensitivity_index
     Parameters
@@ -82,3 +102,12 @@ def get_dprime_pipeline(hit_rate, fa_rate, limits=(0.01, 0.99)):
         return d_prime[0]
     else:
         return d_prime
+
+
+def trial_number_limit(p, N):
+    if N == 0:
+        return np.nan
+    if not pd.isnull(p):
+        p = np.max((p, 1. / (2 * N)))
+        p = np.min((p, 1 - 1. / (2 * N)))
+    return p
--- allensdk/test/brain_observatory/behavior/test_dprime.py
+++ allensdk/test/brain_observatory/behavior/test_dprime.py
@@ -1,10 +1,90 @@
 import numpy as np
+import pytest
+import pandas as pd
+import datetime
+import pytz
 
-from allensdk.brain_observatory.behavior.dprime import get_hit_rate, get_false_alarm_rate, get_dprime_pipeline
+from allensdk.brain_observatory.behavior.dprime import get_hit_rate, get_false_alarm_rate, get_dprime, get_trial_count_corrected_false_alarm_rate, get_trial_count_corrected_hit_rate
 
 
 NaN = float('nan')
 
+
+@pytest.fixture
+def mock_trials_fixture():
+
+    n_tr = 500
+    np.random.seed(42)
+    change = np.random.random(n_tr) > 0.8
+    incorrect = np.random.random(n_tr) > 0.8
+    detect = change.copy()
+    detect[incorrect] = ~detect[incorrect]
+
+    trials = pd.DataFrame({
+        'change': change,
+        'detect': detect,
+    },)
+    trials['trial_type'] = trials['change'].map(lambda x: ['catch', 'go'][x])
+    trials['response'] = trials['detect']
+    trials['change_time'] = np.sort(np.random.rand(n_tr)) * 3600
+    trials['reward_lick_latency'] = 0.1
+    trials['reward_lick_count'] = 10
+    trials['auto_rewarded'] = False
+    trials['lick_frames'] = [[] for row in trials.iterrows()]
+    trials['trial_length'] = 8.5
+    trials['reward_times'] = trials.apply(lambda r: [r['change_time']+0.2] if r['change']*r['detect'] else [],axis=1)
+    trials['reward_volume'] = 0.005 * trials['reward_times'].map(len)
+    trials['response_latency'] = trials.apply(lambda r: 0.2 if r['detect'] else np.inf,axis=1)
+    trials['blank_duration_range'] = [[0.5, 0.5] for row in trials.iterrows()]
+
+    metadata = {}
+    metadata['mouse_id'] = 'M999999'
+    metadata['user_id'] = 'johnd'
+
+    metadata['startdatetime'] = datetime.datetime(2017, 7, 19, 10, 35, 8, 369000, tzinfo=pytz.utc)
+    metadata['dayofweek'] = metadata['startdatetime'].weekday()
+    metadata['startdatetime'] = metadata['startdatetime']
+
+    metadata['behavior_session_uuid'] = 12345
+    metadata['stage'] = 'test'
+    metadata['stimulus'] = 'natural_scenes'
+    metadata['stimulus_distribution'] = 'exponential'
+
+    for k, v in metadata.items():
+        trials[k] = v
+    return trials
+
+from collections import defaultdict
+
+@pytest.fixture
+def mock_dprime_fixture(mock_trials_fixture):
+
+    data_dict = defaultdict(list)
+    for ri, row in mock_trials_fixture[['trial_type', 'response', 'change_time']].iterrows():
+        assert not pd.isnull(row['change_time'])
+        if row['trial_type'] == 'go' and row['response'] == True:
+            hit = True
+            miss = false_alarm = correct_reject = False
+        elif row['trial_type'] == 'go' and row['response'] == False:
+            miss = True
+            hit = false_alarm = correct_reject = False
+        elif row['trial_type'] == 'catch' and row['response'] == True:
+            false_alarm = True
+            miss = hit = correct_reject = False
+        elif row['trial_type'] == 'catch' and row['response'] == False:
+            correct_reject = True
+            hit = false_alarm = miss = False
+        else:
+            raise RuntimeError
+        data_dict['hit'].append(hit)
+        data_dict['miss'].append(miss)
+        data_dict['false_alarm'].append(false_alarm)
+        data_dict['correct_reject'].append(correct_reject)
+        data_dict['aborted'].append(False)
+
+
+
+    return pd.DataFrame(data_dict)
 
 def test_get_hit_rate():
 
@@ -17,7 +97,7 @@ def test_get_hit_rate():
     np.testing.assert_allclose(result, [0, .5, 1/3, 2/3])
 
 
-def test_get_false_alarm_rate():
+def test_get_false_alarm_rate(mock_trials_fixture):
 
     false_alarm, correct_reject, aborted = (
         [0, 1, 0, 0, 0, 1],
@@ -28,7 +108,7 @@ def test_get_false_alarm_rate():
     np.testing.assert_allclose(result, [0, .5, 1/3, 2/3])
 
 
-def test_dprime():
+def test_dprime_unit():
 
     hit, miss, false_alarm, correct_reject, aborted = (
         [0, 0, 1, 0, 0, 1],
@@ -41,3 +121,37 @@ def test_dprime():
     far = get_false_alarm_rate(false_alarm=false_alarm, correct_reject=correct_reject, aborted=aborted, sliding_window=3)
     result = get_dprime_pipeline(hr, far)
     np.testing.assert_allclose(result, [NaN, NaN, NaN, 2.326348, 4.652696, 4.652696])
+
+import visual_behavior.utilities as vbu
+
+
+def test_dprime_integration_legacy(mock_dprime_fixture):
+    sliding_window = 100
+
+    hit = mock_dprime_fixture.hit
+    miss = mock_dprime_fixture.miss
+    false_alarm = mock_dprime_fixture.false_alarm
+    correct_reject = mock_dprime_fixture.correct_reject
+    aborted = mock_dprime_fixture.aborted
+
+    hr = get_hit_rate(hit=hit, miss=miss, aborted=aborted, sliding_window=sliding_window)
+    cr = get_false_alarm_rate(false_alarm=false_alarm, correct_reject=correct_reject, aborted=aborted, sliding_window=sliding_window)
+    dprime = get_dprime(hr, cr)
+
+    assert dprime[2] == 4.6526957480816815
+
+
+def test_dprime_integration(mock_dprime_fixture):
+    sliding_window = 100
+
+    hit = mock_dprime_fixture.hit
+    miss = mock_dprime_fixture.miss
+    false_alarm = mock_dprime_fixture.false_alarm
+    correct_reject = mock_dprime_fixture.correct_reject
+    aborted = mock_dprime_fixture.aborted
+
+    hr = get_trial_count_corrected_hit_rate(hit=hit, miss=miss, aborted=aborted, sliding_window=sliding_window)
+    cr = get_trial_count_corrected_false_alarm_rate(false_alarm=false_alarm, correct_reject=correct_reject, aborted=aborted, sliding_window=sliding_window)
+    dprime = get_dprime(hr, cr)
+
+    assert dprime[2] == 0.6744897501960817
