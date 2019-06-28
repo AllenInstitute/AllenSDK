5e28951af6953430079dc9b293d7f1e991ee49f4
nicholasc <nicain.seattle@gmail.com>
2019-06-28 16:52:57 -0700

    Mesoscope:2 add basic MesoscopeSession functionality and LIMS api

--- allensdk/internal/api/mesoscope_lims_api.py
+++ allensdk/internal/api/mesoscope_lims_api.py
@@ -11,40 +11,39 @@ from allensdk.api.cache import memoize
 
 logger = logging.getLogger(__name__)
 
-class MesoscopeLimsApi (PostgresQueryMixin) :
+class MesoscopeLimsApi(PostgresQueryMixin):
 
-    def get_mesoscope_session_data(self, session_id):
+    def __init__(self, session_id):
+        self.session_id = session_id
+        super().__init__()
 
-            query = ' '.join((
-                "SELECT oe.id as experiment_id, os.id as session_id",
-                ", os.storage_directory as session_folder, oe.storage_directory as experiment_folder",
-                ", sp.name as specimen",
-                ", os.date_of_acquisition as date",
-                ", imaging_depths.depth as depth",
-                ", st.acronym as structure",
-                ", os.parent_session_id as parent_id",
-                ", oe.workflow_state",
-                ", os.stimulus_name as stimulus",
-                " FROM ophys_experiments oe",
-                "join ophys_sessions os on os.id = oe.ophys_session_id "
-                "join specimens sp on sp.id = os.specimen_id "
-                "join projects p on p.id = os.project_id "
-                "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
-                "join structures st on st.id = oe.targeted_structure_id "
-                "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
-                " and os.id='{}'  ",
-            ))
+    def get_session_id(self):
+        return self.session_id
 
-            lims_data = self.psycopg2_selec(query.format(session_id))
+    def get_metadata(self):
+        raise NotImplementedError
 
-            if lims_data == []:
-                self.data_present = False
-            else:
-                self.data_pointer = lims_data
-                self.data_present = True
-
-
-        return lims_data
+    def get_mesoscope_session_df(self):
+        query = ' '.join((
+            "SELECT oe.id as experiment_id, os.id as session_id",
+            ", os.storage_directory as session_folder, oe.storage_directory as experiment_folder",
+            ", sp.name as specimen",
+            ", os.date_of_acquisition as date",
+            ", imaging_depths.depth as depth",
+            ", st.acronym as structure",
+            ", os.parent_session_id as parent_id",
+            ", oe.workflow_state",
+            ", os.stimulus_name as stimulus",
+            " FROM ophys_experiments oe",
+            "join ophys_sessions os on os.id = oe.ophys_session_id "
+            "join specimens sp on sp.id = os.specimen_id "
+            "join projects p on p.id = os.project_id "
+            "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
+            "join structures st on st.id = oe.targeted_structure_id "
+            "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
+            " and os.id='{}'  ",
+        ))
+        return pd.read_sql(query.format(self.get_session_id()), self.get_connection())
 
     def get_mesoscope_experiment_data(self):
 
