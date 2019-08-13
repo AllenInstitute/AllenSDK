ea39f8dd8fafc2738a5c1cbbc2fddbed704ce385
nilegraddis <nilegraddis@gmail.com>
2019-08-13 16:28:09 -0700

    Bugfix unit access in EcephysProjectCache

--- allensdk/brain_observatory/ecephys/ecephys_project_api/ecephys_project_api.py
+++ allensdk/brain_observatory/ecephys/ecephys_project_api/ecephys_project_api.py
@@ -1,30 +1,30 @@
 class EcephysProjectApi:
-    def get_sessions(self):
+    def get_sessions(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_session_data(self, session_id):
+    def get_session_data(self, session_id, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_targeted_regions(self):
+    def get_targeted_regions(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_isi_experiments(self):
+    def get_isi_experiments(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_units(self):
+    def get_units(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_channels(self):
+    def get_channels(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_probes(self):
+    def get_probes(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_probe_lfp_data(self, probe_id):
+    def get_probe_lfp_data(self, probe_id, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_natural_movie_template(self, number):
+    def get_natural_movie_template(self, number, *args, **kwargs):
         raise NotImplementedError()
 
-    def get_natural_scene_template(self, number):
+    def get_natural_scene_template(self, number, *args, **kwargs):
         raise NotImplementedError()
--- allensdk/brain_observatory/ecephys/ecephys_project_api/ecephys_project_fixed_api.py
+++ allensdk/brain_observatory/ecephys/ecephys_project_api/ecephys_project_fixed_api.py
@@ -7,32 +7,32 @@ class MissingDataError(ValueError):
 
 class EcephysProjectFixedApi(EcephysProjectApi):
 
-    def get_session_data(self, session_id):
+    def get_session_data(self, session_id, *args, **kwargs):
         raise MissingDataError(f"data for session {session_id} not found!")
 
-    def get_probe_lfp_data(self, probe_id):
+    def get_probe_lfp_data(self, probe_id, *args, **kwargs):
         raise MissingDataError(f"lfp data for probe {probe_id} not found!")
 
-    def get_sessions(self):
+    def get_sessions(self, *args, **kwargs):
         raise MissingDataError(f"Data not found!")
 
-    def get_targeted_regions(self):
+    def get_targeted_regions(self, *args, **kwargs):
         raise MissingDataError(f"Data not found!")
 
-    def get_isi_experiments(self):
+    def get_isi_experiments(self, *args, **kwargs):
         raise MissingDataError(f"Data not found!")
 
-    def get_units(self):
+    def get_units(self, *args, **kwargs):
         raise MissingDataError(f"Data not found!")
 
-    def get_channels(self):
+    def get_channels(self, *args, **kwargs):
         raise MissingDataError(f"Data not found!")
 
-    def get_probes(self):
+    def get_probes(self, *args, **kwargs):
         raise MissingDataError(f"Data not found!")
 
-    def get_natural_movie_template(self, number):
+    def get_natural_movie_template(self, number, *args, **kwargs):
         raise MissingDataError(f"natural movie template not found for movie {number}")
 
-    def get_natural_scene_template(self, number):
+    def get_natural_scene_template(self, number, *args, **kwargs):
         raise MissingDataError(f"natural scene template not found for scene {number}")
