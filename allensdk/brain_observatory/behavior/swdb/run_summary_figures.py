0c18303315bda866af459aecb9f68e037a57cdfc
matchings <marinag@alleninstitute.org>
2019-08-13 15:44:49 -0700

    update to analysis_files folder

--- allensdk/brain_observatory/behavior/swdb/run_summary_figures.py
+++ allensdk/brain_observatory/behavior/swdb/run_summary_figures.py
@@ -20,7 +20,7 @@ job_settings = {'queue': 'braintv',
 
 cache_json = {'manifest_path': '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/SWDB_2019/visual_behavior_data_manifest.csv',
               'nwb_base_dir': '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/SWDB_2019/nwb_files',
-              'analysis_files_base_dir': '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/SWDB_2019/extra_files_final',
+              'analysis_files_base_dir': '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/SWDB_2019/analysis_files',
               'analysis_files_metadata_path': '/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/SWDB_2019/analysis_files_metadata.json',
               }
 
