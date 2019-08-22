baca50406f9afd67117fd79374f4a06723647faa
nilegraddis <nilegraddis@gmail.com>
2019-08-22 14:06:16 -0700

    #940 move default optotagging conditions to schemas

--- allensdk/brain_observatory/ecephys/optotagging_table/__main__.py
+++ allensdk/brain_observatory/ecephys/optotagging_table/__main__.py
@@ -9,21 +9,6 @@ from allensdk.brain_observatory.ecephys.file_io.ecephys_sync_dataset import (
 from ._schemas import InputParameters, OutputParameters
 
 
-@dataclass
-class OptoCondition:
-    duration: float
-    name: str
-    description: str
-
-
-kwown_conditions = {
-    0: OptoCondition(duration=1.0, name="fast_pulses", description="2.5 ms pulses at 10 Hz"),
-    1: OptoCondition(duration=0.005, name="single_pulse", description="a single square pulse"),
-    2: OptoCondition(duration=0.01, name="single_pulse", description="a single square pulse"),
-    3: OptoCondition(duration=1.0, name="raised_cosine", description="a cosine wave"),
-}
-
-
 def build_opto_table(args):
 
     opto_file = pd.read_pickle(args['opto_pickle_path'])
@@ -31,26 +16,24 @@ def build_opto_table(args):
 
     optotagging_table = pd.DataFrame({
         'start_time': sync_file.extract_led_times(),
-        'condition': opto_file['opto_conditions'],
+        'condition': [str(item) for item in opto_file['opto_conditions']],
         'level': opto_file['opto_levels']
     })
-    optotagging_table = optotagging_table.sort_values(by='Start', axis=0)
-    
+    optotagging_table = optotagging_table.sort_values(by='start_time', axis=0)    
 
     stop_times = []
     names = []
-    descriptions = []
-    for ii, row in optagging_table.iterrows():
-        condition = known_conditions[row["condition"]]
-        stop_times.append(row["start_time"] + condition.duration)
-        names.append(condition.name)
-        descriptions.append(condition.description)
+    conditions = []
+    for ii, row in optotagging_table.iterrows():
+        condition = args["conditions"][row["condition"]]
+        stop_times.append(row["start_time"] + condition["duration"])
+        names.append(condition["name"])
+        conditions.append(condition["condition"])
 
     optotagging_table["stop_time"] = stop_times
     optotagging_table["name"] = names
-    optotagging_table["description"] = descriptions
+    optotagging_table["condition"] = conditions
     optotagging_table["duration"] = optotagging_table["stop_time"] - optotagging_table["start_time"]
-    optotagging_table.drop(columns="condition", inplace=True)
 
     optotagging_table.to_csv(args['output_opto_table_path'], index=False)
     return {'output_opto_table_path': args['output_opto_table_path']}
