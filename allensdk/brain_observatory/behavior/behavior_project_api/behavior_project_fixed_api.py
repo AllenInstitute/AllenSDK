5245b330294905a0005b800105c5f0fdf353b9ce
Nick Ponvert <nickponvert@gmail.com>
2019-08-30 00:55:52 -0700

    clean up unneded ecephys api methods

--- allensdk/brain_observatory/behavior/behavior_project_api/behavior_project_fixed_api.py
+++ allensdk/brain_observatory/behavior/behavior_project_api/behavior_project_fixed_api.py
@@ -1,9 +1,7 @@
 from allensdk.brain_observatory.behavior.behavior_project_api import BehaviorProjectApi
 
-
 class MissingDataError(ValueError):
     pass
-
 
 class BehaviorProjectFixedApi(BehaviorProjectApi):
 
@@ -21,24 +19,3 @@ class BehaviorProjectFixedApi(BehaviorProjectApi):
 
     def get_sessions(self, *args, **kwargs):
         raise MissingDataError(f"Data not found!")
-
-    def get_targeted_regions(self, *args, **kwargs):
-        raise MissingDataError(f"Data not found!")
-
-    def get_isi_experiments(self, *args, **kwargs):
-        raise MissingDataError(f"Data not found!")
-
-    def get_units(self, *args, **kwargs):
-        raise MissingDataError(f"Data not found!")
-
-    def get_channels(self, *args, **kwargs):
-        raise MissingDataError(f"Data not found!")
-
-    def get_probes(self, *args, **kwargs):
-        raise MissingDataError(f"Data not found!")
-
-    def get_natural_movie_template(self, number, *args, **kwargs):
-        raise MissingDataError(f"natural movie template not found for movie {number}")
-
-    def get_natural_scene_template(self, number, *args, **kwargs):
-        raise MissingDataError(f"natural scene template not found for scene {number}")
