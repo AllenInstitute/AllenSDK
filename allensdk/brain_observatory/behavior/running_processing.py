e6ea929893854889a828a130d61627cd123ba46b
Doug Ollerenshaw <d.ollerenshaw@gmail.com>
2019-09-12 16:22:11 -0700

    make assert more verbose

--- allensdk/brain_observatory/behavior/running_processing.py
+++ allensdk/brain_observatory/behavior/running_processing.py
@@ -2,6 +2,7 @@ import scipy.signal
 import numpy as np
 import pandas as pd
 
+
 def calc_deriv(x, time):
     dx = np.diff(x)
     dt = np.diff(time)
@@ -10,6 +11,7 @@ def calc_deriv(x, time):
     dxdt = np.vstack((dxdt_rt, dxdt_lt))
     dxdt = np.nanmean(dxdt, axis=0)
     return dxdt
+
 
 def deg_to_dist(speed_deg_per_s):
     '''
@@ -23,12 +25,18 @@ def deg_to_dist(speed_deg_per_s):
     running_speed_cm_per_sec = np.pi * speed_deg_per_s * running_radius / 180.
     return running_speed_cm_per_sec
 
+
 def get_running_df(data, time):
     dx_raw = data["items"]["behavior"]["encoders"][0]["dx"]
     v_sig = data["items"]["behavior"]["encoders"][0]["vsig"]
     v_in = data["items"]["behavior"]["encoders"][0]["vin"]
-    assert len(v_in) == len(time)
-    dx = scipy.signal.medfilt(dx_raw, kernel_size=5) # remove big, single frame spikes in encoder values
+    assert len(v_in) == len(time), "length of v_in ({}) must match length of time ({}), they are off by {}".format(
+        len(v_in), 
+        len(time), 
+        abs(len(v_in) - len(time))
+    )
+    # remove big, single frame spikes in encoder values
+    dx = scipy.signal.medfilt(dx_raw, kernel_size=5)
     dx = np.cumsum(dx)  # wheel rotations
     speed = calc_deriv(dx, time)  # speed in degrees/s
     speed = deg_to_dist(speed)
