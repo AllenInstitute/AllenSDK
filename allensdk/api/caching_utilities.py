bbf4cd91c6f54b2e09380897e0bfe5e260fbdeee
nile graddis <NileGraddis@gmail.com>
2019-10-23 17:05:08 -0700

    #1112 implement simple call caching

---
+++ allensdk/api/caching_utilities.py
@@ -0,0 +1,131 @@
+import functools
+from pathlib import Path
+import warnings
+import os
+
+from typing import overload, Callable, Any, Union, Optional, TypeVar
+
+from allensdk.api.cache import Cache
+
+
+P = TypeVar("P")
+Q = TypeVar("Q")
+
+AnyPath = Union[Path, str]
+
+
+def call_caching(
+    fetch: Callable[[], Q],
+    write: Callable[[Q], None],
+    read: Callable[[], P],
+    pre_write: Optional[Callable[[Q], Q]] = None,
+    cleanup: Optional[Callable[[], None]] = None,
+    lazy: bool = True,
+    num_tries: int = 1,
+    failure_message: str = "",
+) -> P:
+    """ Access data, caching on a local store for future accesses.
+
+    Parameters
+    ----------
+    fetch :
+        Function which pulls data from a remote/expensive source.
+    write : 
+        Function which stores data in a local/inexpensive store.
+    read :
+        Function which pulls data from a local/inexpensive store.
+    pre_write :
+        Function applied to obtained data after fetching, but before writing.
+    cleanup :
+        Function for fixing a failed fetch. e.g. unlinking a partially 
+        downloaded file. Exceptions raised by cleanup are not themselves 
+        handled
+    lazy :
+        If True, attempt to read the data from the local/inexpensive store 
+        before fetching it. If False, forcibly fetch from the 
+        remote/expensive store.
+    num_tries :
+        How many fetches to attempt before (re)raising an exception. A fetch 
+        is failed if reading the result raises an exception.
+    failure_message :
+        Provides additional context in the event of a failed download. Emitted 
+        when retrying, and when a fetch failure occurs after tries are 
+        exhausted
+
+    Returns
+    -------
+    The result of calling read
+
+    """
+
+    try:
+        if not lazy:
+            data = fetch()
+            if pre_write is not None:
+                data = pre_write(data)
+            write(data)
+
+        return read()
+
+    except Exception:
+        if cleanup is not None and not lazy:
+            cleanup()
+
+        num_tries -= 1 - lazy  # don't count fetchless reads
+
+        if num_tries <= 0:
+            if failure_message:
+                warnings.warn(failure_message)
+            raise
+
+        retry_message = f"retrying fetch ({num_tries} tries remaining)"
+        if failure_message:
+            retry_message = f"{failure_message} {retry_message}"
+        warnings.warn(retry_message)
+
+        return call_caching(
+            fetch,
+            write,
+            read,
+            pre_write=pre_write,
+            cleanup=cleanup,
+            lazy=False,
+            num_tries=num_tries,
+            failure_message=failure_message,
+        )
+
+
+def one_file_call_caching(
+    path: AnyPath,
+    fetch: Callable[[], Any],
+    write: Callable[[], Any],
+    read: Callable[[AnyPath], P],
+    **kwargs
+) -> P:
+    """ A call_caching variant where the local store is a single file. See 
+    call_caching for complete documentation.
+
+    Parameters
+    ----------
+    path : 
+        Path at which the data will be stored
+
+    """
+
+    def safe_unlink():
+        try:
+            os.unlink(path)
+        except IOError:
+            pass
+
+    write = functools.partial(write, path)
+    read = functools.partial(read, path)
+    cleanup = kwargs.pop("cleanup", safe_unlink)
+
+    return call_caching(
+        fetch,
+        write,
+        read,
+        cleanup=cleanup,
+        **kwargs
+    )
