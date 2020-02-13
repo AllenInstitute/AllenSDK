corrects determinant calculation in morphology.apply_affine() (#1364)
* corrects determinant calculation in morphology.apply_affine()

* fixes indexing

* adds test for apply_affine

allensdk/internal/morphology/morphology.py
@@ -808,14 +808,11 @@ class Morphology( object ):
         #
         if scale is None:
             # calculate the determinant
-            det0 = aff[0] * (aff[4]*aff[8] - aff[5]*aff[7])
-            det1 = aff[1] * (aff[3]*aff[8] - aff[5]*aff[6])
-            det2 = aff[2] * (aff[3]*aff[7] - aff[4]*aff[6])
-            det = det0 + det1 + det2
+            determinant = np.linalg.det(np.reshape(aff[0:9], (3, 3)))
             # determinant is change of volume that occurred during transform
             # assume equal scaling along all axes. take 3rd root to get
             #   scale factor
-            det_scale = math.pow(abs(det), 1.0/3.0)
+            det_scale = np.power(abs(determinant), 1.0 / 3.0)
             ## measure scale along each axis
             ## keep this code here in case 
             #scale_x = abs(aff[0] + aff[3] + aff[6])