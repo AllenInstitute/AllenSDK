ebb25d17b34137356f4c44dd22a769464b17b0d8
nile graddis <NileGraddis@gmail.com>
2020-02-20 14:02:08 -0800

    1.5.0 remove opencv dependency from visbeh sync integration test

--- allensdk/test/brain_observatory/behavior/test_sync_processing.py
+++ allensdk/test/brain_observatory/behavior/test_sync_processing.py
@@ -1,23 +1,37 @@
 import os
 import pytest
-import cv2
 from allensdk.brain_observatory.behavior.sync import get_sync_data
 
-def number_of_video_frames(video_path):
-    video = cv2.VideoCapture(video_path)
-    return int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+base_dir = os.path.join(
+    "/",
+    "allen",
+    "programs",
+    "braintv",
+    "production",
+    "visualbehavior",
+    "prod0",
+    "specimen_789992909",
+    "ophys_session_819949602",
+)
+sync_path=os.path.join(
+    base_dir, 
+    "819949602_sync.h5"
+)
 
-base_dir='/allen/programs/braintv/production/visualbehavior/prod0/specimen_789992909/ophys_session_819949602/'
-eye_tracking_video_path=os.path.join(base_dir, '819949602_video-1.avi')
-behaviormon_video_path=os.path.join(base_dir, '819949602_video-0.avi')
-sync_path=os.path.join(base_dir, '819949602_sync.h5')
 
 @pytest.mark.requires_bamboo
-@pytest.mark.parametrize('path_to_video, path_to_sync_file, sync_key', [
-    pytest.param(behaviormon_video_path, sync_path, 'behavior_monitoring'),
-    pytest.param(eye_tracking_video_path, sync_path, 'eye_tracking')
+@pytest.mark.parametrize("sync_path, sync_key, count_exp, last_exp", [
+    [sync_path, "ophys_frames", 140082, 4530.11659],
+    [sync_path, "lick_times", 2099, 3860.94482],
+    [sync_path, "ophys_trigger", 1, 6.8612],
+    [sync_path, "eye_tracking", 135908, 4531.00479],
+    [sync_path, "behavior_monitoring", 135887, 4530.19092],
+    [sync_path, "stim_photodiode", 4512, 4510.80997],
+    [sync_path, "stimulus_times_no_delay", 269977, 4510.25654],
 ])
-def test_video_exposure_time_sync(path_to_video, path_to_sync_file, sync_key):
-    n_video_frames = number_of_video_frames(path_to_video)
-    n_sync_timestamps = get_sync_data(path_to_sync_file)[sync_key].shape[0]
-    assert n_video_frames == n_sync_timestamps
+def test_get_time_sync_integration(sync_path, sync_key, count_exp, last_exp):
+    obt = get_sync_data(sync_path)[sync_key]
+    assert count_exp == len(obt)
+    assert last_exp == obt[-1]
+
+
