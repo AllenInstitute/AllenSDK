d727d80b7a86f447b66e612e401372d16f6068f7
Ani Nandi <anirban6908@gmail.com>
2020-03-09 18:00:29 -0700

    adding test for biophysical all-active legacy models

---
+++ allensdk/test/model/test_biophysical_all_active.py
@@ -0,0 +1,86 @@
+# Allen Institute Software License - This software license is the 2-clause BSD
+# license plus a third clause that prohibits redistribution for commercial
+# purposes without further permission.
+#
+# Copyright 2017. Allen Institute. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Redistributions for commercial purposes are not permitted without the
+# Allen Institute's written permission.
+# For purposes of this license, commercial purposes is the incorporation of the
+# Allen Institute's software into anything for which you will charge fees or
+# other compensation. Contact terms@alleninstitute.org for commercial licensing
+# opportunities.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+import pytest
+import os
+import numpy
+from allensdk.model.biophys_sim.config import Config
+from allensdk.model.biophysical.utils import Utils
+from allensdk.api.queries.biophysical_api import BiophysicalApi
+from allensdk.ephys import ephys_features
+
+def test_biophysical_all_active():
+    """
+    Test for backward compatibility of the legacy all-active models
+    """
+    neuronal_model_id = 497233125    # get this from the web site
+
+    model_directory = 'aa_model'
+
+    bp = BiophysicalApi('http://api.brain-map.org')
+    bp.cache_stimulus = False  # don't want to download the large stimulus NWB file
+    bp.cache_data(neuronal_model_id, working_directory=model_directory)
+    os.chdir(model_directory)
+    os.system('nrnivmodl modfiles')
+
+    description = Config().load('manifest.json')
+    utils = Utils(description)
+    h = utils.h
+
+    manifest = description.manifest
+    morphology_path = manifest.get_path('MORPHOLOGY')
+    utils.generate_morphology(morphology_path.encode('ascii', 'ignore').decode("utf-8"))
+    utils.load_cell_parameters()
+
+    stim = h.IClamp(h.soma[0](0.5))
+    stim.amp = 0.33
+    stim.delay = 1000.0
+    stim.dur = 1000.0
+
+    h.tstop = 3000.0
+
+    vec = utils.record_values()
+
+    h.finitialize()
+    h.run()
+
+    junction_potential = description.data['fitting'][0]['junction_potential']
+    mV = 1.0e-3
+    ms = 1.0e-3
+
+    output_data = (numpy.array(vec['v']) - junction_potential) * mV
+    output_times = numpy.array(vec['t']) * ms
+    num_spikes = len(ephys_features.detect_putative_spikes(output_data,output_times))
+    assert num_spikes == 18 # taken from the web app where the legacy model output is shown
