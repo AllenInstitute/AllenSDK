4398ff146ff3a8042c6180772bc0b078c62762b2
Ani Nandi <anirban6908@gmail.com>
2020-03-11 18:01:57 -0700

    Updated the examples with more documentation

--- doc_template/examples_root/examples/biophysical_sim/biophysical_sim.py
+++ doc_template/examples_root/examples/biophysical_sim/biophysical_sim.py
@@ -1,13 +1,13 @@
-from allensdk.model.biophysical import runner,_schemas
+from allensdk.model.biophysical import runner
 import json
 import os
-from argschema import ArgSchemaParser
 from allensdk.api.queries.biophysical_api import BiophysicalApi
 from allensdk.core.nwb_data_set import NwbDataSet
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import shutil
+import subprocess
 
 sns.set(style='whitegrid',font_scale=1.5)
 plt.rcParams.update({'axes.grid':False})
@@ -18,19 +18,48 @@ def get_manifest_args(args):
     return runner.load_description(args)
     
 
-def get_sweep_data(nwb_file,sweep_number):
+def get_sweep_data(nwb_file,sweep_number,time_scale=1e3,voltage_scale=1e3,stim_scale=1e12):
+    """
+    Extract data and stim characteristics for a specific DC sweep from nwb file
+    Parameters
+    ----------
+    nwb_file : string
+        File name of a pre-existing NWB file.
+    sweep_number : integer
+        
+    time_scale : float
+        Convert to ms scale
+    voltage_scale : float
+        Convert to mV scale
+    stim_scale : float
+        Convert to pA scale
+
+    Returns
+    -------
+    t : numpy array
+        Sampled time points in ms
+    v : numpy array
+        Recorded voltage at the sampled time points in mV
+    stim_start_time : float
+        Stimulus start time in ms
+    stim_end_time : float
+        Stimulus end time in ms
+    """
     nwb = NwbDataSet(nwb_file)
     sweep = nwb.get_sweep(sweep_number)
-    stim_diff = np.diff(sweep['stimulus']*1e12)
+    stim = sweep['stimulus']*stim_scale # in pA
+    stim_diff = np.diff(stim)
     stim_start = np.where(stim_diff != 0)[0][-2]
     stim_end = np.where(stim_diff != 0)[0][-1]
     
     # read v and t as numpy arrays
-    v = sweep['response']*1e3
-    dt = 1.0e3 / sweep['sampling_rate']
+    v = sweep['response']*voltage_scale # in mV
+    dt = time_scale / sweep['sampling_rate'] # in ms
     num_samples = len(v)
     t = np.arange(num_samples) * dt
-    return t,v,t[stim_start],t[stim_end]
+    stim_start_time = t[stim_start]
+    stim_end_time = t[stim_end]
+    return t,v,stim_start_time,stim_end_time
 
 def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
@@ -48,17 +77,14 @@ sweep_num = 46 # Select a Long Square sweep :  1s DC
 
 #%% Download all-acive model
 
-sdk_model_templates = {491455321:'all_active',329230710:'perisomatic'}
+sdk_model_templates = {'all_active':491455321,'perisomatic':329230710}
 bp = BiophysicalApi()
 bp.cache_stimulus = True 
-model_list = bp.get_neuronal_models(cell_id)
-model_dict = {}
-for model_dict_ in model_list:
-    if model_dict_['neuronal_model_template_id'] in sdk_model_templates.keys():
-        model_dict[sdk_model_templates[model_dict_['neuronal_model_template_id']]] = model_dict_['id']
+model_list = bp.get_neuronal_models(cell_id,model_type_ids=[sdk_model_templates['all_active']]) # Only get the all-active model for the cell
+model_dict = model_list[0]
 
 model_dir = 'all_active_models'
-bp.cache_data(model_dict['all_active'], working_directory=model_dir)
+bp.cache_data(model_dict['id'], working_directory=model_dir) 
 new_model_file = 'fit_parameters_new.json'
 shutil.copyfile(new_model_file,os.path.join(model_dir,new_model_file))
 copytree('modfiles',os.path.join(model_dir,'modfiles'))
@@ -66,7 +92,7 @@ copytree('modfiles',os.path.join(model_dir,'modfiles'))
 #%% Running the legacy all-active models
 
 os.chdir(model_dir)
-os.system('nrnivmodl modfiles/')
+subprocess.check_call(['nrnivmodl','modfiles/'])
 manifest_file = 'manifest.json'
 manifest_dict = json.load(open(manifest_file))
 
@@ -76,9 +102,8 @@ if 'sweeps_by_type' not in manifest_dict['runs'][0]:
     manifest_dict['runs'][0]['sweeps_by_type'] = {"Long Square":[sweep_num]}
 json.dump(manifest_dict,open(manifest_file,'w'),indent=2) 
 
-schema_legacy = ArgSchemaParser(input_data=dict(manifest_file=manifest_file,log_level = 'INFO'),
-                                         schema_type=_schemas.runner_config)
-runner.run(schema_legacy.args,procs=1,sweeps=[sweep_num])
+schema_legacy = dict(manifest_file=manifest_file)
+runner.run(schema_legacy,procs=1,sweeps=[sweep_num])
 
 #%% Running the new all-active models
 
@@ -94,16 +119,15 @@ new_manifest_file = 'manifest_new.json'
 manifest_dict['biophys'][0]['model_file'] = [new_manifest_file,new_model_file]
 json.dump(manifest_dict,open(new_manifest_file,'w'),indent=2)  
 
-schema_new = ArgSchemaParser(input_data=dict(manifest_file=new_manifest_file,log_level = 'INFO', axon_type = 'stub_axon'),
-                                         schema_type=_schemas.runner_config)
-runner.run(schema_new.args,procs=1,sweeps=[sweep_num])
+schema_new = dict(manifest_file=new_manifest_file,axon_type = 'stub_axon')
+runner.run(schema_new,procs=1,sweeps=[sweep_num])
 
 #%% Comparing the responses
 
-legacy_config = get_manifest_args(schema_legacy.args)
+legacy_config = get_manifest_args(schema_legacy)
 output_nwb_legacy = legacy_config.manifest.get_path('output_path')
 
-new_config =  get_manifest_args(schema_new.args)
+new_config =  get_manifest_args(schema_new)
 output_nwb_new = new_config.manifest.get_path('output_path')
 
 exp_nwb = new_config.manifest.get_path('stimulus_path')
@@ -121,5 +145,5 @@ sns.despine(ax=ax)
 h,l = ax.get_legend_handles_labels()
 fig.legend(h,l,ncol=3,frameon=False,loc='lower center')
 fig.tight_layout(rect=[0.0,0.05,1,0.95])
-
 plt.show()
+
