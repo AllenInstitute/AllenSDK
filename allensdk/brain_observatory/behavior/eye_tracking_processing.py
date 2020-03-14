Fix outlier calculation
The previous outlier implementation was calculating
z score across a rows instead of along a column.

allensdk/brain_observatory/behavior/eye_tracking_processing.py
@@ -74,7 +74,7 @@ def determine_outliers(data_df: pd.DataFrame,
     # in determine_likely_blinks().
     nan_filled_df = data_df.fillna(data_df.mean())
 
-    outliers = (np.abs(stats.zscore(nan_filled_df, axis=1)) > z_threshold)
+    outliers = (nan_filled_df.apply(stats.zscore).apply(np.abs) > z_threshold)
     return pd.Series(outliers.any(axis=1))
 
 

allensdk/test/brain_observatory/behavior/test_eye_tracking_processing.py
@@ -90,20 +90,26 @@ def test_load_eye_tracking_hdf(hdf_fixture: Path, expected: pd.DataFrame):
     (create_area_df(
         np.array([[1, 1, 2],
                   [2, 2, 1],
-                  [1, 9, 1],
+                  [1, 7, 3],
                   [1, 1, 1],
-                  [2, 1, 100]])),
-     3.0,
-     pd.Series([False, False, True, False, True])),
+                  [1, 3, 2],
+                  [1, 1, 1],
+                  [1, 2, 1],
+                  [2, 1, 1000]])),
+     2.5,
+     pd.Series([False, False, False, False, False, False, False, True])),
 
     (create_area_df(
         np.array([[1, 1, 2],
                   [2, 2, 1],
-                  [1, 9, 1],
+                  [1, 7, 3],
                   [1, 1, 1],
-                  [2, 1, 100]])),
-     3.7,
-     pd.Series([False, False, False, False, True])),
+                  [1, 3, 2],
+                  [1, 1, 1],
+                  [1, 2, 1],
+                  [2, 1, 1000]])),
+     2.0,
+     pd.Series([False, False, True, False, False, False, False, True])),
 
 ])
 def test_determine_outliers(data_df, z_threshold, expected):