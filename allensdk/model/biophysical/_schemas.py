427f644feae86ca59e331a9eab337af8361bfc33
Ani Nandi <anirban6908@gmail.com>
2020-03-23 15:40:17 -0700

    removed separate schema file ad voltage output

--- allensdk/model/biophysical/_schemas.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-# Create the parser
-sim_parser = argparse.ArgumentParser(description='arguments to be passed for biophysical simualtions')
-sim_parser.add_argument('manifest_file',
-                        help='.json configurations for running the simulations')
-sim_parser.add_argument('--axon_type', required= False,
-                        help='axon replacement for biophysical models')
