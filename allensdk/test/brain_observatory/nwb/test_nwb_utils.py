830022e4cd003fe41e8e187b7c17d4c983e946fc
isaak-willett <58192697+isaak-willett@users.noreply.github.com>
2020-06-23 12:07:46 -0700

    Omitted stimuli bug (#1623)
    * Added function to set the stop_time for omitted stimuli for writing NWB
    
    This function had to be added as the omitted stimuli did not by design
    have an ending timestamp. These stimuli therfore could not be written to
    NWB files as the interval data object in PyNWB requires an ending time.
    This commit adds a function that adds a stop_time to a omitted row that
    is based off the known time that omitted stimuli are shown. This allows
    for omitted stimuli types to be written to NWB intervals.
    
    * Added doc strings to stimulus_presentations.py
    
    I added documentation for the functions in stimulus_presentations.py that I have touched during investigation. These functions are get_stimulus_presentations and get_visual_stimuli_df. I added doc strings to better explain what is happening in the code.

--- allensdk/test/brain_observatory/nwb/test_nwb_utils.py
+++ allensdk/test/brain_observatory/nwb/test_nwb_utils.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 
 from allensdk.brain_observatory.nwb import nwb_utils
 
@@ -29,3 +30,29 @@ def test_get_stimulus_name_column_exceptions(input_cols,
         nwb_utils.get_column_name(input_cols, possible_names)
     for expected_value in expected_excep_cols:
         assert expected_value in str(error.value)
+
+
+@pytest.mark.parametrize("stimulus_table, expected_table_data", [
+    ({'image_index': [8, 9],
+      'image_name': ['omitted', 'not_omitted'],
+      'image_set': ['omitted', 'not_omitted'],
+      'index': [201, 202],
+      'omitted': [True, False],
+      'start_frame': [231060, 232340],
+      'start_time': [0, 250],
+      'stop_time': [None, 1340509]},
+     {'image_index': [8, 9],
+      'image_name': ['omitted', 'not_omitted'],
+      'image_set': ['omitted', 'not_omitted'],
+      'index': [201, 202],
+      'omitted': [True, False],
+      'start_frame': [231060, 232340],
+      'start_time': [0, 250],
+      'stop_time': [0.25, 1340509]}
+     )
+])
+def test_set_omitted_stop_time(stimulus_table, expected_table_data):
+    stimulus_table = pd.DataFrame.from_dict(data=stimulus_table)
+    expected_table = pd.DataFrame.from_dict(data=expected_table_data)
+    nwb_utils.set_omitted_stop_time(stimulus_table)
+    assert stimulus_table.equals(expected_table)
