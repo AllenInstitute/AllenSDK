101372c3f4e0f1a9ec0281788ed93419da5948e4
isaak-willett <58192697+isaak-willett@users.noreply.github.com>
2020-06-23 12:07:46 -0700

    Omitted stimuli bug (#1623)
    * Added function to set the stop_time for omitted stimuli for writing NWB
    
    This function had to be added as the omitted stimuli did not by design
    have an ending timestamp. These stimuli therfore could not be written to
    NWB files as the interval data object in PyNWB requires an ending time.
    This commit adds a function that adds a stop_time to a omitted row that
    is based off the known time that omitted stimuli are shown. This allows
    for omitted stimuli types to be written to NWB intervals.
    
    * Added doc strings to stimulus_presentations.py
    
    I added documentation for the functions in stimulus_presentations.py that I have touched during investigation. These functions are get_stimulus_presentations and get_visual_stimuli_df. I added doc strings to better explain what is happening in the code.

--- allensdk/brain_observatory/nwb/nwb_utils.py
+++ allensdk/brain_observatory/nwb/nwb_utils.py
@@ -1,3 +1,9 @@
+import pandas as pd
+# All of the omitted stimuli have a duration of 250ms as defined
+# by the Visual Behavior team. For questions about duration contact that
+# team.
+
+
 def get_column_name(table_cols: list,
                     possible_names: set) -> str:
     """
@@ -17,3 +23,30 @@ def get_column_name(table_cols: list,
         raise KeyError("Table expected one name column in intersection, found:"
                        f" {column_names}")
     return column_names[0]
+
+
+def set_omitted_stop_time(stimulus_table: pd.DataFrame,
+                          omitted_time_duration: float=0.25) -> None:
+    """
+    This function sets the stop time for a row that of a stimuli table that
+    is a omitted stimuli. A omitted stimuli is a stimuli where a mouse is
+    shown only a grey screen and these last for 250 milliseconds. These do not
+    include a stop_time or end_frame as other stimuli in the stimulus table due
+    to design choices. For these stimuli to be added they must have the
+    stop_time calculated and put into the row as data before writing to NWB.
+    :param stimulus_table: pd.DataFrame that contains the stimuli presented to
+                           an experiment subject
+    :param omitted_time_duration: The duration in seconds of the expected length
+                                  of the omitted stimuli
+    :return:
+          stimulus_table_row: returns the same dictionary as inputted but with
+                              an additional entry for stop_time.
+    """
+    omitted_row_indexs = stimulus_table.index[stimulus_table['omitted']].tolist()
+    for omitted_row_idx in omitted_row_indexs:
+        row = stimulus_table.iloc[omitted_row_idx]
+        start_time = row['start_time']
+        end_time = start_time + omitted_time_duration
+        row['stop_time'] = end_time
+        row['duration'] = omitted_time_duration
+        stimulus_table.iloc[omitted_row_idx] = row
