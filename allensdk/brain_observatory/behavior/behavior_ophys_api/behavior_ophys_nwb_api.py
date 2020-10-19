ac7fde761f92dd5ebf435b4471ed14866beba72c
Nicholas Mei <nicholas.mei@alleninstitute.org>
2020-10-19 15:22:17 -0700

    Add experiment descriptions to behavior ophys NWB files
    This commit adds experiment descriptions to be havior ophys NWB files.
    These descriptions can be determined based on the behavior ophys
    "session type" string. Example:
    
    "OPHYS_1_images_A" is the first stage of the visual behavior experiments
    "OPHYS_6_images_A" is the last stage of the visual behavior experiments

--- allensdk/brain_observatory/behavior/behavior_ophys_api/behavior_ophys_nwb_api.py
+++ allensdk/brain_observatory/behavior/behavior_ophys_api/behavior_ophys_nwb_api.py
@@ -29,6 +29,73 @@ load_pynwb_extension(OphysBehaviorMetadataSchema, 'ndx-aibs-behavior-ophys')
 load_pynwb_extension(BehaviorTaskParametersSchema, 'ndx-aibs-behavior-ophys')
 
 
+def _get_expt_description(session_type: str) -> str:
+    """Determine a behavior ophys session's experiment description based on
+    session type.
+
+    Parameters
+    ----------
+    session_type : str
+        A session description string (e.g. OPHYS_1_images_B )
+
+    Returns
+    -------
+    str
+        A description of the experiment based on the session_type.
+
+    Raises
+    ------
+    RuntimeError
+        Behavior ophys sessions should only have 6 different session types.
+        Unknown session types (or malformed session_type strings) will raise
+        an error.
+    """
+    # Experiment descriptions for different session types:
+    # OPHYS_1 -> OPHYS_6
+    ophys_1_3 = dict.fromkeys(
+        ["OPHYS_1", "OPHYS_3"],
+        ("2-photon calcium imaging in the visual cortex of the mouse "
+         "brain as the mouse performs a visual change detection task "
+         "with a set of natural scenes upon which it has previously been "
+         "trained.")
+    )
+    ophys_4_6 = dict.fromkeys(
+        ["OPHYS_4", "OPHYS_6"],
+        ("2-photon calcium imaging in the visual cortex of the mouse "
+         "brain as the mouse performs a visual change detection task "
+         "with a set of natural scenes that are unique from those on "
+         "which it was previously trained.")
+    )
+    ophys_2_5 = {
+        "OPHYS_2": ("2-photon calcium imaging in the visual cortex of the "
+                    "mouse brain as the mouse is shown images from a "
+                    "change detection task with a set of natural scenes "
+                    "upon which it has previously been trained, but with "
+                    "the lick-response sensor withdrawn (passive/open "
+                    "loop mode)."),
+        "OPHYS_5": ("2-photon calcium imaging in the visual cortex of the "
+                    "mouse brain as the mouse is shown images from a "
+                    "change detection task with a set of natural scenes "
+                    "that are unique from those on which it was "
+                    "previously trained, but with the lick-response "
+                    "sensor withdrawn (passive/open loop mode)."),
+    }
+    expt_description_dict = {**ophys_1_3, **ophys_2_5, **ophys_4_6}
+
+    # Session type string will look something like: OPHYS_4_images_A
+    truncated_session_type = "_".join(session_type.split("_")[:2])
+
+    try:
+        return expt_description_dict[truncated_session_type]
+    except KeyError as e:
+        e_msg = (
+            f"Encountered an unknown session type "
+            f"({truncated_session_type}) when trying to determine "
+            f"experiment descriptions. Valid session types are: "
+            f"{expt_description_dict.keys()}")
+        raise RuntimeError(e_msg) from e
+
+
 class BehaviorOphysNwbApi(NwbApi, BehaviorOphysApiBase):
 
     def __init__(self, *args, **kwargs):
@@ -37,12 +104,17 @@ class BehaviorOphysNwbApi(NwbApi, BehaviorOphysApiBase):
 
     def save(self, session_object):
 
+        session_type = str(session_object.metadata['session_type'])
+
         nwbfile = NWBFile(
-            session_description=str(session_object.metadata['session_type']),
+            session_description=session_type,
             identifier=str(session_object.ophys_experiment_id),
             session_start_time=session_object.metadata['experiment_datetime'],
             file_create_date=pytz.utc.localize(datetime.datetime.now()),
-            institution="Allen Institute for Brain Science"
+            institution="Allen Institute for Brain Science",
+            keywords=["2-photon", "calcium imaging", "visual cortex",
+                      "behavior", "task"],
+            experiment_description=_get_expt_description(session_type)
         )
 
         # Add stimulus_timestamps to NWB in-memory object:
