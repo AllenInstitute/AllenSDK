c6d4efb258933814b34438092cf059852614cd3d
danielsf <scott.daniel@alleninstitute.org>
2021-01-26 15:17:10 -0800

    get_fluorescence_traces does not assume ROI ID order has not changed

--- allensdk/brain_observatory/behavior/session_apis/data_transforms/behavior_ophys_data_xforms.py
+++ allensdk/brain_observatory/behavior/session_apis/data_transforms/behavior_ophys_data_xforms.py
@@ -347,18 +347,27 @@ class BehaviorOphysDataXforms(BehaviorOphysBase):
     def get_corrected_fluorescence_traces(self):
         demix_file = self.get_demix_file()
 
-        g = h5py.File(demix_file)
-        corrected_fluorescence_traces = np.asarray(g['data'])
-        g.close()
+        with h5py.File(demix_file, 'r') as in_file:
+            corrected_fluorescence_traces = in_file['data'][()]
+            corrected_fluorescence_roi_id = in_file['roi_names'][()]
 
         cell_roi_id_list = self.get_cell_roi_ids()
+
+        if not np.in1d(corrected_fluorescence_roi_id, cell_roi_id_list).all():
+            raise RuntimeError("corrected_fluorescence_traces contains ROI IDs "
+                               "not present in cell_specimen_table")
+        if not np.in1d(cell_roi_id_list, corrected_fluorescence_roi_id).all():
+            raise RuntimeError("cell_specimen_table contains ROI IDs "
+                               "not present in corrected_fluorescence_traces")
+
         ophys_timestamps = self.get_ophys_timestamps()
 
         num_trace_timepoints = corrected_fluorescence_traces.shape[1]
         assert num_trace_timepoints == ophys_timestamps.shape[0]
         df = pd.DataFrame(
             {'corrected_fluorescence': list(corrected_fluorescence_traces)},
-            index=pd.Index(cell_roi_id_list, name='cell_roi_id'))
+            index=pd.Index(corrected_fluorescence_roi_id.astype(int),
+                           name='cell_roi_id'))
 
         cell_specimen_table = self.get_cell_specimen_table()
         df = cell_specimen_table[['cell_roi_id']].join(df, on='cell_roi_id')
