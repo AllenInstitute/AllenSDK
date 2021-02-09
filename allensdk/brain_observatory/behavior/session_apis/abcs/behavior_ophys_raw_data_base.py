aabd54f716298d1c980f6d81cc094647cf356b2a
Nicholas Mei <nicholas.mei@alleninstitute.org>
2021-02-09 11:46:33 -0800

    Decorate abstract base class methods with abc.abstractmethod
    Previously methods in the BehaviorOphysRawDataBase and
    BehaviorRawDataBase only raised a NotImplementedError().
    This resulted in an error only being thrown when the method
    in questions was called without an override.
    
    This commit adds the abstractmethod decorator which will result in
    an error being thrown when trying to instantiate without a method
    override.

--- allensdk/brain_observatory/behavior/session_apis/abcs/behavior_ophys_raw_data_base.py
+++ allensdk/brain_observatory/behavior/session_apis/abcs/behavior_ophys_raw_data_base.py
@@ -1,7 +1,8 @@
+import abc
+from typing import Dict, Optional
+
 from allensdk.brain_observatory.behavior.session_apis.abcs import (
     BehaviorRawDataBase)
-
-from typing import Dict, Optional
 
 
 class BehaviorOphysRawDataBase(BehaviorRawDataBase):
@@ -9,79 +10,95 @@ class BehaviorOphysRawDataBase(BehaviorRawDataBase):
     raw (from LIMS or from JSON) behavior + ophys session data.
     """
 
+    @abc.abstractmethod
     def get_ophys_experiment_id(self) -> int:
         """Return the ophys experiment id (experiments are an internal alias
         for an imaging plane)"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_ophys_session_id(self) -> int:
         """Return the ophys session id"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_surface_2p_pixel_size_um(self) -> float:
         """Get the pixel size for 2-photon movies in micrometers"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_sync_file(self) -> str:
         """Get the filepath of the sync timing file associated with the
         ophys experiment"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_field_of_view_shape(self) -> Dict[str, int]:
         """Get a field of view dictionary for a given ophys experiment.
            ex: {"width": int, "height": int}
         """
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_experiment_container_id(self) -> int:
         """Get the experiment container id associated with an ophys
         experiment"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_targeted_structure(self) -> str:
         """Get the targeted structure (acronym) for an ophys experiment
         (ex: "Visp")"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_imaging_depth(self) -> int:
         """Get the imaging depth for an ophys experiment
         (ex: 400, 500, etc.)"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_dff_file(self) -> str:
         """Get the filepath of the dff trace file associated with an ophys
         experiment."""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_ophys_cell_segmentation_run_id(self) -> int:
         """Get the ophys cell segmentation run id associated with an
         ophys experiment id"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_raw_cell_specimen_table_dict(self) -> dict:
         """Get the cell_rois table from LIMS in dictionary form"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_demix_file(self) -> str:
         """Get the filepath of the demixed traces file associated with an
         ophys experiment"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_average_intensity_projection_image_file(self) -> str:
         """Get the avg intensity project image filepath associated with an
         ophys experiment"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_max_projection_file(self) -> str:
         """Get the filepath of the max projection image associated with the
         ophys experiment"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_rigid_motion_transform_file(self) -> str:
         """Get the filepath for the motion transform file (.csv) associated
         with an ophys experiment"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_imaging_plane_group(self) -> Optional[int]:
         """Get the imaging plane group number. This is a numeric index
         that indicates the order that the frames were acquired when
@@ -91,6 +108,7 @@ class BehaviorOphysRawDataBase(BehaviorRawDataBase):
         """
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_plane_group_count(self) -> int:
         """Gets the total number of plane groups in the session.
         This is required for resampling ophys timestamps for mesoscope
@@ -99,15 +117,18 @@ class BehaviorOphysRawDataBase(BehaviorRawDataBase):
         """
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_eye_tracking_rig_geometry(self) -> dict:
         """Get the eye tracking rig geometry associated with an
         ophys experiment"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_eye_tracking_filepath(self) -> dict:
         """Get the eye tracking filepath containing ellipse fits"""
         raise NotImplementedError()
 
+    @abc.abstractmethod
     def get_eye_gaze_mapping_file_path(self) -> str:
         """Get h5 filepath containing eye gaze behavior of the experiment's
         subject"""
