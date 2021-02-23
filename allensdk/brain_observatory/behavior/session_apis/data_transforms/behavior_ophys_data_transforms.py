ec9d43d148b6c20c34afb4edfbdb8d518e79a0a7
danielsf <scott.daniel@alleninstitute.org>
2021-02-23 10:11:28 -0800

    remove get_licks from BehaviorOphysDataTransforms
    it was identical to the get_licks implementation in
    BehaviorDataTransform, from which BehaviorOphysDataTransform
    inherits

--- allensdk/brain_observatory/behavior/session_apis/data_transforms/behavior_ophys_data_transforms.py
+++ allensdk/brain_observatory/behavior/session_apis/data_transforms/behavior_ophys_data_transforms.py
@@ -327,25 +327,6 @@ class BehaviorOphysDataTransforms(BehaviorDataTransforms, BehaviorOphysBase):
         return pd.DataFrame({'time': lick_times})
 
     @memoize
-    def get_licks(self) -> pd.DataFrame:
-        """
-        Returns
-        -------
-        pd.DataFrame
-            Two columns: "time", which contains the sync time
-            of the licks that occurred in this session and "frame",
-            the frame numbers of licks that occurred in this session
-        """
-        data = self._behavior_stimulus_file()
-        timestamps = self.get_stimulus_timestamps()
-        # Get licks from pickle file (need to add an offset to align with
-        # the trial_log time stream)
-        lick_frames = (data["items"]["behavior"]["lick_sensors"][0]
-                       ["lick_events"])
-        lick_times = timestamps[lick_frames]
-        return pd.DataFrame({"time": lick_times, "frame": lick_frames})
-
-    @memoize
     def get_rewards(self):
         data = self._behavior_stimulus_file()
         timestamps = self.get_stimulus_timestamps()
