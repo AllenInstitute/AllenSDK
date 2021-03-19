787804f86a6da6edcb06ab966431c568de10f03d
aamster <adam.amster@alleninstitute.org>
2021-03-19 14:16:09 -0700

    change int to enum value

--- allensdk/test/brain_observatory/behavior/test_behavior_project_cache.py
+++ allensdk/test/brain_observatory/behavior/test_behavior_project_cache.py
@@ -150,16 +150,16 @@ def test_session_table_reads_from_cache(TempdirBehaviorCache, session_table,
     cache = TempdirBehaviorCache
     cache.get_session_table()
     expected_first = [
-        ('call_caching', 20, 'Reading data from cache'),
-        ('call_caching', 20, 'No cache file found.'),
-        ('call_caching', 20, 'Fetching data from remote'),
-        ('call_caching', 20, 'Writing data to cache'),
-        ('call_caching', 20, 'Reading data from cache'),
-        ('call_caching', 20, 'Reading data from cache'),
-        ('call_caching', 20, 'No cache file found.'),
-        ('call_caching', 20, 'Fetching data from remote'),
-        ('call_caching', 20, 'Writing data to cache'),
-        ('call_caching', 20, 'Reading data from cache')]
+        ('call_caching', logging.INFO, 'Reading data from cache'),
+        ('call_caching', logging.INFO, 'No cache file found.'),
+        ('call_caching', logging.INFO, 'Fetching data from remote'),
+        ('call_caching', logging.INFO, 'Writing data to cache'),
+        ('call_caching', logging.INFO, 'Reading data from cache'),
+        ('call_caching', logging.INFO, 'Reading data from cache'),
+        ('call_caching', logging.INFO, 'No cache file found.'),
+        ('call_caching', logging.INFO, 'Fetching data from remote'),
+        ('call_caching', logging.INFO, 'Writing data to cache'),
+        ('call_caching', logging.INFO, 'Reading data from cache')]
     assert expected_first == caplog.record_tuples
     caplog.clear()
     cache.get_session_table()
@@ -173,16 +173,16 @@ def test_behavior_table_reads_from_cache(TempdirBehaviorCache, behavior_table,
     cache = TempdirBehaviorCache
     cache.get_behavior_session_table()
     expected_first = [
-        ('call_caching', 20, 'Reading data from cache'),
-        ('call_caching', 20, 'No cache file found.'),
-        ('call_caching', 20, 'Fetching data from remote'),
-        ('call_caching', 20, 'Writing data to cache'),
-        ('call_caching', 20, 'Reading data from cache'),
-        ('call_caching', 20, 'Reading data from cache'),
-        ('call_caching', 20, 'No cache file found.'),
-        ('call_caching', 20, 'Fetching data from remote'),
-        ('call_caching', 20, 'Writing data to cache'),
-        ('call_caching', 20, 'Reading data from cache')]
+        ('call_caching', logging.INFO, 'Reading data from cache'),
+        ('call_caching', logging.INFO, 'No cache file found.'),
+        ('call_caching', logging.INFO, 'Fetching data from remote'),
+        ('call_caching', logging.INFO, 'Writing data to cache'),
+        ('call_caching', logging.INFO, 'Reading data from cache'),
+        ('call_caching', logging.INFO, 'Reading data from cache'),
+        ('call_caching', logging.INFO, 'No cache file found.'),
+        ('call_caching', logging.INFO, 'Fetching data from remote'),
+        ('call_caching', logging.INFO, 'Writing data to cache'),
+        ('call_caching', logging.INFO, 'Reading data from cache')]
     assert expected_first == caplog.record_tuples
     caplog.clear()
     cache.get_behavior_session_table()
