f8a1e800bd86ab0c4f44c475889e75361ccbf73f
aamster <adam.amster@alleninstitute.org>
2021-04-06 09:26:29 -0700

    clean up example

--- allensdk/brain_observatory/behavior2/examples/BehaviorSession_example.py
+++ allensdk/brain_observatory/behavior2/examples/BehaviorSession_example.py
@@ -10,13 +10,9 @@ from allensdk.core.auth_config import LIMS_DB_CREDENTIAL_MAP
 import allensdk.brain_observatory.behavior2.data_objects.behavior_session as bs
 
 
-def main():
-    dbconn = db_connection_creator(fallback_credentials=LIMS_DB_CREDENTIAL_MAP)
-    behavior_session_id = 870987812
-    bsession = bs.BehaviorSession.from_lims(
-        dbconn, behavior_session_id=behavior_session_id)
-
-    # simple round-trip
+def test_json_roundtrip(bsession: bs.BehaviorSession):
+    """Test a session created from_lims is same as session
+    created from_json"""
     dict_repr = bsession.to_json()
     print(json.dumps(dict_repr, indent=2))
     bsession2 = bs.BehaviorSession.from_json(dict_repr)
@@ -24,23 +20,36 @@ def main():
     print(json.dumps(dict_repr2, indent=2))
     assert dict_repr2 == dict_repr
 
+
+def test_nwb_roundtrip(bsession: bs.BehaviorSession):
+    """Tests a session created from_lims is same as session created from_nwb"""
     nwbfile = NWBFile(
         session_description='test',
-        identifier=str(bsession2.behavior_session_id.value),
+        identifier=str(bsession.behavior_session_id.value),
         session_start_time=pytz.utc.localize(datetime.datetime.now()),
         file_create_date=pytz.utc.localize(datetime.datetime.now()),
         institution="Allen Institute for Brain Science",
         keywords=["visual", "behavior", "task"],
         experiment_description='test'
     )
-    bsession2.to_nwb(nwbfile=nwbfile)
+    bsession.to_nwb(nwbfile=nwbfile)
     nwb_writer = NWBWriter(nwbfile=nwbfile, path='/tmp/test_nwb.nwb')
     nwb_writer.write()
 
     bsession3 = bs.BehaviorSession.from_nwb_path(path='/tmp/test_nwb.nwb')
-    dict_repr3 = bsession3.to_json()
-    print(json.dumps(dict_repr3, indent=2))
-    assert dict_repr3 == dict_repr
+    dict_repr = bsession3.to_json()
+    print(json.dumps(dict_repr, indent=2))
+    assert dict_repr == bsession.to_json()
+
+    
+def main():
+    dbconn = db_connection_creator(fallback_credentials=LIMS_DB_CREDENTIAL_MAP)
+    behavior_session_id = 870987812
+    bsession = bs.BehaviorSession.from_lims(
+        dbconn, behavior_session_id=behavior_session_id)
+
+    test_json_roundtrip(bsession=bsession)
+    test_nwb_roundtrip(bsession=bsession)
 
 
 if __name__ == '__main__':
