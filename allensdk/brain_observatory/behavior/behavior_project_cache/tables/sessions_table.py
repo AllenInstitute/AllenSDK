eb7606022d5ff028f8d4fbd2e91b43beb22df52d
aamster <adam.amster@alleninstitute.org>
2021-04-13 14:53:55 -0700

    Fixes issue in which the session_type in mtrain was missing for certain sessions.

--- allensdk/brain_observatory/behavior/behavior_project_cache/tables/sessions_table.py
+++ allensdk/brain_observatory/behavior/behavior_project_cache/tables/sessions_table.py
@@ -76,6 +76,13 @@ class SessionsTable(ProjectTable):
             self._df = self._df.drop(['date_of_acquisition_behavior',
                                       'date_of_acquisition_ophys'], axis=1)
 
+            # Prioritize behavior session_type
+            self._df['session_type'] = \
+                self._df['session_type_behavior']
+            self._df = self._df.drop(
+                ['session_type_behavior',
+                 'session_type_ophys'], axis=1)
+
     def __add_session_number(self):
         """Parses session number from session type and and adds to dataframe"""
 
