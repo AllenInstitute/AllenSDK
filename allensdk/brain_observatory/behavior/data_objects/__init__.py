1bb32e74e141ed3a06ad972cd71237a246086b62
Nicholas Mei <nicholas.mei@alleninstitute.org>
2021-04-26 16:50:16 -0700

    Add BehaviorSessionId DataObject

--- allensdk/brain_observatory/behavior/data_objects/__init__.py
+++ allensdk/brain_observatory/behavior/data_objects/__init__.py
@@ -1,4 +1,5 @@
 from allensdk.brain_observatory.behavior.data_objects._data_object_abc import DataObject  # noqa: E501, F401
+from allensdk.brain_observatory.behavior.data_objects.behavior_session_id import BehaviorSessionId  # noqa: E501, F401
 from allensdk.brain_observatory.behavior.data_objects.stimulus_timestamps.stimulus_timestamps import StimulusTimestamps  # noqa: E501, F401
 from allensdk.brain_observatory.behavior.data_objects.running_speed.running_speed import RunningSpeed  # noqa: E501, F401
 from allensdk.brain_observatory.behavior.data_objects.running_speed.running_acquisition import RunningAcquisition  # noqa: E501, F401
