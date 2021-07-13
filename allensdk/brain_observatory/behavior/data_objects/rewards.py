946b4b6af4ab34913c8c50f6e7b85f58850ae954
aamster <adam.amster@alleninstitute.org>
2021-07-13 08:52:26 -0700

    adds rewards

---
+++ allensdk/brain_observatory/behavior/data_objects/rewards.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from pynwb import NWBFile, TimeSeries, ProcessingModule
+
+from allensdk.brain_observatory.behavior.data_files import StimulusFile
+from allensdk.brain_observatory.behavior.data_objects import DataObject, \
+    StimulusTimestamps
+from allensdk.brain_observatory.behavior.data_objects.base \
+    .readable_interfaces import \
+    StimulusFileReadableInterface, NwbReadableInterface
+from allensdk.brain_observatory.behavior.data_objects.base\
+    .writable_interfaces import \
+    NwbWritableInterface
+
+
+class Rewards(DataObject, StimulusFileReadableInterface, NwbReadableInterface,
+              NwbWritableInterface):
+    def __init__(self, rewards: pd.DataFrame):
+        super().__init__(name='rewards', value=rewards)
+
+    @classmethod
+    def from_stimulus_file(cls, stimulus_file: StimulusFile) -> "Rewards":
+        """Get reward data from pkl file, based on pkl file timestamps
+        (not sync file).
+        """
+        data = stimulus_file.data
+        timestamps = StimulusTimestamps.from_stimulus_file(
+            stimulus_file=stimulus_file)
+
+        trial_df = pd.DataFrame(data["items"]["behavior"]["trial_log"])
+        rewards_dict = {"volume": [], "timestamps": [], "autorewarded": []}
+        for idx, trial in trial_df.iterrows():
+            rewards = trial["rewards"]
+            # as i write this there can only ever be one reward per trial
+            if rewards:
+                rewards_dict["volume"].append(rewards[0][0])
+                rewards_dict["timestamps"].append(
+                    timestamps.value[rewards[0][2]])
+                auto_rwrd = trial["trial_params"]["auto_reward"]
+                rewards_dict["autorewarded"].append(auto_rwrd)
+
+        df = pd.DataFrame(rewards_dict)
+        return cls(rewards=df)
+
+    @classmethod
+    def from_nwb(cls, nwbfile: NWBFile) -> "Rewards":
+        if 'rewards' in nwbfile.processing:
+            rewards = nwbfile.processing['rewards']
+            time = rewards.get_data_interface('autorewarded').timestamps[:]
+            autorewarded = rewards.get_data_interface('autorewarded').data[:]
+            volume = rewards.get_data_interface('volume').data[:]
+            df = pd.DataFrame({
+                'volume': volume, 'timestamps': time,
+                'autorewarded': autorewarded})
+        else:
+            df = pd.DataFrame({
+                'volume': [], 'timestamps': [],
+                'autorewarded': []})
+        return cls(rewards=df)
+
+    def to_nwb(self, nwbfile: NWBFile) -> NWBFile:
+        reward_volume_ts = TimeSeries(
+            name='volume',
+            data=self.value['volume'].values,
+            timestamps=self.value['timestamps'].values,
+            unit='mL'
+        )
+
+        autorewarded_ts = TimeSeries(
+            name='autorewarded',
+            data=self.value['autorewarded'].values,
+            timestamps=reward_volume_ts.timestamps,
+            unit='mL'
+        )
+
+        rewards_mod = ProcessingModule('rewards',
+                                       'Licking behavior processing module')
+        rewards_mod.add_data_interface(reward_volume_ts)
+        rewards_mod.add_data_interface(autorewarded_ts)
+        nwbfile.add_processing_module(rewards_mod)
+
+        return nwbfile
---
+++ allensdk/test/brain_observatory/behavior/data_objects/test_rewards.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from pathlib import Path
+
+import pandas as pd
+import pynwb
+import pytest
+
+from allensdk.brain_observatory.behavior.data_files import StimulusFile
+from allensdk.brain_observatory.behavior.data_objects.rewards import Rewards
+from allensdk.test.brain_observatory.behavior.data_objects.lims_util import \
+    LimsTest
+
+
+class TestFromStimulusFile(LimsTest):
+    @classmethod
+    def setup_class(cls):
+        cls.behavior_session_id = 994174745
+
+        dir = Path(__file__).parent.resolve()
+        test_data_dir = dir / 'test_data'
+
+        expected = pd.read_pickle(str(test_data_dir / 'rewards.pkl'))
+        cls.expected = Rewards(rewards=expected)
+
+    @pytest.mark.requires_bamboo
+    def test_from_stimulus_file(self):
+        self.stimulus_file = StimulusFile.from_lims(
+            behavior_session_id=self.behavior_session_id, db=self.dbconn)
+        rewards = Rewards.from_stimulus_file(stimulus_file=self.stimulus_file)
+        assert rewards == self.expected
+
+
+class TestNWB:
+    @classmethod
+    def setup_class(cls):
+        dir = Path(__file__).parent.resolve()
+        test_data_dir = dir / 'test_data'
+
+        rewards = pd.read_pickle(str(test_data_dir / 'rewards.pkl'))
+        cls.rewards = Rewards(rewards=rewards)
+
+    def setup_method(self, method):
+        self.nwbfile = pynwb.NWBFile(
+            session_description='asession',
+            identifier='1234',
+            session_start_time=datetime.now()
+        )
+
+    @pytest.mark.parametrize('roundtrip', [True, False])
+    def test_read_write_nwb(self, roundtrip,
+                            data_object_roundtrip_fixture):
+        self.rewards.to_nwb(nwbfile=self.nwbfile)
+
+        if roundtrip:
+            obt = data_object_roundtrip_fixture(
+                nwbfile=self.nwbfile,
+                data_object_cls=Rewards)
+        else:
+            obt = self.rewards.from_nwb(nwbfile=self.nwbfile)
+
+        assert obt == self.rewards
