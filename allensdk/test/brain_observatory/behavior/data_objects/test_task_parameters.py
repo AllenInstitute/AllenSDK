65c7f662a856455341bc03bc85a6a877ea42fdb6
aamster <adam.amster@alleninstitute.org>
2021-07-19 10:18:56 -0700

    add task parameters

--- allensdk/test/brain_observatory/behavior/data_objects/test_task_parameters.py
+++ allensdk/test/brain_observatory/behavior/data_objects/test_task_parameters.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from pathlib import Path
 
@@ -15,6 +16,8 @@ from allensdk.brain_observatory.behavior.data_objects.stimuli.stimuli import \
 from allensdk.brain_observatory.behavior.data_objects.stimuli.templates \
     import \
     Templates
+from allensdk.brain_observatory.behavior.data_objects.task_parameters import \
+    TaskParameters
 from allensdk.test.brain_observatory.behavior.data_objects.lims_util import \
     LimsTest
 
@@ -27,21 +30,16 @@ class TestFromStimulusFile(LimsTest):
         dir = Path(__file__).parent.resolve()
         test_data_dir = dir / 'test_data'
 
-        presentations = \
-            pd.read_pickle(str(test_data_dir / 'presentations.pkl'))
-        templates = \
-            pd.read_pickle(str(test_data_dir / 'templates.pkl'))
-        cls.expected_presentations = Presentations(presentations=presentations)
-        cls.expected_templates = Templates(templates=templates)
+        with open(test_data_dir / 'task_parameters.json') as f:
+            tp = json.load(f)
+        cls.expected = TaskParameters(**tp)
 
     @pytest.mark.requires_bamboo
     def test_from_stimulus_file(self):
         stimulus_file = StimulusFile.from_lims(
             behavior_session_id=self.behavior_session_id, db=self.dbconn)
-        stimuli = Stimuli.from_stimulus_file(stimulus_file=stimulus_file,
-                                             limit_to_images=['im065'])
-        assert stimuli.presentations == self.expected_presentations
-        assert stimuli.templates == self.expected_templates
+        tp = TaskParameters.from_stimulus_file(stimulus_file=stimulus_file)
+        assert tp == self.expected
 
 
 class TestNWB:
@@ -50,14 +48,9 @@ class TestNWB:
         dir = Path(__file__).parent.resolve()
         cls.test_data_dir = dir / 'test_data'
 
-        presentations = \
-            pd.read_pickle(str(cls.test_data_dir / 'presentations.pkl'))
-        templates = \
-            pd.read_pickle(str(cls.test_data_dir / 'templates.pkl'))
-        presentations = presentations.drop('is_change', axis=1)
-        p = Presentations(presentations=presentations)
-        t = Templates(templates=templates)
-        cls.stimuli = Stimuli(presentations=p, templates=t)
+        with open(cls.test_data_dir / 'task_parameters.json') as f:
+            tp = json.load(f)
+        cls.task_parameters = TaskParameters(**tp)
 
     def setup_method(self, method):
         self.nwbfile = pynwb.NWBFile(
@@ -66,25 +59,16 @@ class TestNWB:
             session_start_time=datetime.now()
         )
 
-        # Need to write stimulus timestamps first
-        bsf = StimulusFile(
-            filepath=self.test_data_dir / 'behavior_stimulus_file.pkl')
-        ts = StimulusTimestamps.from_stimulus_file(stimulus_file=bsf)
-        ts.to_nwb(nwbfile=self.nwbfile)
-
     @pytest.mark.parametrize('roundtrip', [True, False])
     def test_read_write_nwb(self, roundtrip,
                             data_object_roundtrip_fixture):
-        self.stimuli.to_nwb(nwbfile=self.nwbfile)
+        self.task_parameters.to_nwb(nwbfile=self.nwbfile)
 
         if roundtrip:
             obt = data_object_roundtrip_fixture(
                 nwbfile=self.nwbfile,
-                data_object_cls=Stimuli)
+                data_object_cls=TaskParameters)
         else:
-            obt = Stimuli.from_nwb(nwbfile=self.nwbfile)
+            obt = TaskParameters.from_nwb(nwbfile=self.nwbfile)
 
-        # is_change different due to limit_to_images
-        obt.presentations.value.drop('is_change', axis=1, inplace=True)
-
-        assert obt == self.stimuli
+        assert obt == self.task_parameters
