44ee999699364626e79638fa1cef46b439998581
aamster <adam.amster@alleninstitute.org>
2021-07-29 15:56:13 -0700

    fix overindent

--- allensdk/test/brain_observatory/behavior/data_objects/stimulus_timestamps/test_timestamps_processing.py
+++ allensdk/test/brain_observatory/behavior/data_objects/stimulus_timestamps/test_timestamps_processing.py
@@ -12,40 +12,40 @@ from allensdk.internal.brain_observatory.time_sync import OphysTimeAligner
 @pytest.mark.parametrize("pkl_data, expected", [
     # Extremely basic test case
     (
-            # pkl_data
-            {
-                "items": {
-                    "behavior": {
-                        "intervalsms": np.array([
-                            1000, 1001, 1002, 1003, 1004, 1005
-                        ])
-                    }
+        # pkl_data
+        {
+            "items": {
+                "behavior": {
+                    "intervalsms": np.array([
+                        1000, 1001, 1002, 1003, 1004, 1005
+                    ])
                 }
-            },
-            # expected
-            np.array([
-                0.0, 1.0, 2.001, 3.003, 4.006, 5.01, 6.015
-            ])
+            }
+        },
+        # expected
+        np.array([
+            0.0, 1.0, 2.001, 3.003, 4.006, 5.01, 6.015
+        ])
     ),
     # More realistic test case
     (
-            # pkl_data
-            {
-                "items": {
-                    "behavior": {
-                        "intervalsms": np.array([
-                            16.5429, 16.6685, 16.66580001, 16.70569999,
-                            16.6668,
-                            16.69619999, 16.655, 16.6805, 16.75940001, 16.6831
-                        ])
-                    }
+        # pkl_data
+        {
+            "items": {
+                "behavior": {
+                    "intervalsms": np.array([
+                        16.5429, 16.6685, 16.66580001, 16.70569999,
+                        16.6668,
+                        16.69619999, 16.655, 16.6805, 16.75940001, 16.6831
+                    ])
                 }
-            },
-            # expected
-            np.array([
-                0.0, 0.0165429, 0.0332114, 0.0498772, 0.0665829, 0.0832497,
-                0.0999459, 0.1166009, 0.1332814, 0.1500408, 0.1667239
-            ])
+            }
+        },
+        # expected
+        np.array([
+            0.0, 0.0165429, 0.0332114, 0.0498772, 0.0665829, 0.0832497,
+            0.0999459, 0.1166009, 0.1332814, 0.1500408, 0.1667239
+        ])
     )
 ])
 def test_get_behavior_stimulus_timestamps(pkl_data, expected):
