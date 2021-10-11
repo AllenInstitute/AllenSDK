2130a9963fc991f2664376136524adb08221997b
danielsf <scott.daniel@alleninstitute.org>
2021-10-11 11:21:55 -0700

    add test for missing rig_geometry

--- allensdk/test/brain_observatory/behavior/test_incomplete_data_objects.py
+++ allensdk/test/brain_observatory/behavior/test_incomplete_data_objects.py
@@ -1,11 +1,14 @@
 import pytest
 
+import copy
 from allensdk.brain_observatory.behavior.\
     data_objects.eye_tracking.eye_tracking_table import EyeTrackingTable
 from allensdk.brain_observatory.behavior.\
     data_objects.licks import Licks
 from allensdk.brain_observatory.behavior.\
     data_objects.rewards import Rewards
+from allensdk.brain_observatory.behavior.behavior_ophys_experiment import (
+    BehaviorOphysExperiment)
 
 
 @pytest.mark.requires_bamboo
@@ -60,3 +63,42 @@ def test_incomplete_rewards(
     assert populated_columns == empty_columns
 
     assert populated_rewards.index.name == empty_rewards.index.name
+
+
+@pytest.mark.requires_bamboo
+def test_incomplete_rig_geometry(
+        behavior_ophys_experiment_fixture,
+        skeletal_nwb_fixture):
+
+    populated_rig_geom = behavior_ophys_experiment_fixture.\
+                                eye_tracking_rig_geometry
+
+    assert len(populated_rig_geom) > 0
+
+    # copy the BehaviorOphysExperiment, set its rig geometry to
+    # None by hand, write it out to an NWB file, read that NWB
+    # file back, and make sure that an empty data object of the
+    # correct type is returned for eye_tracking_rig_geometry
+
+    no_rig_geom = copy.deepcopy(behavior_ophys_experiment_fixture)
+
+    no_rig_geom._eye_tracking_rig_geometry = None
+
+    # make sure we didn't alter the fixture
+    assert behavior_ophys_experiment_fixture.\
+           _eye_tracking_rig_geometry is not None
+
+    nwb = no_rig_geom.to_nwb()
+    test_ophys_experiment = BehaviorOphysExperiment.from_nwb(nwb)
+
+    assert len(test_ophys_experiment.eye_tracking_rig_geometry) == 0
+
+    assert isinstance(
+            test_ophys_experiment.eye_tracking_rig_geometry,
+            type(behavior_ophys_experiment_fixture.eye_tracking_rig_geometry))
+
+    # make sure that the populated BehaviorOphysExperiment actually
+    # writes out RigGeometry to NWB files
+    nwb = behavior_ophys_experiment_fixture.to_nwb()
+    roundtrip = BehaviorOphysExperiment.from_nwb(nwb)
+    assert len(roundtrip.eye_tracking_rig_geometry) > 0
