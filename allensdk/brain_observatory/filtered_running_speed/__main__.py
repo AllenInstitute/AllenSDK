15e46eb2bae6a51698b6d3be0f7eef0ef23f90c5
ns706 <ns706@cam.ac.uk>
2022-01-27 10:13:01 -0800

    #13 renaming file to multi stimulus

--- allensdk/brain_observatory/filtered_running_speed/__main__.py
+++ allensdk/brain_observatory/filtered_running_speed/__main__.py
@@ -1,5 +1,5 @@
 from allensdk.brain_observatory.\
-    filtered_running_speed.filtered_running_speed import (
+    filtered_running_speed.multi_stimulus_running_speed import (
         MultiStimulusRunningSpeed
     )
 
@@ -8,5 +8,5 @@ DEFAULT_ZSCORE_THRESHOLD = 10.0
 USE_LOWPASS_FILTER = True
 
 if __name__ == "__main__":
-    filtered_running_speed = MultiStimulusRunningSpeed()
-    filtered_running_speed.process()
+    multi_stimulus_running_speed = MultiStimulusRunningSpeed()
+    multi_stimulus_running_speed.process()
