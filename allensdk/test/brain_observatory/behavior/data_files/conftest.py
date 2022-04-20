f96316fb1225bf8329b4bdfbcd9455cf0267202d
danielsf <scott.daniel@alleninstitute.org>
2022-04-20 09:59:33 -0700

    refactor BehaviorSession._read_data_from_stimulus_file
    Break the method into discrete methods with uniform call signatures.
    This willmake it easier to make sub-classes of BehaviorSession that
    have their own unique implementations of some, but not all of these
    methods.

--- allensdk/test/brain_observatory/behavior/data_files/conftest.py
+++ allensdk/test/brain_observatory/behavior/data_files/conftest.py
@@ -3,10 +3,16 @@ import pathlib
 import tempfile
 import pandas as pd
 
+from allensdk.brain_observatory.behavior.data_files.stimulus_file import (
+    BehaviorStimulusFile,
+    MappingStimulusFile,
+    ReplayStimulusFile)
+
 
 @pytest.fixture
 def behavior_pkl_fixture(
-        tmp_path_factory):
+        tmp_path_factory,
+        helper_functions):
     """
     Write a behavior pkl file to disk.
     Return a dict containing the path to that file, as well as the
@@ -24,16 +30,13 @@ def behavior_pkl_fixture(
     pd.to_pickle(result, pkl_path)
     yield {'path': pkl_path, 'expected_frames': nframes}
 
-    try:
-        if pkl_path.exists():
-            pkl_path.unlink()
-    except PermissionError:
-        pass
+    helper_functions.windows_safe_cleanup(file_path=pkl_path)
 
 
 @pytest.fixture
 def general_pkl_fixture(
-        tmp_path_factory):
+        tmp_path_factory,
+        helper_functions):
     """
     Write a non-behavior stimulus pkl file to disk.
     Return a dict containing the path to that file, as well as the
@@ -49,8 +52,37 @@ def general_pkl_fixture(
     pd.to_pickle(result, pkl_path)
     yield {'path': pkl_path, 'expected_frames': nframes}
 
-    try:
-        if pkl_path.exists():
-            pkl_path.unlink()
-    except PermissionError:
-        pass
+    helper_functions.windows_safe_cleanup(file_path=pkl_path)
+
+
+@pytest.fixture
+def behavior_stim_fixture(
+        behavior_pkl_fixture):
+    """
+    A BehaviorStimulusFile
+    """
+    return BehaviorStimulusFile.from_json(
+        dict_repr={"behavior_stimulus_file":
+                   behavior_pkl_fixture["path"]})
+
+
+@pytest.fixture
+def replay_stim_fixture(
+        general_pkl_fixture):
+    """
+    A ReplayStimulusFile
+    """
+    return ReplayStimulusFile.from_json(
+        dict_repr={"replay_stimulus_file":
+                   general_pkl_fixture["path"]})
+
+
+@pytest.fixture
+def mapping_stim_fixture(
+        general_pkl_fixture):
+    """
+    A MappingStimulusFile
+    """
+    return MappingStimulusFile.from_json(
+        dict_repr={"mapping_stimulus_file":
+                   general_pkl_fixture["path"]})
