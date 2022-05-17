79092886216bab48ad46a9a0a238c8dec8355002
danielsf <scott.daniel@alleninstitute.org>
2022-05-17 11:30:30 -0700

    unit test does not depend on S3 connection

--- allensdk/test/brain_observatory/behavior/behavior_project_cache/test_vbo_cache.py
+++ allensdk/test/brain_observatory/behavior/behavior_project_cache/test_vbo_cache.py
@@ -1,8 +1,14 @@
 import pytest
-import pandas as pd
-from allensdk.brain_observatory.behavior.behavior_project_cache\
-    .behavior_project_cache import \
-        VisualBehaviorOphysProjectCache
+import pathlib
+
+from allensdk.brain_observatory.behavior.behavior_project_cache \
+    .behavior_project_cache import (
+        VisualBehaviorOphysProjectCache)
+
+from allensdk.brain_observatory.behavior.behavior_session import (
+    BehaviorSession)
+from allensdk.brain_observatory.behavior.behavior_ophys_experiment import (
+    BehaviorOphysExperiment)
 
 
 @pytest.mark.requires_bamboo
@@ -16,14 +22,21 @@ def test_session_from_vbo_cache():
 
     This is really just a smoke test
     """
-    cache_dir = '/allen/aibs/informatics/module_test_data/vbo_cache'
+    cache_dir = pathlib.Path('/allen/aibs/informatics/'
+                             'module_test_data/vbo_cache/'
+                             'visual-behavior-ophys-1.0.1')
 
-    cache = VisualBehaviorOphysProjectCache.from_s3_cache(cache_dir=cache_dir)
-    session = cache.get_behavior_session(behavior_session_id=870987812)
-    experiment = cache.get_behavior_ophys_experiment(
-                    ophys_experiment_id=951980471)
+    ophys_dir = cache_dir / 'behavior_ophys_experiments'
+    beh_dir = cache_dir / 'behavior_sessions'
 
-    assert isinstance(cache.get_behavior_session_table(), pd.DataFrame)
-    assert isinstance(cache.get_ophys_session_table(), pd.DataFrame)
-    assert isinstance(cache.get_ophys_experiment_table(), pd.DataFrame)
-    assert isinstance(cache.get_ophys_cells_table(), pd.DataFrame)
+    BehaviorSession.from_nwb_path(
+            nwb_path=beh_dir / 'behavior_session_870987812.nwb')
+    BehaviorOphysExperiment.from_nwb_path(
+            nwb_path=ophys_dir / 'behavior_ophys_experiment_948507789.nwb')
+
+    # these values are baked-in by the choice we made when releasing the
+    # VBO 2021 dataset
+    assert (VisualBehaviorOphysProjectCache.BUCKET_NAME
+            == 'visual-behavior-ophys-data')
+    assert (VisualBehaviorOphysProjectCache.PROJECT_NAME
+            == 'visual-behavior-ophys')
