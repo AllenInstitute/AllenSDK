3595f6ec93e86971c060ceee76be1598c127c443
danielsf <scott.daniel@alleninstitute.org>
2022-09-20 09:35:16 -0700

    lint test_ecephys_session.py
    Note: this violation (E275, missing whitespace after keyword) was
    a new requirement that popped up when I upgraded from flake8
    v 3.x to v 5.x

--- allensdk/test/brain_observatory/ecephys/test_ecephys_session.py
+++ allensdk/test/brain_observatory/ecephys/test_ecephys_session.py
@@ -468,8 +468,8 @@ def test_empty_presentationwise_spike_times(spike_times_api):
             session.stimulus_presentations.index.values,
             session.units.index.values)
 
-    assert(isinstance(obtained, pd.DataFrame))
-    assert(obtained.empty)
+    assert (isinstance(obtained, pd.DataFrame))
+    assert (obtained.empty)
 
 
 def test_conditionwise_spike_statistics(spike_times_api):
@@ -503,11 +503,18 @@ def test_empty_conditionwise_spike_statistics(spike_times_api):
         stimulus_presentation_ids=session.stimulus_presentations.index.values,
         unit_ids=session.units.index.values
     )
-    assert(len(obtained) == 12)
-    assert(not np.any(obtained['spike_count']))  # check all spike_counts are 0
-    assert(not np.any(obtained['spike_mean']))  # spike_means are 0
-    assert(np.all(np.isnan(obtained['spike_std'])))  # std/sem is undefined
-    assert(np.all(np.isnan(obtained['spike_sem'])))
+    assert (len(obtained) == 12)
+
+    # check all spike_counts are 0
+    assert (not np.any(obtained['spike_count']))
+
+    # spike_means are 0
+    assert (not np.any(obtained['spike_mean']))
+
+    # std/sem is undefined
+    assert (np.all(np.isnan(obtained['spike_std'])))
+
+    assert (np.all(np.isnan(obtained['spike_sem'])))
 
 
 def test_get_stimulus_parameter_values(just_stim_table_api):
