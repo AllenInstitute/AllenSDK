2574b915f6d2f51b334c7a7b22b26e474b07dcd7
Christopher B. Morrison <chris.morrison@alleninstitute.org>
2022-11-29 12:04:18 -0800

    Finish isort+black.

--- allensdk/model/biophysical/utils.py
+++ allensdk/model/biophysical/utils.py
@@ -35,20 +35,21 @@
 #
 import logging
 import os
-from ..biophys_sim.neuron.hoc_utils import HocUtils
-from allensdk.core.nwb_data_set import NwbDataSet
 from math import gcd
-from skimage.measure import block_reduce
-import scipy.interpolate
+
 import numpy as np
-from pkg_resources import resource_filename #@UnresolvedImport
+from allensdk.core.nwb_data_set import NwbDataSet
+from pkg_resources import resource_filename  # @UnresolvedImport
+from skimage.measure import block_reduce
+
+from ..biophys_sim.neuron.hoc_utils import HocUtils
 
 PERISOMATIC_TYPE = "Biophysical - perisomatic"
 ALL_ACTIVE_TYPE = "Biophysical - all active"
 
 
 def create_utils(description, model_type=None):
-    ''' Factory method to create a Utils subclass.
+    """Factory method to create a Utils subclass.
 
     Parameters
     ----------
@@ -56,23 +57,22 @@ def create_utils(description, model_type=None):
         used to initialize Utils subclass
 
     model_type : string
-        Must be one of [PERISOMATIC_TYPE, ALL_ACTIVE_TYPE].  If none, defaults to PERISOMATIC_TYPE
+        Must be one of [PERISOMATIC_TYPE, ALL_ACTIVE_TYPE].  If none, defaults
+        to PERISOMATIC_TYPE
 
     Returns
     -------
-    Utils instance    
-    '''
-
-    
+    Utils instance
+    """
 
     if model_type is None:
         try:
-            model_type = description.data['biophys'][0]['model_type']
-        except KeyError as e:
+            model_type = description.data["biophys"][0]["model_type"]
+        except KeyError:
             logging.error("Could not infer model type from description")
 
-    axon_type = description.data['biophys'][1]['axon_type']
-    
+    axon_type = description.data["biophys"][1]["axon_type"]
+
     if model_type == PERISOMATIC_TYPE:
         return Utils(description)
     elif model_type == ALL_ACTIVE_TYPE:
@@ -80,7 +80,7 @@ def create_utils(description, model_type=None):
 
 
 class Utils(HocUtils):
-    '''A helper class for NEURON functionality needed for
+    """A helper class for NEURON functionality needed for
     biophysical simulations.
 
     Attributes
@@ -91,7 +91,7 @@ class Utils(HocUtils):
         The NEURON python object.
     neuron : module
         The NEURON module.
-    '''
+    """
 
     _log = logging.getLogger(__name__)
 
@@ -105,37 +105,39 @@ class Utils(HocUtils):
         self.stimulus_sampling_rate = None
 
         self.stim_vec_list = []
-        
 
-    def update_default_cell_hoc(self, description, default_cell_hoc='cell.hoc'):
-        ''' replace the default 'cell.hoc' path in the manifest with 'cell.hoc' packaged
-        within AllenSDK if it does not exist '''
+    def update_default_cell_hoc(
+        self, description, default_cell_hoc="cell.hoc"
+    ):
+        """replace the default 'cell.hoc' path in the manifest with 'cell.hoc'
+        packaged within AllenSDK if it does not exist"""
 
-        hoc_files = description.data['neuron'][0]['hoc']
+        hoc_files = description.data["neuron"][0]["hoc"]
         try:
             hfi = hoc_files.index(default_cell_hoc)
 
             if not os.path.exists(default_cell_hoc):
-                abspath_ch = resource_filename(__name__, 
-                                               default_cell_hoc)
+                abspath_ch = resource_filename(__name__, default_cell_hoc)
                 hoc_files[hfi] = abspath_ch
 
                 if not os.path.exists(abspath_ch):
                     raise IOError("cell.hoc does not exist!")
 
-                self._log.warning("Using cell.hoc from the following location: %s", abspath_ch)
-        except ValueError as e:
+                self._log.warning(
+                    "Using cell.hoc from the following location: %s",
+                    abspath_ch,
+                )
+        except ValueError:
             pass
 
-
     def generate_morphology(self, morph_filename):
-        '''Load a swc-format cell morphology file.
+        """Load a swc-format cell morphology file.
 
         Parameters
         ----------
         morph_filename : string
             Path to swc.
-        '''
+        """
         h = self.h
 
         swc = self.h.Import3d_SWC_read()
@@ -150,7 +152,7 @@ class Utils(HocUtils):
             sec.nseg = 1 + 2 * int(sec.L / 40.0)
             if sec.name()[:4] == "axon":
                 h.delete_section(sec=sec)
-        h('create axon[2]')
+        h("create axon[2]")
         for sec in h.axon:
             sec.L = 30
             sec.diam = 1
@@ -161,18 +163,18 @@ class Utils(HocUtils):
         h.define_shape()
 
     def load_cell_parameters(self):
-        '''Configure a neuron after the cell morphology has been loaded.'''
-        passive = self.description.data['passive'][0]
-        genome = self.description.data['genome']
-        conditions = self.description.data['conditions'][0]
+        """Configure a neuron after the cell morphology has been loaded."""
+        passive = self.description.data["passive"][0]
+        genome = self.description.data["genome"]
+        conditions = self.description.data["conditions"][0]
         h = self.h
 
         h("access soma")
 
         # Set fixed passive properties
         for sec in h.allsec():
-            sec.Ra = passive['ra']
-            sec.insert('pas')
+            sec.Ra = passive["ra"]
+            sec.insert("pas")
             for seg in sec:
                 seg.pas.e = passive["e_pas"]
 
@@ -185,26 +187,34 @@ class Utils(HocUtils):
                 h(p["name"] + " = %g " % p["value"])
             else:
                 if p["mechanism"] != "":
-                    h('forsec "' + p["section"] +
-                      '" { insert ' + p["mechanism"] + ' }')
-                h('forsec "' + p["section"] +
-                  '" { ' + p["name"] + ' = %g }' % p["value"])
+                    h(
+                        'forsec "'
+                        + p["section"]
+                        + '" { insert '
+                        + p["mechanism"]
+                        + " }"
+                    )
+                h(
+                    'forsec "'
+                    + p["section"]
+                    + '" { '
+                    + p["name"]
+                    + " = %g }" % p["value"]
+                )
 
         # Set reversal potentials
-        for erev in conditions['erev']:
+        for erev in conditions["erev"]:
             h('forsec "' + erev["section"] + '" { ek = %g }' % erev["ek"])
             h('forsec "' + erev["section"] + '" { ena = %g }' % erev["ena"])
 
-    def setup_iclamp(self,
-                     stimulus_path,
-                     sweep=0):
-        '''Assign a current waveform as input stimulus.
+    def setup_iclamp(self, stimulus_path, sweep=0):
+        """Assign a current waveform as input stimulus.
 
         Parameters
         ----------
         stimulus_path : string
             NWB file name
-        '''
+        """
         self.stim = self.h.IClamp(self.h.soma[0](0.5))
         self.stim.amp = 0
         self.stim.delay = 0
@@ -216,7 +226,11 @@ class Utils(HocUtils):
         # NEURON's dt is in milliseconds
         simulation_dt = 1.0e3 / self.simulation_sampling_rate
         stimulus_dt = 1.0e3 / self.stimulus_sampling_rate
-        self._log.debug("Using simulation dt %f, stimulus dt %f", simulation_dt, stimulus_dt)
+        self._log.debug(
+            "Using simulation dt %f, stimulus dt %f",
+            simulation_dt,
+            stimulus_dt,
+        )
 
         self.h.dt = simulation_dt
         stim_vec = self.h.Vector(self.stim_curr)
@@ -227,12 +241,12 @@ class Utils(HocUtils):
         self.stim_vec_list.append(stim_vec)
 
     def read_stimulus(self, stimulus_path, sweep=0):
-        '''Load current values for a specific experiment sweep and setup simulation
-        and stimulus sampling rates.
+        """Load current values for a specific experiment sweep and setup
+        simulation and stimulus sampling rates.
 
-        NOTE: NEURON only allows simulation timestamps of multiples of 40KHz.  To 
-        avoid aliasing, we set the simulation sampling rate to the least common
-        multiple of the stimulus sampling rate and 40KHz.
+        NOTE: NEURON only allows simulation timestamps of multiples of 40KHz.
+        To avoid aliasing, we set the simulation sampling rate to the least
+        common multiple of the stimulus sampling rate and 40KHz.
 
         Parameters
         ----------
@@ -240,32 +254,35 @@ class Utils(HocUtils):
             NWB file name
         sweep : integer, optional
             sweep index
-        '''
+        """
         Utils._log.info(
-            "reading stimulus path: %s, sweep %s",
-            stimulus_path,
-            sweep)
+            "reading stimulus path: %s, sweep %s", stimulus_path, sweep
+        )
 
         stimulus_data = NwbDataSet(stimulus_path)
         sweep_data = stimulus_data.get_sweep(sweep)
 
         # convert to nA for NEURON
-        self.stim_curr = sweep_data['stimulus'] * 1.0e9
+        self.stim_curr = sweep_data["stimulus"] * 1.0e9
 
         # convert from Hz
-        hz = int(sweep_data['sampling_rate'])
+        hz = int(sweep_data["sampling_rate"])
         neuron_hz = Utils.nearest_neuron_sampling_rate(hz)
 
         self.simulation_sampling_rate = neuron_hz
         self.stimulus_sampling_rate = hz
 
         if hz != neuron_hz:
-            Utils._log.debug("changing sampling rate from %d to %d to avoid NEURON aliasing", hz, neuron_hz)
+            Utils._log.debug(
+                "changing sampling rate from %d to %d to avoid NEURON "
+                "aliasing",
+                hz,
+                neuron_hz,
+            )
 
     def record_values(self):
-        '''Set up output voltage recording.'''
-        vec = {"v": self.h.Vector(),
-               "t": self.h.Vector()}
+        """Set up output voltage recording."""
+        vec = {"v": self.h.Vector(), "t": self.h.Vector()}
 
         vec["v"].record(self.h.soma[0](0.5)._ref_v)
         vec["t"].record(self.h._ref_t)
@@ -273,36 +290,42 @@ class Utils(HocUtils):
         return vec
 
     def get_recorded_data(self, vec):
-        '''Extract recorded voltages and timestamps given the recorded Vector instance.  
-        If self.stimulus_sampling_rate is smaller than self.simulation_sampling_rate, 
-        resample to self.stimulus_sampling_rate.
+        """Extract recorded voltages and timestamps given the recorded Vector
+        instance.
+        If self.stimulus_sampling_rate is smaller than
+        self.simulation_sampling_rate, resample to self.stimulus_sampling_rate.
 
         Parameters
         ----------
-        vec : neuron.Vector 
+        vec : neuron.Vector
            constructed by self.record_values
 
         Returns
         -------
-        dict with two keys: 'v' = numpy.ndarray with voltages, 't' = numpy.ndarray with timestamps
+        dict with two keys: 'v' = numpy.ndarray with voltages,
+            't' = numpy.ndarray with timestamps
 
-        '''
-        junction_potential = self.description.data['fitting'][0]['junction_potential']
-        
+        """
+        junction_potential = self.description.data["fitting"][0][
+            "junction_potential"
+        ]
+
         v = np.array(vec["v"])
         t = np.array(vec["t"])
 
         if self.stimulus_sampling_rate < self.simulation_sampling_rate:
-            factor = self.simulation_sampling_rate / self.stimulus_sampling_rate
-                
+            factor = (
+                self.simulation_sampling_rate / self.stimulus_sampling_rate
+            )
+
             Utils._log.debug("subsampling recorded traces by %dX", factor)
-            v = block_reduce(v, (factor,), np.mean)[:len(self.stim_curr)]
-            t = block_reduce(t, (factor,), np.min)[:len(self.stim_curr)]
+            v = block_reduce(v, (factor,), np.mean)[: len(self.stim_curr)]
+            t = block_reduce(t, (factor,), np.min)[: len(self.stim_curr)]
 
         mV = 1.0e-3
         v = (v - junction_potential) * mV
-        
-        return { "v": v, "t": t }
+
+        return {"v": v, "t": t}
 
     @staticmethod
     def nearest_neuron_sampling_rate(hz, target_hz=40000):
@@ -312,7 +335,6 @@ class Utils(HocUtils):
 
 
 class AllActiveUtils(Utils):
-    
     def __init__(self, description, axon_type):
         """
         Parameters
@@ -320,33 +342,39 @@ class AllActiveUtils(Utils):
         description : Config
             Configuration to run the simulation
         axon_type : string
-                truncated - diameter of the axon segments is read from .swc (default)
+                truncated - diameter of the axon segments is read from .swc
+                    (default)
                 stub - diameter of axon segments is 1 micron
             How the axon is replaced within NEURON
-                
+
         """
         super(AllActiveUtils, self).__init__(description)
         self.axon_type = axon_type
 
     def generate_morphology(self, morph_filename):
-        '''Load a neurolucida or swc-format cell morphology file.
+        """Load a neurolucida or swc-format cell morphology file.
 
         Parameters
         ----------
         morph_filename : string
             Path to morphology.
-        '''
-        if self.axon_type == 'stub':
-            self._log.info('Replacing axon with a stub : length 60 micron, diameter 1 micron')
+        """
+        if self.axon_type == "stub":
+            self._log.info(
+                "Replacing axon with a stub : length 60 micron, diameter 1 "
+                "micron"
+            )
             super(AllActiveUtils, self).generate_morphology(morph_filename)
             return
-        
-        self._log.info('Legacy model - Truncating reconstructed axon after 60 micron')
+
+        self._log.info(
+            "Legacy model - Truncating reconstructed axon after 60 micron"
+        )
         morph_basename = os.path.basename(morph_filename)
-        morph_extension = morph_basename.split('.')[-1]
-        if morph_extension.lower() == 'swc':
+        morph_extension = morph_basename.split(".")[-1]
+        if morph_extension.lower() == "swc":
             morph = self.h.Import3d_SWC_read()
-        elif morph_extension.lower() == 'asc':
+        elif morph_extension.lower() == "asc":
             morph = self.h.Import3d_Neurolucida3()
         else:
             raise Exception("Unknown filetype: %s" % morph_extension)
@@ -371,7 +399,7 @@ class AllActiveUtils(Utils):
         for sec in self.h.allsec():
             if sec.name()[:4] == "axon":
                 self.h.delete_section(sec=sec)
-        self.h('create axon[2]')
+        self.h("create axon[2]")
         for index, sec in enumerate(self.h.axon):
             sec.L = 30
             sec.diam = axon_diams[index]
@@ -383,21 +411,21 @@ class AllActiveUtils(Utils):
         self.h.axon[1].connect(self.h.axon[0], 1.0, 0.0)
 
         # make sure diam reflects 3d points
-        self.h.area(.5, sec=self.h.soma[0])
+        self.h.area(0.5, sec=self.h.soma[0])
 
     def load_cell_parameters(self):
-        '''Configure a neuron after the cell morphology has been loaded.'''
-        passive = self.description.data['passive'][0]
-        genome = self.description.data['genome']
-        conditions = self.description.data['conditions'][0]
+        """Configure a neuron after the cell morphology has been loaded."""
+        passive = self.description.data["passive"][0]
+        genome = self.description.data["genome"]
+        conditions = self.description.data["conditions"][0]
         h = self.h
 
         h("access soma")
 
         # Set fixed passive properties
         for sec in h.allsec():
-            sec.Ra = passive['ra']
-            sec.insert('pas')
+            sec.Ra = passive["ra"]
+            sec.insert("pas")
             # for seg in sec:
             #     seg.pas.e = passive["e_pas"]
 
@@ -415,37 +443,47 @@ class AllActiveUtils(Utils):
             else:
                 if hasattr(h, section_array):
                     if mechanism != "":
-                        print('Adding mechanism %s to %s'
-                              % (mechanism, section_array))
+                        print(
+                            "Adding mechanism %s to %s"
+                            % (mechanism, section_array)
+                        )
                         for section in getattr(h, section_array):
-                            if self.h.ismembrane(str(mechanism),
-                                                 sec=section) != 1:
+                            if (
+                                self.h.ismembrane(str(mechanism), sec=section)
+                                != 1
+                            ):
                                 section.insert(mechanism)
 
-                    print('Setting %s to %.6g in %s'
-                          % (param_name, param_value, section_array))
+                    print(
+                        "Setting %s to %.6g in %s"
+                        % (param_name, param_value, section_array)
+                    )
                     for section in getattr(h, section_array):
                         setattr(section, param_name, param_value)
 
         # Set reversal potentials
-        for erev in conditions['erev']:
+        for erev in conditions["erev"]:
             erev_section_array = erev["section"]
             ek = float(erev["ek"])
             ena = float(erev["ena"])
 
-            print('Setting ek to %.6g and ena to %.6g in %s'
-                  % (ek, ena, erev_section_array))
+            print(
+                "Setting ek to %.6g and ena to %.6g in %s"
+                % (ek, ena, erev_section_array)
+            )
 
             if hasattr(h, erev_section_array):
                 for section in getattr(h, erev_section_array):
                     if self.h.ismembrane("k_ion", sec=section) == 1:
-                        setattr(section, 'ek', ek)
+                        setattr(section, "ek", ek)
 
                     if self.h.ismembrane("na_ion", sec=section) == 1:
-                        setattr(section, 'ena', ena)
+                        setattr(section, "ena", ena)
             else:
-                print("Warning: can't set erev for %s, "
-                      "section array doesn't exist" % erev_section_array)
+                print(
+                    "Warning: can't set erev for %s, "
+                    "section array doesn't exist" % erev_section_array
+                )
 
-        self.h.v_init = conditions['v_init']
-        self.h.celsius = conditions['celsius']
+        self.h.v_init = conditions["v_init"]
+        self.h.celsius = conditions["celsius"]
