afb49bb6e072e59aa6471c3633d911487e435ded
Adam Amster <adam.amster@alleninstitute.org>
2022-12-22 16:09:04 -0500

    Add script to deploy add-on visual coding data to S3

---
+++ scripts/brain_observatory/deploy_visual_coding_ophys_eye_tracking.py
@@ -0,0 +1,85 @@
+"""Script to deploy add-on visual coding data to S3"""
+import os
+import tempfile
+from argparse import ArgumentParser
+from pathlib import Path
+
+from data_release_tools.run_data_release.__main__ import DataReleaseTool
+
+import allensdk
+import pandas as pd
+
+from allensdk.brain_observatory.data_release_utils.metadata_utils\
+    .id_generator import \
+    FileIDGenerator
+from allensdk.brain_observatory.data_release_utils.metadata_utils.utils \
+    import \
+    add_file_paths_to_metadata_table
+
+parser = ArgumentParser('deploy visual coding ophys eye tracking data')
+parser.add_argument(
+    '--data_path',
+    help='dir containing eye tracking data',
+    required=True)
+args = parser.parse_args()
+
+
+def main():
+    files = [x for x in os.listdir(args.data_path) if Path(x).suffix == '.npy']
+    if len(files) > 0:
+        exp_ids = [x.replace('.npy', '') for x in files]
+        for exp_id in exp_ids:
+            os.makedirs(Path(args.data_path) / exp_id)
+            os.rename(
+                Path(args.data_path) / f'{exp_id}.npy',
+                Path(args.data_path) / exp_id / 'eye_tracking.npy')
+
+    metadata_path = _write_metadata()
+    pipeline_metadata = []
+    sdk_metadata = {
+        "name": "AllenSDK",
+        "version": str(allensdk.__version__),
+        "script_name": Path(__file__).name,
+        "comment": "",
+    }
+    pipeline_metadata.append(sdk_metadata)
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        release_tool = DataReleaseTool(input_data={
+            'metadata_files': [metadata_path],
+            'project_name': 'visual-coding-ophys',
+            'data_pipeline_metadata': pipeline_metadata,
+            'bucket_name': 'visual-coding-ophys-data',
+            'remote_client': 'AWS_S3',
+            'release_semver_type': 'minor',
+            'staging_directory': tmp_dir
+        }, args=[])
+        release_tool.run()
+
+
+def _write_metadata():
+    exp_ids = [x for x in os.listdir(args.data_path)
+               if (Path(args.data_path) / x).is_dir()]
+
+    metadata_table = pd.DataFrame({'ophys_experiment_id': exp_ids})
+    file_id_generator = FileIDGenerator()
+
+    # The data release tool assumes that
+    metadata_table = add_file_paths_to_metadata_table(
+        metadata_table=metadata_table,
+        id_generator=file_id_generator,
+        file_dir=Path(args.data_path),
+        file_prefix=None,
+        index_col="ophys_experiment_id",
+        data_dir_col="ophys_experiment_id",
+        on_missing_file='error',
+        file_suffix='npy',
+        file_stem='eye_tracking'
+    )
+    output_path = str(Path(args.data_path) / 'metadata.csv')
+    metadata_table.to_csv(output_path, index=False)
+    return output_path
+
+
+if __name__ == '__main__':
+    main()
