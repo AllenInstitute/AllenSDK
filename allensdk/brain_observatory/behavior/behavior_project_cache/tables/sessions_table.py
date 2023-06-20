423ea15176dcb2cbed18f7c99d57bee464dd2941
Christopher Morrison <chris.morrison@alleninstitute.org>
2023-06-20 13:33:34 -0700

    Add stimulus template for movies.
    Add calculation to templates
    Modify templates to take/return list.
    Make movie loading optional.
    Move metadata_parsers
    Add index type to output dataframe of StimulusTemplates.
    Change Templates input to dictionary.
    Add default image and movie template keys.
    Add raise for illformed template input.
    Add/update docs.
    Update unittests.
    
    Add unittest for templates exception,

--- allensdk/brain_observatory/behavior/behavior_project_cache/tables/sessions_table.py
+++ allensdk/brain_observatory/behavior/behavior_project_cache/tables/sessions_table.py
@@ -14,7 +14,7 @@ from allensdk.brain_observatory.behavior.behavior_project_cache.tables.ophys_ses
 from allensdk.brain_observatory.behavior.behavior_project_cache.tables.project_table import (  # noqa: E501
     ProjectTable,
 )
-from allensdk.brain_observatory.behavior.behavior_project_cache.tables.util.metadata_parsers import (  # noqa: E501
+from allensdk.brain_observatory.behavior.utils.metadata_parsers import (  # noqa: E501
     parse_behavior_context,
     parse_stimulus_set,
 )
