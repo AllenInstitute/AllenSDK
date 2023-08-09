c1915de2a16d8894f8f3021ed04755cf38d603f7
Mike Huang <huangjmike@gmail.com>
2023-08-09 14:57:38 -0700

    remove python3.7 from setup metadata

--- setup.py
+++ setup.py
@@ -87,7 +87,6 @@ setup(
         "License :: Other/Proprietary License", # Allen Institute License
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
